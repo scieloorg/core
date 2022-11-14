1a9f99d01722b6a07ccc3cf87368274475e759ed
Rossi-Luciano <luciano.rossi.lucross@gmail.com>
2022-11-14 16:53:58 -0300

    Adiciona 'tasks.py' para carregar áreas temáticas via interface

---
+++ thematic_areas/tasks.py
@@ -0,0 +1,21 @@
+from django.contrib.auth import get_user_model
+
+from config import celery_app
+
+from thematic_areas import scripts
+
+
+User = get_user_model()
+
+
+@celery_app.task()
+def load_area(*args):
+    """
+    Load thematic area from CSV file.
+
+    Sync or Async function
+    """
+
+    user = User.objects.get(id=args[0]) if args else 1
+
+    scripts.load_thematic_area(user)
