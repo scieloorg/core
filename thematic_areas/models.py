2b5ca5695c259a7bdc2feede987acedc5e2e4181
Rossi-Luciano <luciano.rossi.lucross@gmail.com>
2022-11-14 12:24:56 -0300

    Adiciona o modelo 'GenericThematicArea'

--- thematic_areas/models.py
+++ thematic_areas/models.py
@@ -10,6 +10,56 @@ from . import choices
 
 
 # Create your models here.
+class GenericThematicArea(CommonControlField):
+    text = models.CharField(_("Thematic Area"),  max_length=255, null=True, blank=True)
+    lang = models.CharField(_("Language"), choices=choices.languages, max_length=10, null=True, blank=True)
+    origin = models.CharField(_("Origin Data Base"), max_length=255, null=True, blank=True)
+    level0 = models.CharField(_("Level 0"), choices=choices.thematic_level0,
+                              max_length=255, null=True, blank=True,
+                              help_text=_(
+                                  "Here the thematic colleges of CAPES must be registered, more about these areas access: https://www.gov.br/capes/pt-br/acesso-a-informacao/acoes-e-programas/avaliacao/sobre-a-avaliacao/areas-avaliacao/sobre-as-areas-de-avaliacao/sobre-as-areas-de-avaliacao"))
+
+    level1 = models.CharField(_("Level 1"), choices=choices.thematic_level1,
+                              max_length=255, null=True, blank=True,
+                              help_text=_(
+                                  "Here the thematic colleges of CAPES must be registered, more about these areas access: https://www.gov.br/capes/pt-br/acesso-a-informacao/acoes-e-programas/avaliacao/sobre-a-avaliacao/areas-avaliacao/sobre-as-areas-de-avaliacao/sobre-as-areas-de-avaliacao"))
+
+    level2 = models.CharField(_("Level 2"), choices=choices.thematic_level2,
+                              max_length=255, null=True, blank=True,
+                              help_text=_(
+                                  "Here the thematic colleges of CAPES must be registered, more about these areas access: https://www.gov.br/capes/pt-br/acesso-a-informacao/acoes-e-programas/avaliacao/sobre-a-avaliacao/areas-avaliacao/sobre-as-areas-de-avaliacao/sobre-as-areas-de-avaliacao"))
+
+    class Meta:
+        verbose_name = _("Generic Thematic Area")
+        verbose_name_plural = _("Generic Thematic Areas")
+
+    def __unicode__(self):
+        return u'%s | %s | %s | %s' % (self.text, self.level0, self.level1, self.level2,)
+
+    def __str__(self):
+        return u'%s | %s | %s | %s' % (self.text, self.level0, self.level1, self.level2,)
+
+    @classmethod
+    def get_or_create(cls, text, lang, origin, level0, level1, level2, user):
+        try:
+            return GenericThematicArea.objects.get(text=text, lang=lang, origin=origin,
+                                                   level0=level0, level1=level1, level2=level2)
+        except GenericThematicArea.DoesNotExist:
+            the_area = GenericThematicArea()
+            the_area.text = text
+            the_area.lang = lang
+            the_area.origin = origin
+            the_area.level0 = level0
+            the_area.level1 = level1
+            the_area.level2 = level2
+            the_area.creator = user
+            the_area.save()
+
+        return the_area
+
+    base_form_class = ThematicAreaForm
+
+
 class ThematicArea(CommonControlField):
     """
     Represent the thematic areas wit 3 levels.
