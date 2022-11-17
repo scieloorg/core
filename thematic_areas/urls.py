6e1636900d495aa5c18c85aa9602fad4bcd396ed
Luciano <luciano.rossi.lucross@gmail.com>
2022-11-17 15:34:09 -0300

    Adiciona 'urlpatternsgeneric'

--- thematic_areas/urls.py
+++ thematic_areas/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 
-from .views import import_file, validate, download_sample
+from .views import import_file, validate, download_sample, \
+    generic_import_file, generic_validate, generic_download_sample
 
 app_name = "thematic_areas"
 urlpatterns = [
     path("validate", view=validate, name="validate"),
     path("import", view=import_file, name="import_file"),
     path("download", view=download_sample, name="download_sample"),
+]
+urlpatternsgeneric = [
+    path("validate", view=generic_validate, name="validate"),
+    path("import", view=generic_import_file, name="import_file"),
+    path("download", view=generic_download_sample, name="download_sample"),
 ]
