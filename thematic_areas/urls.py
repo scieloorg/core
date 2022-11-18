af9239d22b2d2d3ca41dae9b091886bda7384a8f
Luciano <luciano.rossi.lucross@gmail.com>
2022-11-18 14:50:58 -0300

    Atualiza 'urls.py'

--- thematic_areas/urls.py
+++ thematic_areas/urls.py
@@ -4,14 +4,12 @@ from .views import import_file, validate, download_sample, \
     generic_import_file, generic_validate, generic_download_sample
 
 app_name = "thematic_areas"
-# urlpatterns = [
-#     path("validate", view=validate, name="validate"),
-#     path("import", view=import_file, name="import_file"),
-#     path("download", view=download_sample, name="download_sample"),
-# ]
-
 urlpatterns = [
-    path("validate", view=generic_validate, name="validate"),
-    path("import", view=generic_import_file, name="import_file"),
-    path("download", view=generic_download_sample, name="download_sample"),
+    path("validate", view=validate, name="validate"),
+    path("import", view=import_file, name="import_file"),
+    path("download", view=download_sample, name="download_sample"),
+    path("validate_generic", view=generic_validate, name="validate"),
+    path("import_generic", view=generic_import_file, name="import_file"),
+    path("download_generic", view=generic_download_sample, name="download_sample"),
 ]
+
