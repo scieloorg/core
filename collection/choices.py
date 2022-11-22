72338a46455785fb7de2fd09c88aba9170097ed3
Luciano <luciano.rossi.lucross@gmail.com>
2022-11-22 15:49:45 -0300

    Adiciona 'choices.py' em 'collection'

---
+++ collection/choices.py
@@ -0,0 +1,13 @@
+
+STATUS = [
+    ('certified', 'Certified'),
+    ('development', 'Development'),
+    ('diffusion', 'Diffusion'),
+    ('independent', 'Independent'),
+]
+
+TYPE = [
+    ('journals', 'Journals'),
+    ('preprints', 'Preprints'),
+    ('repositories', 'Repositories'),
+]
