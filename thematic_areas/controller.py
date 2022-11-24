d6bab2b9b72c7b810ac1513f18d65cef63e9d0cc
Luciano <luciano.rossi.lucross@gmail.com>
2022-11-24 19:22:10 -0300

    Corrige 'controller.py'

--- thematic_areas/controller.py
+++ thematic_areas/controller.py
@@ -1,7 +1,7 @@
 import os
 import csv
 
-from models import GenericThematicArea
+from thematic_areas.models import GenericThematicArea
 
 
 # This script add bulk of thematic areas
@@ -14,25 +14,26 @@ SEPARATOR = ';'
 def load_thematic_area(user):
     lang, origin = 'pt', 'CAPES'
 
-    with open(os.path.dirname(os.path.realpath(__file__)) + "/../fixtures/thematic_areas.csv", 'r') as csvfile:
-        data = csv.reader(csvfile, delimiter="SEPARATOR")
+    with open(os.path.dirname(os.path.realpath(__file__)) + "/./fixtures/thematic_areas.csv", 'r') as csvfile:
+        data = csv.reader(csvfile, delimiter=SEPARATOR)
         for row in data:
             for level in range(len(row)):
-                if level > 0:
-                    level_up = GenericThematicArea.objects.get(
-                        text=row[level - 1].rstrip('\n'),
+                if row[level]:
+                    if level > 0:
+                        level_up = GenericThematicArea.objects.get(
+                            text=row[level - 1].rstrip('\n'),
+                            lang=lang,
+                            origin=origin,
+                            level=level - 1
+                        )
+                    else:
+                        level_up = None
+
+                    GenericThematicArea.get_or_create(
+                        text=row[level].rstrip('\n'),
                         lang=lang,
                         origin=origin,
-                        level=level - 1
+                        level=level,
+                        level_up=level_up,
+                        user=user
                     )
-                else:
-                    level_up = None
-
-                GenericThematicArea.get_or_create(
-                    text=row[level].rstrip('\n'),
-                    lang=lang,
-                    origin=origin,
-                    level=level,
-                    level_up=level_up,
-                    user=user
-                )
