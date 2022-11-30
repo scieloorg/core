06fe9a544e95bf29494908dc6b55b11d5773df32
Luciano <luciano.rossi.lucross@gmail.com>
2022-11-30 12:04:12 -0300

    Adiciona 'abas' para interação na interface

--- journal/models.py
+++ journal/models.py
@@ -101,15 +101,60 @@ class ScieloJournal(CommonControlField, ClusterableModel, SocialNetwork):
     logo = models.ForeignKey('wagtailimages.Image', null=True, blank=True, on_delete=models.SET_NULL, related_name='+')
     submission_online_url = models.URLField(_("Submission online URL"), max_length=255, null=True, blank=True)
 
-    panels = [
+    panels_identification = [
         FieldPanel('official'),
+        FieldPanel('short_title'),
     ]
 
+    panels_title = [
+        InlinePanel('title', label=_('SciELO Journal Title'), classname='collapsed'),
+    ]
+
+    panels_mission = [
+        InlinePanel('mission', label=_('Mission'), classname="collapsed"),
+    ]
+
+    panels_owner = [
+        InlinePanel('owner', label=_('Owner'), classname="collapsed"),
+    ]
+
+    panels_editorial_manager = [
+        InlinePanel('editorialmanager', label=_('Editorial Manager'), classname="collapsed"),
+    ]
+
+    panels_publisher = [
+        InlinePanel('publisher', label=_('Publisher'), classname="collapsed"),
+    ]
+
+    panels_sponsor = [
+        InlinePanel('sponsor', label=_('Sponsor'), classname="collapsed"),
+    ]
+
+    panels_website = [
+        ImageChooserPanel('logo', heading=_('Logo')),
+        FieldPanel('submission_online_url'),
+        InlinePanel('journalsocialnetwork', label=_('Social Network'))
+    ]
+
+    edit_handler = TabbedInterface(
+        [
+            ObjectList(panels_identification, heading=_('Identification')),
+            ObjectList(panels_title, heading=_('Title')),
+            ObjectList(panels_mission, heading=_('Missions')),
+            ObjectList(panels_owner, heading=_('Owners')),
+            ObjectList(panels_editorial_manager, heading=_('Editorial Manager')),
+            ObjectList(panels_publisher, heading=_('Publisher')),
+            ObjectList(panels_sponsor, heading=_('Sponsor')),
+            ObjectList(panels_website, heading=_('Website')),
+        ]
+    )
+
     class Meta:
-        verbose_name = _('Journal')
-        verbose_name_plural = _('Journals')
+        verbose_name = _('SciELO Journal')
+        verbose_name_plural = _('SciELO Journals')
         indexes = [
             models.Index(fields=['official', ]),
+            models.Index(fields=['short_title', ]),
         ]
 
     @property
@@ -128,3 +173,32 @@ class ScieloJournal(CommonControlField, ClusterableModel, SocialNetwork):
         return u'%s' % self.official or ''
 
     base_form_class = JournalForm
+
+
+class ScieloJournalTitle(Orderable):
+    page = ParentalKey(ScieloJournal, related_name='title')
+    journal_title = models.CharField(_('SciELO Journal Title'), max_length=255, null=True, blank=True)
+
+
+class Mission(Orderable, RichTextWithLang):
+    page = ParentalKey(ScieloJournal, on_delete=models.CASCADE, related_name='mission')
+
+
+class Owner(Orderable, InstitutionHistory):
+    page = ParentalKey(ScieloJournal, on_delete=models.CASCADE, related_name='owner')
+
+
+class EditorialManager(Orderable, InstitutionHistory):
+    page = ParentalKey(ScieloJournal, on_delete=models.CASCADE, related_name='editorialmanager')
+
+
+class Publisher(Orderable, InstitutionHistory):
+    page = ParentalKey(ScieloJournal, on_delete=models.CASCADE, related_name='publisher')
+
+
+class Sponsor(Orderable, InstitutionHistory):
+    page = ParentalKey(ScieloJournal, on_delete=models.CASCADE, related_name='sponsor')
+
+
+class JournalSocialNetwork(Orderable, SocialNetwork):
+    page = ParentalKey(ScieloJournal, on_delete=models.CASCADE, related_name='journalsocialnetwork')
