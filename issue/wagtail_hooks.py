a57ed58ef49d362116da9054310e4b98d5f6bafe
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2022-12-05 14:52:19 -0300

    Adiciona interface

---
+++ issue/wagtail_hooks.py
@@ -0,0 +1,44 @@
+from django.http import HttpResponseRedirect
+from django.utils.translation import gettext as _
+from wagtail.contrib.modeladmin.options import ModelAdmin, modeladmin_register, ModelAdminGroup
+from wagtail.contrib.modeladmin.views import CreateView
+
+from .models import Issue
+
+
+class IssueCreateView(CreateView):
+
+    def form_valid(self, form):
+        self.object = form.save_all(self.request.user)
+        return HttpResponseRedirect(self.get_success_url())
+
+
+class IssueAdmin(ModelAdmin):
+    model = Issue
+    inspect_view_enabled = True
+    menu_label = _('Issues')
+    create_view_class = IssueCreateView
+    menu_icon = 'folder'
+    menu_order = 300
+    add_to_settings_menu = False
+    exclude_from_explorer = False
+
+    list_display = (
+        'journal',
+        'number',
+        'volume',
+        'date',
+        'url',
+    )
+    list_filter = (
+        'date',
+    )
+    search_fields = (
+        'journal',
+        'number',
+        'volume',
+        'date',
+    )
+
+
+modeladmin_register(IssueAdmin)
