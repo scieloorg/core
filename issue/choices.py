dd49bd16304aa553f2eb03fcce21a9a1e62b6eaf
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2022-12-06 16:03:25 -0300

    Adiciona 'choices.py'

---
+++ issue/choices.py
@@ -0,0 +1,17 @@
+from django.utils.translation import gettext_lazy as _
+
+
+MONTHS = [
+    ('01', _('January')),
+    ('02', _('February')),
+    ('03', _('March')),
+    ('04', _('April')),
+    ('05', _('May')),
+    ('06', _('June')),
+    ('07', _('July')),
+    ('08', _('August')),
+    ('09', _('September')),
+    ('10', _('October')),
+    ('11', _('November')),
+    ('12', _('December')),
+]
