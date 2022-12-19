17ca53fb49e99796d2da6d7c1d37641d97b7b5de
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2022-12-19 11:35:37 -0300

    Adiciona função para retornar a lista de periódicos de uma coleção

--- journal/controller.py
+++ journal/controller.py
@@ -20,3 +20,21 @@ def get_collection():
         error.type = str(type(e))
         error.save()
 
+
+def get_issn(collection):
+    try:
+        collections = requests.get(f"http://{collection}/scielo.php?script=sci_alphabetic&lng=es&nrm=iso&debug=xml",
+                                   timeout=10)
+        data = xmltodict.parse(collections.text)
+
+        for issn in data['SERIALLIST']['LIST']['SERIAL']:
+            yield issn['TITLE']['@ISSN']
+
+    except Exception as e:
+        error = ProcessingError()
+        error.step = "Collection ISSN's list search error"
+        error.description = str(e)[:509]
+        error.type = str(type(e))
+        error.save()
+
+
