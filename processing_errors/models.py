56d268bb9fc9e72143b4014e8633c21884146c82
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2022-12-21 17:48:15 -0300

    Adiciona atributo em 'ProcessingError'

--- processing_errors/models.py
+++ processing_errors/models.py
@@ -7,6 +7,7 @@ from .forms import ProcessingErrorsForm
 
 
 class ProcessingError(CommonControlField):
+    item = models.CharField(_('Error item'), max_length=510, null=True, blank=True)
     description = models.CharField(_('Error description'), max_length=510, null=True, blank=True)
     type = models.CharField(_('Error type'), max_length=255, null=True, blank=True)
     step = models.CharField(_('Error occurrence step'), max_length=255, null=True, blank=True)
