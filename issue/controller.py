088708735a3b36d8c1a975961ed61b82f5c5da78
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2023-01-22 11:15:18 -0300

    Adiciona função para obter os ISSN's de cada coleção

--- issue/controller.py
+++ issue/controller.py
@@ -21,3 +21,28 @@ def get_collection():
         error.save()
 
 
+def get_issn(collection):
+    try:
+        collections = requests.get(
+            f"http://{collection}/scielo.php?script=sci_alphabetic&lng=es&nrm=iso&debug=xml", timeout=10)
+        data = xmltodict.parse(collections.text)
+
+        for issn in data['SERIALLIST']['LIST']['SERIAL']:
+            try:
+                yield issn['TITLE']['@ISSN']
+            except Exception as e:
+                error = ProcessingError()
+                error.item = f"ISSN's list of {collection} collection error"
+                error.step = "Get an ISSN from a collection error"
+                error.description = str(e)[:509]
+                error.type = str(type(e))
+                error.save()
+
+    except Exception as e:
+        error = ProcessingError()
+        error.step = "Collection ISSN's list search error"
+        error.description = str(e)[:509]
+        error.type = str(type(e))
+        error.save()
+
+
