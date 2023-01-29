5b3b80bb0ab93f3957b63781f3ad66d94aeaf663
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2023-01-29 18:46:50 -0300

    Adiciona modelo 'article_funding'

--- article/models.py
+++ article/models.py
@@ -48,3 +48,46 @@ class Article(CommonControlField):
     base_form_class = CoreAdminModelForm
 
 
+class ArticleFunding(CommonControlField):
+    award_id = models.CharField(_("Award ID"), blank=True, null=True, max_length=50)
+    funding_source = models.ForeignKey(Sponsor, null=True, blank=True, on_delete=models.SET_NULL)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['award_id', ]),
+        ]
+
+    panels = [
+        FieldPanel('award_id'),
+        FieldPanel('funding_source'),
+    ]
+
+    def __unicode__(self):
+        return u'%s | %s' % (self.award_id, self.funding_source)
+
+    def __str__(self):
+        return u'%s | %s' % (self.award_id, self.funding_source)
+
+    @property
+    def data(self):
+        _data = {
+            'article_funding__award_id': self.award_id,
+        }
+        if self.funding_source:
+            _data.update(self.funding_source.data)
+
+        return _data
+
+    @classmethod
+    def get_or_create(cls, award_id, funding_source):
+        try:
+            return cls.objects.filter(award_id=award_id, funding_source=funding_source)
+        except:
+            article_funding = cls()
+            article_funding.award_id = award_id
+            article_funding.funding_source = funding_source
+            article_funding.save()
+
+            return article_funding
+
+    base_form_class = CoreAdminModelForm
