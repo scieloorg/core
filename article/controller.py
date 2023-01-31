005821b9c9d4f4059f764ddf1b9265cab722ed41
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2023-01-31 07:20:48 -0300

    Corrige 'controller.py'

--- article/controller.py
+++ article/controller.py
@@ -1,28 +1,32 @@
 import csv
+import logging
 
 from .models import Article, ArticleFunding
 from institution.models import Sponsor
 
 
 def load_financial_data(row, user):
-    sponsor = Sponsor.get_or_create(
-        inst_name=row.get('funding_source'),
-        inst_acronym=None,
-        level_1=None,
-        level_2=None,
-        level_3=None,
-        location=None,
-        official=None,
-        is_official=None
-    )
-    article_funding = ArticleFunding.get_or_create(award_id=row.get('award_id'), funding_source=sponsor)
-    article = Article.get_or_create(pid_v2=row.get('pid_v2'), funding=article_funding)
+    article_findings = []
+    for institution in row.get('funding_source').split(','):
+        sponsor = Sponsor.get_or_create(
+            inst_name=institution,
+            inst_acronym=None,
+            level_1=None,
+            level_2=None,
+            level_3=None,
+            location=None,
+            official=None,
+            is_official=None
+        )
+        article_findings.append(ArticleFunding.get_or_create(award_id=row.get('award_id'), funding_source=sponsor, user=user))
+    article = Article.get_or_create(pid_v2=row.get('pid_v2'), fundings=article_findings, user=user)
 
     return article
 
 
-def read_file(user):
-    with open("article/fixtures/financial_data.csv", 'r') as csvfile:
+def read_file(user, file_path):
+    with open(file_path, 'r') as csvfile:
         data = csv.DictReader(csvfile)
         for row in data:
+            logging.debug(row)
             load_financial_data(row, user)
