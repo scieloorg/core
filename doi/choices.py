fa4a80496c946d11eaf9003f7f0b2aaab7c7cd35
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2023-02-01 08:21:47 -0300

    Adiciona 'choices.py'

---
+++ doi/choices.py
@@ -0,0 +1,9 @@
+from django.utils.translation import gettext as _
+
+
+STATUS = [
+    ('DATA_CREATED', _('DATA_CREATED')),
+    ('SUBMITTED', _('SUBMITTED')),
+    ('QUEUED', _('QUEUED')),
+    ('DEPOSITED', _('DEPOSITED')),
+]
