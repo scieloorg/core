acdd7438c276199b2b39bec51743db711f522803
Roberta Takenaka <roberta.takenaka@scielo.org>
2023-02-21 22:45:35 -0300

    Cria o modelo Language na app core

--- core/models.py
+++ core/models.py
@@ -101,3 +101,47 @@ class FlexibleDate(models.Model):
             date__month=self.month,
             date__day=self.day,
         )
+
+
+class Language(CommonControlField):
+    """
+    Represent the list of states
+
+    Fields:
+        name
+        code2
+    """
+    name = models.TextField(_("Language Name"), blank=True, null=True)
+    code2 = models.TextField(_("Language code 2"), blank=True, null=True)
+
+    class Meta:
+        verbose_name = _("Language")
+        verbose_name_plural = _("Languages")
+
+    def __unicode__(self):
+        return self.code2 or 'idioma ausente / não informado'
+
+    def __str__(self):
+        return self.code2 or 'idioma ausente / não informado'
+
+    @classmethod
+    def get_or_create(cls, name=None, code2=None, creator=None):
+        if code2:
+            try:
+                return cls.objects.get(code2__icontains=code2)
+            except cls.DoesNotExist:
+                pass
+
+        if name:
+            try:
+                return cls.objects.get(name__icontains=name)
+            except cls.DoesNotExist:
+                pass
+
+        if name or code2:
+            obj = Language()
+            obj.name = name
+            obj.code2 = code2 or ''
+            obj.creator = creator
+            obj.save()
+            return obj
