3563274081e8d2bac54273a4eeff134a924840e5
Roberta Takenaka <roberta.takenaka@scielo.org>
2023-02-21 23:52:18 -0300

    Cria funcionalidade para registrar arquivos no Minio

---
+++ files_storage/exceptions.py
@@ -0,0 +1,18 @@
+class MinioFileGetOrCreateError(Exception):
+    ...
+
+
+class GetFilesStorageError(Exception):
+    ...
+
+
+class PushPidProviderXMLError(Exception):
+    ...
+
+
+class PushFileError(Exception):
+    ...
+
+
+class PutXMLContentError(Exception):
+    ...
