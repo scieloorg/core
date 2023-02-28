e9315fa1bbef63837f50ef4fb3f98ffc23c06945
Regis Santos <regis42santos@gmail.com>
2023-02-28 02:31:38 -0300

    Adiciona mais campos. close #72

--- dataset/models.py
+++ dataset/models.py
@@ -1,10 +1,48 @@
 from django.db import models
+from core.models import CommonControlField
 
 
-class Dataset(models.Model):
+class Dataset(CommonControlField):
     name = models.TextField(null=True, blank=True)
+    name_of_dataverse = models.TextField(null=True, blank=True)
+    citation = models.TextField(null=True, blank=True)
+    citationHtml = models.TextField(null=True, blank=True)
+    dataset_citation = models.TextField(null=True, blank=True)
     dataset_id = models.PositiveIntegerField(null=True, blank=True)
+    dataset_id = models.PositiveIntegerField(null=True, blank=True)
+    dataset_name = models.TextField(null=True, blank=True)
+    dataset_persistent_id = models.TextField(null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    #
+    file_content_type = models.CharField(max_length=100, null=True, blank=True)
     file_id = models.PositiveIntegerField(null=True, blank=True)
+    file_persistent_id = models.TextField(null=True, blank=True)
+    file_type = models.CharField(max_length=25, null=True, blank=True)
+    file_count = models.PositiveIntegerField(null=True, blank=True)
+    #
+    global_id = models.TextField(null=True, blank=True)
+    identifier = models.CharField(max_length=15, null=True, blank=True)
+    identifier_of_dataverse = models.CharField(max_length=15, null=True, blank=True)
+    #
+    major_version = models.PositiveIntegerField(null=True, blank=True)
+    minor_version = models.PositiveIntegerField(null=True, blank=True)
+    md5 = models.CharField(max_length=32, null=True, blank=True)
+    #
+    published_at = models.DateTimeField(null=True, blank=True)
+    publisher = models.ForeignKey(
+        'Publisher',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+    #
+    size_in_bytes = models.PositiveIntegerField(null=True, blank=True)
+    storage_identifier = models.TextField(null=True, blank=True)
+    unf = models.CharField(max_length=30, null=True, blank=True)
+    url = models.URLField(null=True, blank=True)
+    version_id = models.PositiveIntegerField(null=True, blank=True)
+    version_state = models.CharField(max_length=8, null=True, blank=True)
+    #
     authors = models.ManyToManyField('Author', blank=True)
     contacts = models.ManyToManyField('Contact', blank=True)
     datasources = models.ManyToManyField('Datasource', blank=True)
@@ -14,7 +52,7 @@ class Dataset(models.Model):
     subjects = models.ManyToManyField('Subject', blank=True)
 
     def __str__(self):
-        return f"{self.name}"
+        return f'{self.name}'
 
 
 class Checksum(models.Model):
@@ -23,7 +61,14 @@ class Checksum(models.Model):
     dataset = models.ForeignKey(Dataset, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
-        return f"{self.type_field}"
+        return f'{self.type_field}'
+
+
+class Publisher(models.Model):
+    name = models.TextField(unique=True)
+
+    def __str__(self):
+        return f'{self.name}'
 
 
 class Author(models.Model):
@@ -31,18 +76,18 @@ class Author(models.Model):
     given_name = models.CharField(max_length=300)
 
     def __str__(self):
-        return f"{self.given_name}, {self.surname}"
+        return f'{self.given_name}, {self.surname}'
 
 
 class Affiliation(models.Model):
-    name = models.TextField(Unique=True)
+    name = models.TextField(unique=True)
 
     def __str__(self):
-        return f"{self.name}"
+        return f'{self.name}'
 
 
 class Contact(models.Model):
-    name = models.CharField(max_length=300, Unique=True)
+    name = models.CharField(max_length=300, unique=True)
     affiliation = models.ForeignKey(
         Affiliation,
         on_delete=models.SET_NULL,
@@ -51,28 +96,28 @@ class Contact(models.Model):
     )
 
     def __str__(self):
-        return f"{self.name}"
+        return f'{self.name}'
 
 
 class Datasource(models.Model):
-    title = models.TextField(Unique=True)
+    title = models.TextField(unique=True)
 
     def __str__(self):
-        return f"{self.title}"
+        return f'{self.title}'
 
 
 class Keyword(models.Model):
-    name = models.CharField(max_length=255, Unique=True)
+    name = models.CharField(max_length=255, unique=True)
 
     def __str__(self):
-        return f"{self.name}"
+        return f'{self.name}'
 
 
 class Producer(models.Model):
-    name = models.CharField(max_length=300, Unique=True)
+    name = models.CharField(max_length=300, unique=True)
 
     def __str__(self):
-        return f"{self.name}"
+        return f'{self.name}'
 
 
 class Publication(models.Model):
@@ -81,12 +126,12 @@ class Publication(models.Model):
 
     def __str__(self):
         if self.citation:
-            return f"{self.citation}"
-        return f"{self.url}"
+            return f'{self.citation}'
+        return f'{self.url}'
 
 
 class Subject(models.Model):
-    title = models.TextField(Unique=True)
+    title = models.TextField(unique=True)
 
     def __str__(self):
-        return f"{self.title}"
+        return f'{self.title}'
