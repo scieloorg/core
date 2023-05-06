e39f3b4c534d9ee73200a4b27bbc120029140bd5
Edgar <eduranm@dgb.unam.mx>
2023-05-06 11:54:20 -0500

    Agrega formularios páginas informativas y agrupa pestañas de instituciones

--- journal/models.py
+++ journal/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from django.utils.safestring import mark_safe
 from modelcluster.fields import ParentalKey
 from modelcluster.models import ClusterableModel
 from wagtail.admin.panels import FieldPanel, InlinePanel, ObjectList, TabbedInterface
 from wagtail.models import Orderable
+from wagtail.fields import RichTextField
 
 from collection.models import Collection
 from core.forms import CoreAdminModelForm
@@ -188,21 +190,10 @@ class ScieloJournal(CommonControlField, ClusterableModel, SocialNetwork):
         InlinePanel("mission", label=_("Mission"), classname="collapsed"),
     ]
 
-    panels_owner = [
+    panels_institutions = [
         InlinePanel("owner", label=_("Owner"), classname="collapsed"),
-    ]
-
-    panels_editorial_manager = [
-        InlinePanel(
-            "editorialmanager", label=_("Editorial Manager"), classname="collapsed"
-        ),
-    ]
-
-    panels_publisher = [
+        InlinePanel("editorialmanager", label=_("Editorial Manager"), classname="collapsed"),
         InlinePanel("publisher", label=_("Publisher"), classname="collapsed"),
-    ]
-
-    panels_sponsor = [
         InlinePanel("sponsor", label=_("Sponsor"), classname="collapsed"),
     ]
 
@@ -212,15 +203,31 @@ class ScieloJournal(CommonControlField, ClusterableModel, SocialNetwork):
         InlinePanel("journalsocialnetwork", label=_("Social Network")),
     ]
 
+    panels_about = [
+        InlinePanel("history", label=_("Brief History"), classname="collapsed"),
+        InlinePanel("focus", label=_("Focus and Scope"), classname="collapsed"),
+    ]
+
+    panels_policy = [
+        InlinePanel("open_data", label=_("Open data"), classname="collapsed"),
+        InlinePanel("preprint", label=_("Preprint"), classname="collapsed"),
+        InlinePanel("review", label=_("Peer review"), classname="collapsed"),
+        InlinePanel("ecommittee", label=_("Ethics Committee"), classname="collapsed"),
+        InlinePanel("copyright", label=_("Copyright"), classname="collapsed"),
+        InlinePanel("website_responsibility", label=_("Intellectual Property / Terms of use / Website responsibility"), classname="collapsed"),
+        InlinePanel("author_responsibility", label=_("Intellectual Property / Terms of use / Author responsibility"), classname="collapsed"),
+        InlinePanel("policies", label=_("Retraction Policy | Ethics and Misconduct Policy"), classname="collapsed"),
+        InlinePanel("conflict_policy", label=_("Conflict of interest policy"), classname="collapsed"),
+    ]
+
     edit_handler = TabbedInterface(
         [
             ObjectList(panels_identification, heading=_("Identification")),
             ObjectList(panels_mission, heading=_("Missions")),
-            ObjectList(panels_owner, heading=_("Owners")),
-            ObjectList(panels_editorial_manager, heading=_("Editorial Manager")),
-            ObjectList(panels_publisher, heading=_("Publisher")),
-            ObjectList(panels_sponsor, heading=_("Sponsor")),
+            ObjectList(panels_institutions, heading=_("Related Institutions")),
             ObjectList(panels_website, heading=_("Website")),
+            ObjectList(panels_about, heading=_("About Journal")),
+            ObjectList(panels_policy, heading=_("Journal Policy")),
         ]
     )
 
@@ -364,4 +371,111 @@ class Sponsor(Orderable, InstitutionHistory):
 class JournalSocialNetwork(Orderable, SocialNetwork):
     page = ParentalKey(
         ScieloJournal, on_delete=models.CASCADE, related_name="journalsocialnetwork"
+    )
+
+
+class OpenData(Orderable, RichTextWithLang, CommonControlField):
+    text = RichTextField(null=True, blank=True, 
+            help_text=mark_safe(_("""Refers to sharing data, codes, methods and other materials used and 
+            resulting from research that are usually the basis of the texts of articles published by journals. 
+            Guide: <a target='_blank' href='https://wp.scielo.org/wp-content/uploads/Guia_TOP_pt.pdf'>https://wp.scielo.org/wp-content/uploads/Guia_TOP_pt.pdf</a>""")))
+    journal = ParentalKey(
+        ScieloJournal, on_delete=models.CASCADE, related_name="open_data"
+    )
+
+
+class Preprint(Orderable, RichTextWithLang, CommonControlField):
+    text = RichTextField(null=True, blank=True, 
+            help_text=_("""A preprint is defined as a manuscript ready for submission to a journal that is deposited 
+            with trusted preprint servers before or in parallel with submission to a journal. 
+            This practice joins that of continuous publication as mechanisms to speed up research communication. 
+            Preprints share with journals the originality in the publication of articles and inhibit the use of 
+            the double-blind procedure in the evaluation of manuscripts. 
+            The use of preprints is an option and choice of the authors and it is up to the journals to adapt 
+            their policies to accept the submission of manuscripts previously deposited in a preprints server 
+            recognized by the journal."""))
+    journal = ParentalKey(
+        ScieloJournal, on_delete=models.CASCADE, related_name="preprint"
+    )
+
+    
+class History(Orderable, RichTextWithLang, CommonControlField):
+    text = RichTextField(null=True, blank=True, 
+            help_text=_("Insert here a brief history with events and milestones in the trajectory of the journal"))
+    journal = ParentalKey(
+        ScieloJournal, on_delete=models.CASCADE, related_name="history"
+    )
+
+
+class Focus(Orderable, RichTextWithLang, CommonControlField):
+    text = RichTextField(null=True, blank=True,
+            help_text=_("Insert here the focus and scope of the journal"))
+    journal = ParentalKey(
+        ScieloJournal, on_delete=models.CASCADE, related_name="focus"
+    )
+
+
+class Review(Orderable, RichTextWithLang, CommonControlField):
+    text = RichTextField(null=True, blank=True,
+            help_text=_("Brief description of the review flow"))
+    journal = ParentalKey(
+        ScieloJournal, on_delete=models.CASCADE, related_name="review"
+    )
+
+
+class Ecommittee(Orderable, RichTextWithLang, CommonControlField):
+    text = RichTextField(null=True, blank=True,
+            help_text=_("""Authors must attach a statement of approval from the ethics committee of 
+            the institution responsible for approving the research"""))
+    journal = ParentalKey(
+        ScieloJournal, on_delete=models.CASCADE, related_name="ecommittee"
+    )
+
+
+class Copyright(Orderable, RichTextWithLang, CommonControlField):
+    text = RichTextField(null=True, blank=True,
+            help_text=_("""Describe the policy used by the journal on copyright issues. 
+            We recommend that this section be in accordance with the recommendations of the SciELO criteria, 
+            item 5.2.10.1.2. - Copyright"""))
+    journal = ParentalKey(
+        ScieloJournal, on_delete=models.CASCADE, related_name="copyright"
+    )
+
+
+class WebsiteResponsibility(Orderable, RichTextWithLang, CommonControlField):
+    text = RichTextField(null=True, blank=True,
+            help_text=_("""EX. DOAJ: Copyright terms applied to posted content must be clearly stated and separate 
+            from copyright terms applied to the website"""))
+    journal = ParentalKey(
+        ScieloJournal, on_delete=models.CASCADE, related_name="website_responsibility"
+    )
+
+
+class AuthorResponsibility(Orderable, RichTextWithLang, CommonControlField):
+    text = RichTextField(null=True, blank=True, 
+            help_text=_("""The author's declaration of responsibility for the content published in 
+            the journal that owns the copyright Ex. DOAJ: The terms of copyright must not contradict 
+            the terms of the license or the terms of the open access policy. "All rights reserved" is 
+            never appropriate for open access content"""))
+    journal = ParentalKey(
+        ScieloJournal, on_delete=models.CASCADE, related_name="author_responsibility"
+    )
+
+
+class Policies(Orderable, RichTextWithLang, CommonControlField):
+    text = RichTextField(null=True, blank=True,
+            help_text=mark_safe(_("""Describe here how the journal will deal with ethical issues and/or 
+            issues that may damage the journal's reputation. What is the journal's position regarding 
+            the retraction policy that the journal will adopt in cases of misconduct. 
+            Best practice guide: <a target='_blank' 
+            href='https://wp.scielo.org/wp-content/uploads/Guia-de-Boas-Praticas-para-o-Fortalecimento-da-Etica-na-Publicacao-Cientifica.pdf'>
+            https://wp.scielo.org/wp-content/uploads/Guia-de-Boas-Praticas-para-o-Fortalecimento-da-Etica-na-Publicacao-Cientifica.pdf</a>""")))
+    journal = ParentalKey(
+        ScieloJournal, on_delete=models.CASCADE, related_name="policies"
+    )
+
+
+class ConflictPolicy(Orderable, RichTextWithLang, CommonControlField):
+    journal = ParentalKey(
+        ScieloJournal, on_delete=models.CASCADE, related_name="conflict_policy"
     )
