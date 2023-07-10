b446b9736a689cd7b71e4da5cc982ff61c043cb5
Roberta Takenaka <roberta.takenaka@scielo.org>
2023-07-10 15:54:10 -0300

    Faz com que a URL da API seja uma variável

--- article/scripts/load_preprint.py
+++ article/scripts/load_preprint.py
@@ -1,5 +1,6 @@
 from article.tasks import load_preprint
 
 
-def run(user_id=None):
-    load_preprint.apply_async(args=(user_id,))
+def run(user_id=None, oai_pmh_preprint_uri=None):
+	oai_pmh_preprint_uri = oai_pmh_preprint_uri or "https://preprints.scielo.org/index.php/scielo/oai"
+    load_preprint.apply_async(args=(user_id, oai_pmh_preprint_uri))
