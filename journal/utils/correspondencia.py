a3315c87dc424700cd0ea1b9d08f0df79c6e5b34
Samuel <samuelveigarangel@hotmail.com>
2023-07-13 11:54:19 -0300

    Removido correspondencia do issue e adicionado comentario sobre correspondencia

--- journal/utils/correspondencia.py
+++ journal/utils/correspondencia.py
@@ -1,10 +1,14 @@
+"""    
+O dicionário de correspondência contém os valores das chaves obtidos em:
+    - https://scielo.readthedocs.io/projects/scielo-pc-programs/en/latest/titlemanager_title.html
+"""
+
 correspondencia_journal = {
     "v5": "type_of_literature",
     "v6": "treatment_level",
     "v10": "center_code",
     "v20": "national_code",
     "v30": "identification_number",
-    "v35": "issn_scielo",
     "v37": "secs_code",
     "v50": "publication_status",
     "v51": "journal_status_history_in_this_collection",
