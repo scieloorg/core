8297af812a8ee210baeb88d33faf5d87ead3e74c
Roberta Takenaka <roberta.takenaka@scielo.org>
2023-07-25 21:12:59 -0300

    adiciona chamada para solicitar PID v3 para uma lista de PIDs v2

--- pid_provider/scripts/provide_pid_for_am_xmls.py
+++ pid_provider/scripts/provide_pid_for_am_xmls.py
@@ -1,11 +1,21 @@
 from pid_provider import tasks
 
 
-def run(username, collection_acron, pid_v2):
+def run(username, collection_acron, pids):
+
+    if "," in pids:
+        tasks.provide_pid_for_am_xmls.apply_async(
+            kwargs={
+                "username": username,
+                "collection_acron": collection_acron,
+                "pids": pids.split(","),
+            }
+        )
+        return
     tasks.provide_pid_for_am_xml.apply_async(
         kwargs={
             "username": username,
             "collection_acron": collection_acron,
-            "pid_v2": pid_v2,
+            "pid_v2": pids,
         }
     )
