72d581b4322edace617ce31cc233b9c96a0bef66
Roberta Takenaka <roberta.takenaka@scielo.org>
2023-07-27 17:08:21 -0300

    Remove "tradução" dos nomes das tasks

--- pid_provider/tasks.py
+++ pid_provider/tasks.py
@@ -40,13 +40,13 @@ def _read_jsonl(jsonl_file_path):
             yield json.loads(row.strip())
 
 
-@celery_app.task(bind=True, name=_("load_xml"))
+@celery_app.task(bind=True, name="load_xml")
 def load_xml(self, username, uri, name, acron, year):
     user = _get_user(self.request, username=username)
     kernel.load_xml(user, uri, name, acron, year)
 
 
-@celery_app.task(bind=True, name=_("load_xmls"))
+@celery_app.task(bind=True, name="load_xmls")
 def load_xmls(
     self, username=None, domain=None, article_list=None, jsonl_file_path=None
 ):
@@ -75,7 +75,7 @@ def load_xmls(
         )
 
 
-@celery_app.task(bind=True, name=_("load_xml_lists"))
+@celery_app.task(bind=True, name="load_xml_lists")
 def load_xml_lists(self, username=None, jsonl_files_path=None):
     if not jsonl_files_path:
         raise ValueError("pid_provider.tasks.load_xml_lists requires jsonl_files_path")
@@ -92,7 +92,7 @@ def load_xml_lists(self, username=None, jsonl_files_path=None):
 """
 {"begin_date":"2023-06-01 00-00-00","collection":"scl","dictionary_date":"Sat, 01 Jul 2023 00:00:00 GMT","documents":{"JFhVphtq6czR6PHMvC4w38N":{"aop_pid":"","create":"Sat, 28 Nov 2020 23:42:43 GMT","default_language":"en","journal_acronym":"aabc","pid":"S0001-37652012000100017","pid_v1":"S0001-3765(12)08400117","pid_v2":"S0001-37652012000100017","pid_v3":"JFhVphtq6czR6PHMvC4w38N","publication_date":"2012-05-22","update":"Fri, 30 Jun 2023 20:57:30 GMT"},"ZZYxjr9xbVHWmckYgDwBfTc":{"aop_pid":"","create":"Sat, 28 Nov 2020 23:42:37 GMT","default_language":"en","journal_acronym":"aabc","pid":"S0001-37652012000100014","pid_v1":"S0001-3765(12)08400114","pid_v2":"S0001-37652012000100014","pid_v3":"ZZYxjr9xbVHWmckYgDwBfTc","publication_date":"2012-02-24","update":"Fri, 30 Jun 2023 20:56:59 GMT"},"pxXcvQXT8jQc8mzWz8JKTcq":{"aop_pid":"","create":"Sat, 28 Nov 2020 23:42:35 GMT","default_language":"en","journal_acronym":"aabc","pid":"S0001-37652012000100006","pid_v1":"S0001-3765(12)08400106","pid_v2":"S0001-37652012000100006","pid_v3":"pxXcvQXT8jQc8mzWz8JKTcq","publication_date":"2012-05-22","update":"Fri, 30 Jun 2023 20:56:50 GMT"},"ttD5sS3n4YcP8LVN7w6nJ4z":{"aop_pid":"","create":"Sat, 28 Nov 2020 23:42:33 GMT","default_language":"en","journal_acronym":"aabc","pid":"S0001-37652012000100008","pid_v1":"S0001-3765(12)08400108","pid_v2":"S0001-37652012000100008","pid_v3":"ttD5sS3n4YcP8LVN7w6nJ4z","publication_date":"2012-02-02","update":"Fri, 30 Jun 2023 20:56:37 GMT"},"wxcRCTCY3VnM4H8WSGF7TyK":{"aop_pid":"","create":"Sun, 29 Nov 2020 08:38:58 GMT","default_language":"en","journal_acronym":"aabc","pid":"S0001-37652012000200001","pid_v1":"S0001-3765(12)08400201","pid_v2":"S0001-37652012000200001","pid_v3":"wxcRCTCY3VnM4H8WSGF7TyK","publication_date":"2012-05-25","update":"Fri, 30 Jun 2023 20:56:41 GMT"}},"end_date":"2023-07-01 00-00-00","limit":5,"page":1,"pages":410,"total":2050}
 """
-@celery_app.task(bind=True, name=_("provide_pid_for_opac_xml"))
+@celery_app.task(bind=True, name="provide_pid_for_opac_xml")
 def provide_pid_for_opac_xml(self, username=None, documents=None):
     user = _get_user(self.request, username=username)
     for pid_v3, article in documents.items():
@@ -113,7 +113,7 @@ def provide_pid_for_opac_xml(self, username=None, documents=None):
             kernel.register_failure(e, user=user, detail={"article": article})
 
 
-@celery_app.task(bind=True, name=_("provide_pid_for_opac_xmls"))
+@celery_app.task(bind=True, name="provide_pid_for_opac_xmls")
 def provide_pid_for_opac_xmls(
     self, username=None, begin_date=None, end_date=None, limit=None, pages=None
 ):
@@ -142,7 +142,7 @@ def provide_pid_for_opac_xmls(
                 break
 
 
-@celery_app.task(bind=True, name=_("provide_pid_for_am_xml"))
+@celery_app.task(bind=True, name="provide_pid_for_am_xml")
 def provide_pid_for_am_xml(
     self, username, collection_acron, pid_v2, processing_date=None,
 ):
@@ -154,7 +154,7 @@ def provide_pid_for_am_xml(
     am.request_pid_v3(user, uri, collection_acron, pid_v2, processing_date)
 
 
-@celery_app.task(bind=True, name=_("provide_pid_for_am_xmls"))
+@celery_app.task(bind=True, name="provide_pid_for_am_xmls")
 def provide_pid_for_am_xmls(
     self, username=None, items=None,
 ):
@@ -166,7 +166,7 @@ def provide_pid_for_am_xmls(
         provide_pid_for_am_xml.apply_async(kwargs=item)
 
 
-@celery_app.task(bind=True, name=_("harvest_pids"))
+@celery_app.task(bind=True, name="harvest_pids")
 def harvest_pids(
     self, username=None, collection_acron=None, from_date=None, limit=None, stop=None,
 ):
