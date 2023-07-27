9bf06dfb7e3198dcadab3aaa001fc7df94ed41e5
Roberta Takenaka <roberta.takenaka@scielo.org>
2023-07-27 17:29:43 -0300

    Corrige o nome da task

--- pid_provider/scripts/provide_pid_for_opac_xmls.py
+++ pid_provider/scripts/provide_pid_for_opac_xmls.py
@@ -3,7 +3,7 @@ from pid_provider import tasks
 
 def run(username, begin_date, end_date, limit=None, pages=None):
     # executa a task imediatamente
-    tasks.load_xml_lists_from_opac.apply_async(
+    tasks.provide_pid_for_opac_xmls.apply_async(
         kwargs={
             "username": username,
             "begin_date": begin_date,
