b9320957d2ba5519f332e4ed84933fa963916a4b
Roberta Takenaka <roberta.takenaka@scielo.org>
2023-07-27 15:29:43 -0300

    Adiciona outras alternativas de chamadas para as tasks de solicitar pid para os XML do AM

--- pid_provider/scripts/provide_pid_for_am_xmls.py
+++ pid_provider/scripts/provide_pid_for_am_xmls.py
@@ -1,18 +1,37 @@
 from pid_provider import tasks
 
 
-def run(username, collection_acron, pids):
+def run(username, collection_acron=None, pids=None):
+    if not collection_acron and not pids:
+        return tasks.harvest_pids.apply_async(
+            kwargs={
+                "username": username,
+                "stop": 100,
+            }
+        )
 
-    if "," in pids:
-        tasks.provide_pid_for_am_xmls.apply_async(
+    if not pids:
+        return tasks.harvest_pids.apply_async(
             kwargs={
                 "username": username,
                 "collection_acron": collection_acron,
-                "pids": pids.split(","),
+                "stop": 100,
             }
         )
-        return
-    tasks.provide_pid_for_am_xml.apply_async(
+
+    if "," in pids:
+        items = [
+            {"collection_acron": collection_acron, "pid_v2": pid_v2}
+            for pid_v2 in pids.split(",")
+        ]
+        return tasks.provide_pid_for_am_xmls.apply_async(
+            kwargs={
+                "username": username,
+                "items": items,
+            }
+        )
+
+    return tasks.provide_pid_for_am_xml.apply_async(
         kwargs={
             "username": username,
             "collection_acron": collection_acron,
