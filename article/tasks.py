c0eb59e51d211dc15826fc4728f69cea1bc74207
GitInno <86991526+gitnnolabs@users.noreply.github.com>
2023-08-16 15:38:41 -0300

    Remove a necessidade de busca o usuário na task.

--- article/tasks.py
+++ article/tasks.py
@@ -11,33 +11,21 @@ from . import controller
 
 User = get_user_model()
 
-
-def _get_user(request, username=None, user_id=None):
-    try:
-        return User.objects.get(pk=request.user.id)
-    except AttributeError:
-        if user_id:
-            return User.objects.get(pk=user_id)
-        if username:
-            return User.objects.get(username=username)
-
-
 @celery_app.task()
 def load_funding_data(user, file_path):
     user = User.objects.get(pk=user)
-
+    
     controller.read_file(user, file_path)
 
 
 @celery_app.task(bind=True, name=_("load_article"))
 def load_article(self, user_id, file_path=None, xml=None):
-    user = _get_user(self.request, user_id=user_id)
+    user = User.objects.get(pk=user_id)
     xmlsps.load_article(user, file_path=file_path, xml=xml)
 
 
 @celery_app.task(bind=True, name=_("load_articles"))
 def load_articles(self, user_id=None):
-    user_id = user_id or self.request.user.id
     from_date = Article.last_created_date()
 
     for item in XMLSPS.list(from_date):
@@ -46,6 +34,6 @@ def load_articles(self, user_id=None):
 
 @celery_app.task(bind=True, name=_("load_preprints"))
 def load_preprint(self, user_id, oai_pmh_preprint_uri):
-    user = _get_user(self.request, user_id=user_id)
+    user = User.objects.get(pk=user_id)
     ## fazer filtro para não coletar tudo sempre
     harvest_preprints(oai_pmh_preprint_uri, user)
