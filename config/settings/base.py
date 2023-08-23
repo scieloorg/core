60f53dc5f8aab48d58ca8e63f691a786935ca077
GitInno <86991526+gitnnolabs@users.noreply.github.com>
2023-08-23 07:52:21 -0300

    Altera o arquivo base.py para conter mais uma ENGINE de indexação e os roteadores para o haystack.

--- config/settings/base.py
+++ config/settings/base.py
@@ -414,13 +414,25 @@ SIMPLE_JWT = {
 HAYSTACK_CONNECTIONS = {
     "default": {
         "ENGINE": "haystack.backends.solr_backend.SolrEngine",
-        "URL": env("SOLR_URL", default="http://solr:8983/solr/core"),
+        "URL": "%s%s" % (env("SOLR_URL", default="http://solr:8983/solr/"), "core"),
         "ADMIN_URL": "http://solr:8983/solr/admin/cores",
         "INCLUDE_SPELLING": True,
         "SILENTLY_FAIL": False,
         "SOLR_TIMEOUT": 10,
+        "EXCLUDED_INDEXES": ['article.search_indexes.ArticleOAIIndex']
+    },
+    "oai": {
+        "ENGINE": "haystack.backends.solr_backend.SolrEngine",
+        "URL": "%s%s" % (env("SOLR_URL", default="http://solr:8983/solr/"), "oai"),
+        "ADMIN_URL": "http://solr:8983/solr/admin/cores",
+        "INCLUDE_SPELLING": True,
+        "SILENTLY_FAIL": False,
+        "SOLR_TIMEOUT": 10,
+        "EXCLUDED_INDEXES": ['article.search_indexes.ArticleIndex']
     }
 }
+
+HAYSTACK_ROUTERS = ['core.routers.UpdateEverythingRouter', ]
 
 HAYSTACK_SIGNAL_PROCESSOR = "haystack.signals.RealtimeSignalProcessor"
 
