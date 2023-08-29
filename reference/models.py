a160545d597db93a47ee3020b8599499f4d2348d
Samuel <samuelveigarangel@hotmail.com>
2023-08-29 10:37:53 -0300

    Adicionado Modelo JournalReference

--- reference/models.py
+++ reference/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from core.models import CommonControlField
 # Create your models here.
+
+
+class JournalReference(CommonControlField):
+    other_titles = models.TextField(null=True, blank=True) 
+
+    def __str__(self):
+        return f"{self.other_titles}"
