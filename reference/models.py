6a0d2a40f7bf5f509f1b0ed1238cc331fc2fbf3b
Samuel <samuelveigarangel@hotmail.com>
2023-08-29 15:07:00 -0300

    Modificado modelo e seu campo para JournalTitle e title

--- reference/models.py
+++ reference/models.py
@@ -3,8 +3,8 @@ from core.models import CommonControlField
 # Create your models here.
 
 
-class JournalReference(CommonControlField):
-    other_titles = models.TextField(null=True, blank=True) 
+class JournalTitle(CommonControlField):
+    title = models.TextField(null=True, blank=True) 
 
     def __str__(self):
-        return f"{self.other_titles}"
+        return f"{self.title}"
