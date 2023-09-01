7a28b4b45c6db38eb640601214ba240263479c35
Samuel <samuelveigarangel@hotmail.com>
2023-09-01 14:29:59 -0300

    Criado novas funcoes para lidar com os novos campos adicionados na coleta do article meta journal

--- journal/utils/journal_utils.py
+++ journal/utils/journal_utils.py
@@ -1,23 +1,33 @@
+import re
 from core.models import Language
 from institution.models import Institution
+from vocabulary.models import Vocabulary
+from ..models import (
+    Collection,
+    OfficialJournal,
+    Journal,
+    SciELOJournal,
+    Mission,
+    Sponsor,
+    SubjectDescriptor,
+    Subject,
+    Standard,
+    WebOfKnowledge,
+    WebOfKnowledgeSubjectCategory,
+)
 
-from ..models import Collection, OfficialJournal, Journal, SciELOJournal, Mission, Sponsor
 
-
-def create_or_update_scielo_journal(
+def create_or_update_journal(
     title,
     issn_scielo,
     short_title,
+    other_titles,
     submission_online_url,
     open_access,
     issn_print_or_electronic,
     type_issn,
     current_issn,
-    collection,
-    mission,
-    sponsor,
     user,
-    journal_acron=None,
 ):
     title = extract_value(title)
     issnl = extract_value(issn_scielo)
@@ -37,6 +47,13 @@ def create_or_update_scielo_journal(
         submission_online_url=extract_value(submission_online_url),
         open_access=extract_value(open_access),
     )
+    return journal
+
+
+def create_or_update_scielo_journal(
+    journal, collection, issn_scielo, journal_acron, user
+):
+    issnl = extract_value(issn_scielo)
     scielo_journal = SciELOJournal.create_or_update(
         user=user,
         collection=get_collection(collection),
@@ -44,8 +61,86 @@ def create_or_update_scielo_journal(
         journal_acron=extract_value(journal_acron),
         journal=journal,
     )
-    get_or_create_mission(mission=mission, journal=journal)
-    get_or_create_sponso(sponsor=sponsor, journal=journal)    
+    return scielo_journal
+
+
+def create_scope(
+    journal,
+    mission,
+    sponsor,
+    subject_descriptors,
+    subject,
+    wos_scie,
+    wos_ssci,
+    wos_ahci,
+    wos_areas,
+    user,
+):
+    get_or_create_mission(journal=journal, mission=mission, user=user)
+    get_or_create_sponso(journal=journal, sponsor=sponsor, user=user)
+    get_or_create_subject_descriptor(
+        journal=journal,
+        subject_descriptors=subject_descriptors,
+        user=user,
+    )
+    create_or_update_subject(journal=journal, subject=subject, user=subject)
+    create_or_update_wos_db(
+        journal=journal,
+        wos_scie=wos_scie,
+        wos_ssci=wos_ssci,
+        wos_ahci=wos_ahci,
+        user=user,
+    )
+    get_or_update_wos_areas(journal=journal, wos_areas=wos_areas, user=user)
+
+
+def create_interoperation(
+    journal, indexed_at, secs_code, medline_code, medline_short_title, user
+):
+    journal.secs_code = extract_value(secs_code)
+    journal.medline_code = extract_value(medline_code)
+    journal.medline_short_title = extract_value(medline_short_title)
+
+
+def create_information(
+    journal,
+    frequency,
+    publishing_model,
+    text_language,
+    abstract_language,
+    standard,
+    vocabulary,
+    alphabet,
+    classification,
+    national_code,
+    type_of_literature,
+    treatment_level,
+    level_of_publication,
+    user,
+):
+    create_or_update_journal_languages(
+        journal=journal,
+        language_data=text_language,
+        language_type="text",
+        user=user,
+    )
+    create_or_update_journal_languages(
+        journal=journal,
+        language_data=abstract_language,
+        language_type="abstract",
+        user=user,
+    )
+    get_or_create_vocabulary(vocabulary=vocabulary, journal=journal, user=user)
+    create_or_update_standard(standard=standard, journal=journal, user=user)
+    journal.frequency = extract_value(frequency)
+    journal.publishing_model = extract_value(publishing_model)
+    journal.alphabet = extract_value(alphabet)
+    journal.classification = extract_value(classification)
+    journal.national_code = extract_value(national_code)
+    journal.type_of_literature = extract_value(type_of_literature)
+    journal.treatment_level = extract_value(treatment_level)
+    if len(extract_value(level_of_publication)) < 3:
+        journal.level_of_publication = extract_value(level_of_publication)
 
 
 def create_or_update_official_journal(
@@ -91,6 +186,7 @@ def get_collection(collection):
         return None
 
 
+# colocar em outro arquivo
 def extract_issn_print_electronic(issn_print_or_electronic):
     issn_print = None
     issn_electronic = None
@@ -104,11 +200,15 @@ def extract_issn_print_electronic(issn_print_or_electronic):
     return issn_print, issn_electronic
 
 
+# colocar em outro arquivo
 def extract_value(value):
     if value and isinstance(value, list):
+        if len(value) > 1:
+            return [x.get("_") for x in value]
         return [x.get("_") for x in value][0]
 
 
+# colocar em outro arquivo
 def extract_value_mission(mission):
     """
     [
@@ -130,7 +230,7 @@ def extract_value_mission(mission):
     return [{"lang": x.get("l"), "mission": x.get("_")} for x in mission]
 
 
-def get_or_create_mission(mission, journal):
+def get_or_create_mission(mission, journal, user):
     if mission and isinstance(mission, list):
         missions = extract_value_mission(mission=mission)
         for m in missions:
@@ -138,20 +238,21 @@ def get_or_create_mission(mission, journal):
                 journal=journal,
                 rich_text=m.get("mission"),
                 language=Language.get_or_create(code2=m.get("lang")),
+                creator=user,
             )
 
 
-def get_or_create_sponso(sponsor, journal):
+def get_or_create_sponso(sponsor, journal, user):
     if sponsor and isinstance(sponsor, list):
         for s in sponsor:
             name = extract_value([s])
             ## FIXME
             ## Sponso de diferentes formas (insta_name e insta_acronym)
-            ## Ex: 
+            ## Ex:
             ## CNPq
             ## Instituto Nacional de Estudos e Pesquisas Educacionais Anísio Teixeira
             ## Fundação Getulio Vargas/ Escola de Administração de Empresas de São Paulo
-            ## CNPq - Conselho Nacional de Desenvolvimento Científico e Tecnológico (PIEB) 
+            ## CNPq - Conselho Nacional de Desenvolvimento Científico e Tecnológico (PIEB)
             obj, created = Sponsor.objects.get_or_create(
                 page=journal,
                 institution=Institution.get_or_create(
@@ -163,5 +264,97 @@ def get_or_create_sponso(sponsor, journal):
                     location=None,
                     official=None,
                     is_official=None,
-                )
+                    user=user,
+                ),
             )
+
+
+def get_or_create_subject_descriptor(subject_descriptors, journal, user):
+    data = []
+    if subject_descriptors:
+        sub_desc = extract_value(subject_descriptors)
+        if isinstance(sub_desc, str):
+            sub_desc = [sub_desc]
+        for s in sub_desc:
+            for word in re.split(",|;", s):
+                word = word.strip()
+                obj, created = SubjectDescriptor.objects.get_or_create(
+                    value=word,
+                    creator=user,
+                )
+                data.append(obj)
+        journal.subject_descriptor.set(data)
+
+
+def create_or_update_subject(subject, journal, user):
+    data = []
+    if subject:
+        sub = extract_value(subject)
+        if isinstance(sub, str):
+            sub = [sub]
+        for s in sub:
+            obj = Subject.create_or_update(code=s, user=user)
+            data.append(obj)
+        journal.subject.set(data)
+
+
+def create_or_update_journal_languages(language_data, journal, language_type, user):
+    data = []
+    if language_type:
+        langs = extract_value(language_data)
+        if isinstance(langs, str):
+            langs = [langs]
+        for l in langs:
+            obj = Language.get_or_create(code2=l, creator=user)
+            data.append(obj)
+
+        if language_type == "text":
+            journal.text_language.set(data)
+        elif language_type == "abstract":
+            journal.abstract_language.set(data)
+
+
+def get_or_create_vocabulary(vocabulary, journal, user):
+    if vocabulary:
+        v = extract_value(vocabulary)
+        obj = Vocabulary.get_or_create(name=None, acronym=v, user=user)
+        journal.vocabulary = obj
+
+
+def create_or_update_standard(standard, journal, user):
+    if standard:
+        standard = extract_value(standard)
+        obj = Standard.create_or_update(
+            code=standard,
+            user=user,
+        )
+        journal.standard = obj
+
+
+def create_or_update_wos_db(journal, wos_scie, wos_ssci, wos_ahci, user):
+    data = []
+    for db in (wos_scie, wos_ssci, wos_ahci):
+        wosdb = extract_value(db)
+        if wosdb:
+            obj = WebOfKnowledge.create_or_update(
+                code=wosdb,
+                user=user,
+            )
+            data.append(obj)
+    journal.wos_db.set(data)
+
+
+def get_or_update_wos_areas(journal, wos_areas, user):
+    data = []
+    if wos_areas:
+        areas = extract_value(wos_areas)
+        if isinstance(areas, str):
+            areas = [areas]
+        for a in areas:
+            for word in re.split(",|;", a):
+                obj, created = WebOfKnowledgeSubjectCategory.objects.get_or_create(
+                    value=word,
+                    creator=user,
+                )
+                data.append(obj)
+        journal.wos_area.set(data)
