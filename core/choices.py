c3e9dd2464f92d6514850aa7fd2968e17404ff31
Samuel <samuelveigarangel@hotmail.com>
2023-09-08 15:48:44 -0300

    Insere choices MONTHS

--- core/choices.py
+++ core/choices.py
@@ -1,3 +1,5 @@
+from django.utils.translation import gettext_lazy as _
+
 LANGUAGE = [
     ("aa", "Afar"),
     ("af", "Afrikaans"),
@@ -184,4 +186,19 @@ LANGUAGE = [
     ("yo", "Yoruba"),
     ("za", "Zhuang, Chuang"),
     ("zu", "Zulu"),
+]
+
+MONTHS = [
+    (1, _("January")),
+    (2, _("February")),
+    (3, _("March")),
+    (4, _("April")),
+    (5, _("May")),
+    (6, _("June")),
+    (7, _("July")),
+    (8, _("August")),
+    (9, _("September")),
+    (10, _("October")),
+    (11, _("November")),
+    (12, _("December")),
 ]
