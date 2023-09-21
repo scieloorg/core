019cf49b8bc9dfc586087df9f6439f4198ba98fe
Samuel <samuelveigarangel@hotmail.com>
2023-09-20 23:01:08 -0300

    Criado novas funcoes de acordo com os novos panels no modelo Journal.

--- journal/utils/journal_utils.py
+++ journal/utils/journal_utils.py
@@ -1,33 +1,44 @@
 import re
+
 from django.db.models import Q
-from core.models import Language
+from core.models import Language, License
 from institution.models import Institution
 from vocabulary.models import Vocabulary
 from reference.models import JournalTitle
-from .funcs_extract_am import extract_issn_print_electronic, extract_value, extract_value_mission
-from ..models import (
+from .funcs_extract_am import (
+    extract_issn_print_electronic,
+    extract_value,
+    extract_value_mission,
+)
+from journal.models import (
     Collection,
     OfficialJournal,
     Journal,
     SciELOJournal,
     Mission,
+    Publisher,
+    Owner,
+    Copyright,
+    Annotation,
     Sponsor,
     SubjectDescriptor,
     Subject,
     Standard,
     WebOfKnowledge,
     WebOfKnowledgeSubjectCategory,
-    IndexedAt
+    IndexedAt,
 )
+from location.models import City, Region, Location, State, Country, Address
+
+from datetime import datetime
 
 
 def create_or_update_journal(
     title,
-    issn_scielo,
     short_title,
     other_titles,
     submission_online_url,
-    open_access,
+    issn_scielo,
     issn_print_or_electronic,
     type_issn,
     current_issn,
@@ -50,26 +61,27 @@ def create_or_update_journal(
         short_title=extract_value(short_title),
         other_titles=other_titles,
         submission_online_url=extract_value(submission_online_url),
-        open_access=extract_value(open_access),
     )
     return journal
 
 
 def create_or_update_scielo_journal(
-    journal, collection, issn_scielo, journal_acron, user
+    journal, collection, issn_scielo, journal_acron, status, user
 ):
     issnl = extract_value(issn_scielo)
+    code_status = extract_value(status)
     scielo_journal = SciELOJournal.create_or_update(
         user=user,
         collection=get_collection(collection),
         issn_scielo=issnl,
         journal_acron=extract_value(journal_acron),
         journal=journal,
+        code_status=code_status,
     )
     return scielo_journal
 
 
-def create_scope(
+def update_panel_scope_and_about(
     journal,
     mission,
     sponsor,
@@ -82,7 +94,7 @@ def create_scope(
     user,
 ):
     get_or_create_mission(journal=journal, mission=mission, user=user)
-    get_or_create_sponso(journal=journal, sponsor=sponsor, user=user)
+    get_or_create_sponsor(journal=journal, sponsor=sponsor, user=user)
     get_or_create_subject_descriptor(
         journal=journal,
         subject_descriptors=subject_descriptors,
@@ -99,7 +111,7 @@ def create_scope(
     get_or_update_wos_areas(journal=journal, wos_areas=wos_areas, user=user)
 
 
-def create_interoperation(
+def update_panel_interoperation(
     journal, indexed_at, secs_code, medline_code, medline_short_title, user
 ):
     get_or_create_indexed_at(journal, indexed_at=indexed_at, user=user)
@@ -108,7 +120,7 @@ def create_interoperation(
     journal.medline_short_title = extract_value(medline_short_title)
 
 
-def create_information(
+def update_panel_information(
     journal,
     frequency,
     publishing_model,
@@ -147,6 +159,134 @@ def create_information(
     journal.treatment_level = extract_value(treatment_level)
     if len(extract_value(level_of_publication)) < 3:
         journal.level_of_publication = extract_value(level_of_publication)
+
+
+def update_panel_institution(
+    journal,
+    publisher,
+    copyright_holder,
+    address,
+    electronic_address,
+    publisher_country,
+    publisher_state,
+    publisher_city,
+    user,
+):
+    location = create_or_update_location(
+    address,    
+    publisher_country,
+    publisher_state,
+    publisher_city,
+    user,
+    )
+    url = extract_value(electronic_address)
+    copyright_holder = extract_value(copyright_holder)
+    publisher = extract_value(publisher)
+    
+    if isinstance(publisher, str):
+        publisher = [publisher]
+    
+    if publisher:
+        for p in publisher:
+            if p:
+                institution = Institution.create_or_update(
+                    inst_name=p,
+                    user=user,
+                    location=location,
+                    url=url,
+                    copyright_holder=copyright_holder,
+                    inst_acronym=None,
+                    level_1=None,
+                    level_2=None,
+                    level_3=None,
+                    official=None,
+                    is_official=None,
+                )
+                publisher = Publisher.objects.get_or_create(
+                    page=journal,
+                    institution=institution,
+                )
+                Owner.objects.get_or_create(
+                    page=journal,
+                    institution=institution,
+                )
+
+
+def update_panel_website(
+    journal,
+    url_of_the_journal,
+    url_of_submission_online,
+    url_of_the_main_collection,
+    license_of_use,
+    user
+):
+    
+    journal.url_of_journal = extract_value(url_of_the_journal)
+    journal.collection_main_url = extract_value(url_of_the_main_collection)
+    journal.submission_online_url = extract_value(url_of_submission_online)
+    license_type = extract_value(license_of_use)
+    if license_type:
+        license = License.create_or_update(
+            license_type=license_type,
+            user=user 
+        )
+        journal.use_license = license
+
+def update_panel_notes(
+    journal,
+    notes,
+    creation_date,
+    update_date,
+    user,
+):
+    """
+        Ex notes:
+            [{'_': 'Editor:'}, {'_': 'Denis Coitinho Silveira'}, {'_': 'Rua Lobo da Costa, 270/501'}, {'_': '90050-110'}, {'_': 'UNISINOS - Universidade do Vale do Rio dos Sinos'}, {'_': 'Porto Alegre'}, {'_': 'RS'}, {'_': 'Brasil'}, {'_': '51 32269513; 51 983107257'}, {'_': 'deniscoitinhosilveira@gmail.com'}]
+            [{'_': 'Iniciou no v12n1'}]
+    """
+    notes = extract_value(notes)
+    if notes:
+        creation_date = datetime.strptime(extract_value(creation_date), "%Y%m%d")
+        update_date = datetime.strptime(extract_value(update_date), "%Y%m%d")
+        if isinstance(notes, str):
+            notes = [notes]
+        n = '\n'.join(notes)
+        obj, created = Annotation.objects.get_or_create(
+            journal=journal,
+            notes=n,
+            creation_date=creation_date,
+            update_date=update_date,
+            updated_by=user,
+        )
+        if created:
+            obj.creator = user
+            obj.save()
+
+
+def update_panel_legacy_compatibility_fields(
+    journal,
+    center_code,
+    identification_number,
+    ftp,
+    user_subscription,
+    subtitle,
+    section,
+    has_supplement,
+    is_supplement,
+    acronym_letters,
+):
+    user_subs = extract_value(user_subscription)
+    if user_subs and 2 <= len(user_subs) <= 3:
+        journal.user_subscription = user_subs
+    print(extract_value(center_code))
+    journal.center_code = extract_value(center_code)
+    journal.identification_number = extract_value(identification_number)
+    journal.ftp = extract_value(ftp)
+    journal.subtitle = extract_value(subtitle)
+    journal.section = extract_value(section)
+    journal.has_supplement = extract_value(has_supplement)
+    journal.is_supplement = extract_value(is_supplement)
+    journal.acronym_letters = extract_value(acronym_letters)
 
 
 def create_or_update_official_journal(
@@ -220,7 +360,7 @@ def get_or_create_mission(mission, journal, user):
             )
 
 
-def get_or_create_sponso(sponsor, journal, user):
+def get_or_create_sponsor(sponsor, journal, user):
     if sponsor and isinstance(sponsor, list):
         for s in sponsor:
             name = extract_value([s])
@@ -231,20 +371,23 @@ def get_or_create_sponso(sponsor, journal, user):
             ## Instituto Nacional de Estudos e Pesquisas Educacionais Anísio Teixeira
             ## Fundação Getulio Vargas/ Escola de Administração de Empresas de São Paulo
             ## CNPq - Conselho Nacional de Desenvolvimento Científico e Tecnológico (PIEB)
-            obj, created = Sponsor.objects.get_or_create(
-                page=journal,
-                institution=Institution.get_or_create(
-                    inst_name=name,
-                    inst_acronym=None,
-                    level_1=None,
-                    level_2=None,
-                    level_3=None,
-                    location=None,
-                    official=None,
-                    is_official=None,
-                    user=user,
-                ),
-            )
+            if name:
+                obj, created = Sponsor.objects.get_or_create(
+                    page=journal,
+                    institution=Institution.create_or_update(
+                        inst_name=name,
+                        copyright_holder=None,
+                        inst_acronym=None,
+                        level_1=None,
+                        level_2=None,
+                        level_3=None,
+                        location=None,
+                        official=None,
+                        is_official=None,
+                        url=None,
+                        user=user,
+                    ),
+                )
 
 
 def get_or_create_subject_descriptor(subject_descriptors, journal, user):
@@ -278,13 +421,14 @@ def create_or_update_subject(subject, journal, user):
 
 def create_or_update_journal_languages(language_data, journal, language_type, user):
     data = []
-    if language_type:
+    if language_data:
         langs = extract_value(language_data)
         if isinstance(langs, str):
             langs = [langs]
         for l in langs:
             obj = Language.get_or_create(code2=l, creator=user)
-            data.append(obj)
+            if obj:
+                data.append(obj)
 
         if language_type == "text":
             journal.text_language.set(data)
@@ -359,3 +503,70 @@ def get_or_create_indexed_at(journal, indexed_at, user):
                 )
             data.append(obj)
         journal.indexed_at.set(data)
+
+
+def create_or_update_location(
+    address,
+    publisher_country,
+    publisher_state,
+    publisher_city,
+    user,
+):
+    """
+    Exemplo de entradas de publisher_country, publisher_state:
+    publisher_state:
+        [{'_': 'Distrito Capital'}], [{'_': 'DF'}] e None
+    publisher_country:
+        [{'_': 'CO'}], [{'_': 'Colombia}] e None
+    address:
+        [{'_': 'Rua Felizardo, 750 Jardim Botânico'}, {'_': 'CEP: 90690-200'}, {'_': 'RS - Porto Alegre'}, {'_': '(51) 3308 5814'}]
+    """
+    country_value = extract_value(publisher_country)
+
+    if country_value is not None and len(country_value) >= 2:
+        country_name = country_value
+        country_acronym = None
+    else:
+        country_name = None
+        country_acronym = country_value
+    country = Country.get_or_create(
+        name=country_name,
+        acronym=country_acronym,
+        user=user,
+    )
+    city = City.get_or_create(
+        name=extract_value(publisher_city),
+        user=user,
+    )
+
+    state_value = extract_value(publisher_state)
+    if state_value is not None and len(state_value) >= 2:
+        state_name = state_value
+        state_acronym = None
+    else:
+        state_name = None
+        state_acronym = state_value
+    state = State.get_or_create(
+        name=state_name,
+        acronym=state_acronym,
+        user=user,
+    )
+    
+    address = extract_value(address)
+    if address:
+        if isinstance(address, str):
+            address = [address]
+        address = '\n'.join(address)
+    address = Address.get_or_create(
+        name=address,
+        user=user,
+    )
+    location = Location.get_or_create(
+        location_region=None,
+        location_country=country,
+        location_city=city,
+        location_state=state,
+        location_address=address,
+        user=user,
+    )
+    return location
