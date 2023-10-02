02bdba022bd62b28905023d25116f872e91155ea
eduranm <58487702+eduranm@users.noreply.github.com>
2023-10-02 06:42:44 -0600

    Procedimiento para carga de links Scimago (#354)
    * Agrega opción para recuperar país si sólo recibe acrónimo de 3 caracteres
    
    * Faltaba asignar los valores de cada registro a los atributos del modelo
    
    * archivo merge de migración
    
    * Agrega modelos Scimago y ScimagoFile
    
    * Funciones para validar e importar archivo csv a modelo de Scimago
    
    * Menús para Scimago y ScimagoFile
    
    * botones de validar e importar
    
    * Archivo forms para Scimago
    
    * validaciones de archivo csv a importar
    
    * archivo de migración para país en location
    
    * archivo de migración para scimago en institucion

---
+++ institution/button_helpers.py
@@ -0,0 +1,49 @@
+from django.urls import reverse
+from django.utils.translation import gettext as _
+from wagtail.contrib.modeladmin.helpers import ButtonHelper
+
+
+class ScimagoHelper(ButtonHelper):
+    validate_button_classnames = [
+        "button-small",
+        "icon",
+    ]
+    import_button_classnames = [
+        "button-small",
+        "icon",
+    ]
+
+    def validate_button(self, obj):
+        text = _("Validate")
+        return {
+            "url": reverse("validate_scimago")
+            + "?file_id="
+            + str(obj.id),
+            "label": text,
+            "classname": self.finalise_classname(self.validate_button_classnames),
+            "title": text,
+        }
+
+    def import_button(self, obj):
+        text = _("Import")
+        return {
+            "url": reverse("import_file_scimago")
+            + "?file_id="
+            + str(obj.id),
+            "label": text,
+            "classname": self.finalise_classname(self.import_button_classnames),
+            "title": text,
+        }
+
+    def get_buttons_for_obj(
+        self, obj, exclude=None, classnames_add=None, classnames_exclude=None
+    ):
+        btns = super().get_buttons_for_obj(
+            obj, exclude, classnames_add, classnames_exclude
+        )
+        if "validate" not in (exclude or []):
+            btns.append(self.validate_button(obj))
+        if "import" not in (exclude or []):
+            btns.append(self.import_button(obj))
+        return btns
+    
--- institution/views.py
+++ institution/views.py
@@ -1,3 +1,76 @@
-from django.shortcuts import render
+import csv
+import os
+from datetime import datetime
+
+from django.contrib.auth import get_user_model
+from django.http import Http404, HttpResponse
+from django.shortcuts import get_object_or_404, redirect
+from django.utils.translation import gettext as _
+from wagtail.admin import messages
+
+from core.libs import chkcsv
+
+from .models import (
+    Scimago,
+    ScimagoFile
+)
 
 # Create your views here.
+def validate_scimago(request):
+    errorlist = []
+    file_id = request.GET.get("file_id", None)
+
+    if file_id:
+        file_upload = get_object_or_404(ScimagoFile, pk=file_id)
+
+    if request.method == "GET":
+        try:
+            upload_path = file_upload.attachment.file.path
+            cols = chkcsv.read_format_specs(
+                os.path.dirname(os.path.abspath(__file__)) + "/scimago_chkcsvfmt.fmt",
+                True,
+                False,
+            )
+            errorlist = chkcsv.check_csv_file(
+                upload_path, cols, True, True, True, False
+            )
+            if errorlist:
+                raise Exception(_("Validation error"))
+            else:
+                file_upload.is_valid = True
+                fp = open(upload_path)
+                file_upload.line_count = len(fp.readlines())
+                file_upload.save()
+        except Exception as ex:
+            messages.error(request, _("Validation error: %s") % errorlist)
+        else:
+            messages.success(request, _("File successfully validated!"))
+
+    return redirect(request.META.get("HTTP_REFERER"))
+
+
+def import_file_scimago(request):
+    file_id = request.GET.get("file_id", None)
+
+    if file_id:
+        file_upload = get_object_or_404(ScimagoFile, pk=file_id)
+
+    file_path = file_upload.attachment.file.path
+
+    try:
+        with open(file_path, "r") as csvfile:
+            data = csv.DictReader(csvfile, delimiter=";")
+
+            for line, row in enumerate(data):
+                institution = row["Organization"]
+                acronym = row["Country"]
+                url = row["URL"]
+                s = Scimago()
+                s.create_or_update(request.user, institution, acronym, url)
+
+    except Exception as ex:
+        messages.error(request, _("Import error: %s, Line: %s") % (ex, str(line + 2)))
+    else:
+        messages.success(request, _("File imported successfully!"))
+
+    return redirect(request.META.get("HTTP_REFERER"))
