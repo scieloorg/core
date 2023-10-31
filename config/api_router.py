0c2fd4b7dcb607aee06543934ec8f75cba057a60
Samuel <samuelveigarangel@hotmail.com>
2023-10-31 10:04:22 -0300

    Adicionado rota pra journal

--- config/api_router.py
+++ config/api_router.py
@@ -4,6 +4,7 @@ from rest_framework.routers import DefaultRouter, SimpleRouter
 from article.api.v1.views import ArticleViewSet
 from issue.api.v1.views import IssueViewSet
 from pid_provider.api.v1.views import PidProviderViewSet
+from journal.api.v1.views import JournalViewSet, JournalIdentifierViewSet
 
 app_name = "pid_provider"
 
@@ -12,8 +13,10 @@ if settings.DEBUG:
 else:
     router = SimpleRouter()
 
-router.register("article", ArticleViewSet, basename="Article")
-router.register("issue", IssueViewSet, basename="Issue")
+router.register("article", ArticleViewSet, basename="article")
+router.register("issue", IssueViewSet, basename="issue")
 router.register("pid_provider", PidProviderViewSet, basename="pid_provider")
+router.register("journal/identifiers", JournalIdentifierViewSet, basename='journal_identifiers')
+router.register("journal", JournalViewSet, basename="journal_detail")
 
 urlpatterns = router.urls
