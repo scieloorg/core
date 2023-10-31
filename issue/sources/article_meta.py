2c726236a2ab001c5889967da7d657801cd8b793
Samuel <samuelveigarangel@hotmail.com>
2023-10-31 10:02:22 -0300

    Fix process_issue_article_meta

--- issue/sources/article_meta.py
+++ issue/sources/article_meta.py
@@ -13,8 +13,9 @@ def process_issue_article_meta(collection, limit, user):
             code = issue["code"]
             url_issue = f"https://articlemeta.scielo.org/api/v1/issue/?code={code}"
             data_issue = utils.fetch_data(url_issue, json=True, timeout=30, verify=True)
+            fields_issue = [data_issue["issue"]]
             issue_dict = rename_dictionary_keys(
-                data_issue["issue"], correspondencia_issue
+                fields_issue, correspondencia_issue
             )
             get_or_create_issue(
                 issn_scielo=issue_dict.get("scielo_issn"),
