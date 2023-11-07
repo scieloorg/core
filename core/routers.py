3386c07994768eb707b31c9d17185854be57b679
Roberta Takenaka <roberta.takenaka@scielo.org>
2023-11-07 08:17:04 -0300

    Aplica black e isort

--- core/routers.py
+++ core/routers.py
@@ -1,5 +1,6 @@
 from haystack import routers
 
+
 class UpdateEverythingRouter(routers.BaseRouter):
     def for_write(self, **hints):
-        return ('default', 'oai')
+        return ("default", "oai")
