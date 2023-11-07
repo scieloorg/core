634c413312705912d69eb4052d90b0981e84fb0e
Roberta Takenaka <505143+robertatakenaka@users.noreply.github.com>
2023-11-07 08:06:51 -0300

    Cria tarefa que configura o sistema (#396)
    * Cria a app bigbang que ajuda a configurar o sistema
    
    * Troca requests.get por fetch_data (Collection.load)

---
+++ bigbang/tasks.py
@@ -0,0 +1,24 @@
+import logging
+
+from django.contrib.auth import get_user_model
+from config import celery_app
+from collection.models import Collection
+
+User = get_user_model()
+
+
+def _get_user(user_id, username):
+    if user_id:
+        return User.objects.get(pk=user_id)
+    if username:
+        return User.objects.get(username=username)
+
+
+@celery_app.task(bind=True)
+def task_start(
+    self,
+    user_id=None,
+    username=None,
+):
+    user = _get_user(user_id, username)
+    Collection.load(user)
