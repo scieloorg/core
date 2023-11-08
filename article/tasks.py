55fe361f52f342724cce138136119949ec903370
Roberta Takenaka <roberta.takenaka@scielo.org>
2023-11-08 17:17:40 -0300

    Adiciona tratamento de exceção para ingressar artigos

--- article/tasks.py
+++ article/tasks.py
@@ -1,3 +1,5 @@
+import sys
+
 from django.contrib.auth import get_user_model
 from django.utils.translation import gettext as _
 
@@ -6,7 +8,7 @@ from article.sources import xmlsps
 from article.sources.preprint import harvest_preprints
 from config import celery_app
 from pid_provider.models import PidProviderXML
-
+from tracker.models import UnexpectedEvent
 from . import controller
 
 User = get_user_model()
@@ -26,11 +28,34 @@ def load_article(self, user_id, file_path=None, xml=None):
 
 @celery_app.task(bind=True, name=_("load_articles"))
 def load_articles(self, user_id=None):
-    from_date = Article.last_created_date()
+    try:
+        from_date = Article.last_created_date()
 
-    for item in PidProviderXML.public_items(from_date):
-        load_article.apply_async(args=(user_id,), kwargs={"xml": item.current_version.xml})
-
+        for item in PidProviderXML.public_items(from_date):
+            try:
+                load_article.apply_async(
+                    args=(user_id,),
+                    kwargs={"xml": item.current_version.xml}
+                )
+            except Exception as exception:
+                exc_type, exc_value, exc_traceback = sys.exc_info()
+                UnexpectedEvent.create(
+                    exception=exception,
+                    exc_traceback=exc_traceback,
+                    detail={
+                        "task": "article.tasks.load_articles",
+                        "item": str(item),
+                    }
+                )
+    except Exception as exception:
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        UnexpectedEvent.create(
+            exception=exception,
+            exc_traceback=exc_traceback,
+            detail={
+                "task": "article.tasks.load_articles",
+            }
+        )
 
 @celery_app.task(bind=True, name=_("load_preprints"))
 def load_preprint(self, user_id, oai_pmh_preprint_uri):
