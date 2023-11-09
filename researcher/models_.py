f06dc7f5235104d2b82ecd1f832062108e5fc96d
Roberta Takenaka <roberta.takenaka@scielo.org>
2023-11-08 22:41:43 -0300

    Move de researcher tudo relacionado com editorialboard que foi incluído em issue A pasta editorialboard não é uma aplicação ativa (não está na lista de apps instaladas) Esta mudança é para preservar o código gerado para carregar uma lista de histórico dos membros da equipe editorial, procedimento que não tinha sido migrado para a app issue. Esta movimentação foi necessária, pois estava criando uma dependeência circular que impedia de gerar as migrações

--- researcher/models_.py
+++ researcher/models_.py
@@ -193,3 +193,126 @@ class FieldEmail(Orderable):
 
 class FieldAffiliation(Orderable, InstitutionHistory):
     page = ParentalKey(Researcher, on_delete=models.CASCADE, related_name="affiliation")
+
+
+class EditorialBoardMember(models.Model):
+    journal = models.ForeignKey(
+        Journal, null=True, blank=True, related_name="+", on_delete=models.CASCADE
+    )
+    member = models.ForeignKey(
+        Researcher, null=True, blank=True, related_name="+", on_delete=models.CASCADE
+    )
+    role = models.CharField(
+        _("Role"), max_length=255, choices=choices.ROLE, null=False, blank=False
+    )
+    initial_year = models.CharField(max_length=4, blank=True, null=True)
+    initial_month = models.CharField(max_length=2, blank=True, null=True, choices=MONTHS)
+    final_year = models.CharField(max_length=4, blank=True, null=True)
+    final_month = models.CharField(max_length=2, blank=True, null=True, choices=MONTHS)
+
+    panels = [
+        AutocompletePanel("journal"),
+        AutocompletePanel("member"),
+        FieldPanel("role"),
+        FieldPanel("initial_year"),
+        FieldPanel("initial_month"),
+        FieldPanel("final_year"),
+        FieldPanel("final_month"),
+    ]
+
+    class Meta:
+        indexes = [
+            models.Index(
+                fields=[
+                    "journal",
+                ]
+            ),
+            models.Index(
+                fields=[
+                    "member",
+                ]
+            ),
+            models.Index(
+                fields=[
+                    "role",
+                ]
+            ),
+        ]
+
+    @classmethod
+    def get_or_create(
+        self,
+        journal,
+        role,
+        initial_year,
+        email,
+        institution_name,
+        given_names,
+        last_name,
+        suffix,
+        orcid,
+        lattes,
+        gender,
+        gender_identification_status,
+        user,
+    ):
+        try:
+            gender = Gender.objects.get(code=gender)
+        except Gender.DoesNotExist:
+            gender = None
+
+        researcher_get = Researcher.get_or_create(
+            given_names,
+            last_name,
+            suffix,
+            orcid,
+            lattes,
+            email,
+            institution_name,
+            gender,
+            gender_identification_status,
+            user,
+        )
+
+        try:
+            journal_get = Journal.objects.get(title=journal)
+            return EditorialBoardMember.objects.get(
+                journal=journal_get, member=researcher_get
+            )
+        except Journal.DoesNotExist as e:
+            # TODO fazer tratamento apropriado para periódico não registrado
+            raise e
+        except EditorialBoardMember.DoesNotExist:
+            editorial_board_member = EditorialBoardMember()
+            editorial_board_member.member = researcher_get
+            editorial_board_member.journal = journal_get
+            editorial_board_member.role = role
+            editorial_board_member.initial_year = initial_year
+            editorial_board_member.creator = user
+            editorial_board_member.save()
+            return editorial_board_member
+
+    def __str__(self):
+        return "%s (%s)" % (
+            self.member or "",
+            self.role or "",
+        )
+
+
+class EditorialBoardMemberFile(models.Model):
+    attachment = models.ForeignKey(
+        "wagtaildocs.Document",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+",
+    )
+    is_valid = models.BooleanField(_("Is valid?"), default=False, blank=True, null=True)
+    line_count = models.IntegerField(
+        _("Number of lines"), default=0, blank=True, null=True
+    )
+
+    def filename(self):
+        return os.path.basename(self.attachment.name)
+
+    panels = [FieldPanel("attachment")]
--- researcher/wagtail_hooks.py
+++ researcher/wagtail_hooks.py
@@ -1,17 +1,12 @@
 from django.http import HttpResponseRedirect
-from django.urls import path
 from django.utils.translation import gettext as _
 from wagtail.contrib.modeladmin.options import (
     ModelAdmin,
-    ModelAdminGroup,
     modeladmin_register,
 )
 from wagtail.contrib.modeladmin.views import CreateView
-from wagtail import hooks
 
-from .button_helper import EditorialBoardMemberHelper
-from .models import EditorialBoardMember, EditorialBoardMemberFile, Researcher
-from .views import import_file_ebm, validate_ebm
+from .models import Researcher
 
 
 class ResearcherCreateView(CreateView):
@@ -36,55 +31,4 @@ class ResearcherAdmin(ModelAdmin):
     )
 
 
-class EditorialBoardMemberAdmin(ModelAdmin):
-    model = EditorialBoardMember
-    menu_label = _("Editorial Board Member")
-    menu_icon = "folder"
-    menu_order = 200
-    add_to_settings_menu = False
-    exclude_from_explorer = False
-
-
-class EditorialBoardMemberFileAdmin(ModelAdmin):
-    model = EditorialBoardMemberFile
-    button_helper_class = EditorialBoardMemberHelper
-    menu_label = "Editorial Board Member Upload"
-    menu_icon = "folder"
-    menu_order = 200
-    add_to_settings_menu = False
-    exclude_from_explorer = False
-    list_display = ("attachment", "line_count", "is_valid")
-    list_filter = ("is_valid",)
-    search_fields = ("attachment",)
-
-
 modeladmin_register(ResearcherAdmin)
-
-
-class EditorialBoardMemberAdminGroup(ModelAdminGroup):
-    menu_label = "Editorial Board Member"
-    menu_icon = "folder-open-inverse"  # change as required
-    menu_order = 200  # will put in 3rd place (000 being 1st, 100 2nd)
-    items = (
-        EditorialBoardMemberAdmin,
-        EditorialBoardMemberFileAdmin,
-    )
-
-
-# modeladmin_register(EditorialBoardMemberAdminGroup)
-
-
-@hooks.register("register_admin_urls")
-def register_editorial_url():
-    return [
-        path(
-            "researcher/editorialboradmember/validate",
-            validate_ebm,
-            name="validate_ebm",
-        ),
-        path(
-            "researcher/editorialboradmember/import_file",
-            import_file_ebm,
-            name="import_file_ebm",
-        ),
-    ]
