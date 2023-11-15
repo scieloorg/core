794636ce805d5907405b0d861c548c2058eff82f
Roberta Takenaka <roberta.takenaka@scielo.org>
2023-11-15 10:18:22 -0300

    Modifica journal.models.JournalHistory

--- journal/models.py
+++ journal/models.py
@@ -1443,52 +1443,47 @@ class Annotation(CommonControlField):
         return annotation
 
 
-class JournalHistory(CommonControlField):
-    initial_year = models.CharField(
-        _("Initial Occurrence date year"), max_length=4, null=True, blank=True
+class JournalHistory(CommonControlField, Orderable):
+    scielo_journal = ParentalKey(
+        SciELOJournal, on_delete=models.SET_NULL, related_name="journal_history", null=True
     )
-    initial_month = models.CharField(
-        _("Initial Occurrence date month"),
+    
+    year = models.CharField(
+        _("Event year"), max_length=4, null=True, blank=True
+    )
+    month = models.CharField(
+        _("Event month"),
         max_length=2,
         choices=MONTHS,
         null=True,
         blank=True,
     )
-    initial_day = models.CharField(
-        _("Initial Occurrence date day"), max_length=2, null=True, blank=True
+    day = models.CharField(
+        _("Event day"), max_length=2, null=True, blank=True
     )
-    final_year = models.CharField(
-        _("Final Occurrence date year"), max_length=4, null=True, blank=True
-    )
-    final_month = models.CharField(
-        _("Final Occurrence date month"),
-        max_length=2,
-        choices=MONTHS,
+    
+    event_type = models.CharField(
+        _("Event type"),
         null=True,
         blank=True,
+        max_length=16,
+        choices=choices.JOURNAL_EVENT_TYPE,
     )
-    final_day = models.CharField(
-        _("Final Occurrence date day"), max_length=2, null=True, blank=True
-    )
-    occurrence_type = models.TextField(
-        _("Occurrence type"),
+    interruption_reason = models.CharField(
+        _("Indexing interruption reason"),
         null=True,
         blank=True,
-    )
+        max_length=16,
+        choices=choices.INDEXING_INTERRUPTION_REASON,
+    ) 
 
-    autocomplete_search_field = "occurrence_type"
-
-    def autocomplete_label(self):
-        return str(self)
-
+    base_form_class = CoreAdminModelForm
     panels = [
-        FieldPanel("initial_year"),
-        FieldPanel("initial_month"),
-        FieldPanel("initial_day"),
-        FieldPanel("final_year"),
-        FieldPanel("final_month"),
-        FieldPanel("final_day"),
-        FieldPanel("occurrence_type"),
+        FieldPanel("year"),
+        FieldPanel("month"),
+        FieldPanel("day"),
+        FieldPanel("event_type"),
+        FieldPanel("interruption_reason"),
     ]
 
     class Meta:
@@ -1497,7 +1492,7 @@ class JournalHistory(CommonControlField):
         indexes = [
             models.Index(
                 fields=[
-                    "occurrence_type",
+                    "event_type",
                 ]
             ),
         ]
@@ -1505,26 +1500,64 @@ class JournalHistory(CommonControlField):
     @property
     def data(self):
         d = {
-            "occurrence_type": self.occurrence_type,
-            "initial_year": self.initial_year,
-            "initial_month": self.initial_month,
-            "initial_day": self.initial_day,
-            "final_year": self.final_year,
-            "final_month": self.final_month,
-            "final_day": self.final_day,
+            "event_type": self.event_type,
+            "interruption_reason": self.interruption_reason,
+            "year": self.year,
+            "month": self.month,
+            "day": self.day,
         }
 
         return d
 
     def __str__(self):
-        return "%s from %s/%s/%s to %s/%s/%s" % (
-            self.occurrence_type,
-            self.initial_year,
-            self.initial_month,
-            self.initial_day or "00",
-            self.final_year,
-            self.final_month,
-            self.final_day or "00",
+        return "%s %s %s/%s/%s" % (
+            self.event_type,
+            self.interruption_reason,
+            self.year,
+            self.month,
+            self.day or "00",
         )
 
-    base_form_class = CoreAdminModelForm
+    @classmethod
+    def am_to_core(
+        cls,
+        scielo_journal, 
+        initial_year,
+        initial_month,
+        initial_day,
+        final_year,
+        final_month,
+        final_day,
+        event_type,
+        interruption_reason,
+    ):
+
+        reasons = {
+            None: "ceased",
+            "not-open-access": "not-open-access",
+            "suspended-by-committee": "by-committee",
+            "suspended-by-editor": "by-editor",
+        }
+        try:
+            obj = cls.objects.get(scielo_journal=scielo_journal, year=initial_year, month=initial_month, day=initial_day)
+        except cls.DoesNotExist:
+            obj = cls()
+            obj.scielo_journal = scielo_journal
+            obj.year = initial_year
+            obj.month = initial_month
+            obj.day = initial_day
+        obj.event_type = "ADMITTED"
+        obj.save()
+
+        if final_year and event_type:
+            try:
+                obj = cls.objects.get(scielo_journal=scielo_journal, year=final_year, month=final_month, day=final_day)
+            except cls.DoesNotExist:
+                obj = cls()
+                obj.scielo_journal = scielo_journal
+                obj.year = final_year
+                obj.month = final_month
+                obj.day = final_day
+            obj.event_type = "INTERRUPTED"
+            obj.interruption_reason = reasons.get(interruption_reason)
+            obj.save()
