db8031cfab9aac5e17835d6997c643b586c80761
Roberta Takenaka <roberta.takenaka@scielo.org>
2023-11-21 17:59:45 -0300

    Faz ajustes para atender os novos testes relacionados à identificação nome e acrônimo de instituições

--- core/utils/standardizer.py
+++ core/utils/standardizer.py
@@ -16,25 +16,30 @@ def get_splitted_text(text):
 
     text_ = text.replace(" - ", "/").replace("- ", "/").replace(" -", "/")
     text_ = text_.replace(". ", "/")
+    text_ = remove_extra_spaces(text_)
 
     separators = get_separators(text_)
 
-    # padroniza a quantidade de espaços
-    text_ = " ".join([item.strip() for item in text_.split() if item.strip()])
     for sep in separators:
         text_ = text_.replace(sep, "#####")
 
     return [item.strip() for item in text_.split("#####") if item.strip()]
 
 
+def remove_extra_spaces(text):
+    # padroniza a quantidade de espaços
+    return " ".join([item.strip() for item in text.split() if item.strip()])
+
+
 def standardize_acronym_and_name(
     original, possible_multiple_return=None, q_locations=None
 ):
     """
-    Dado o texto original, identifica pares de acrônimo e nome.
-    Os separadores podem separar acrônimo e nome e/ou itens de lista.
-    Ex.: USP / Unicamp
-    São Paulo/SP, Rio de Janeiro/RJ
+    Dado o texto original, identifica pares de acrônimo e nome,
+    ou lista de acrônimos ou lista de nomes.
+    Retorna um ou mais itens dependendo do valor de q_locations que
+    deve ser correspondente à quantidade de itens identificados,
+    caso contrário, retornará `{"name": original}
 
     Parameters
     ----------
@@ -60,7 +65,7 @@ def standardize_acronym_and_name(
             else:
                 acrons.append(value)
 
-    if possible_multiple_return:
+    if possible_multiple_return and q_locations and q_locations > 1:
         yield from standardize_acronym_and_name_multiple(
             splitted_text,
             acrons,
@@ -68,39 +73,30 @@ def standardize_acronym_and_name(
             original,
             q_locations,
         )
-    yield standardize_acronym_and_name_one(splitted_text, acrons, names)
+    else:
+        yield standardize_acronym_and_name_one(splitted_text, acrons, names, original)
 
 
-def standardize_acronym_and_name_one(splitted_text, acrons, names):
+def standardize_acronym_and_name_one(splitted_text, acrons, names, original):
     """
-    Dado o texto original, identifica pares de acrônimo e nome.
-    Os separadores podem separar acrônimo e nome e/ou itens de lista.
-    Ex.: USP / Unicamp
-    São Paulo/SP, Rio de Janeiro/RJ
-
+    Retorna um par acron e name ou somente um name ou somente um acron,
+    caso contrário retorna `{"name": original}`
     """
-
+    original = remove_extra_spaces(original)
     if acrons and not names:
-        for acron in acrons:
-            return {"acronym": acron}
+        if len(acrons) > 1:
+            return {"name": original}
 
     if names and not acrons:
-        for name in names:
-            return {"name": name}
+        if len(names) > 1:
+            return name_and_divisions(splitted_text)
 
-    if len(names) == len(acrons):
-        match = None
+    if len(names) == len(acrons) == 1:
         for acron, name in zip(acrons, names):
             if name.startswith(acron[0]):
-                match = True
-            else:
-                match = False
-                break
-        if match:
-            for acron, name in zip(acrons, names):
                 return {"acronym": acron, "name": name}
-        else:
-            return name_and_divisions(splitted_text)
+            else:
+                return {"name": original}
 
     if len(acrons) == 1 and len(names) > 1:
         acron = acrons[0]
@@ -109,9 +105,10 @@ def standardize_acronym_and_name_one(splitted_text, acrons, names):
             splitted_text.remove(acron)
             d.update(name_and_divisions(splitted_text))
             return d
-
+        else:
+            return {"name": original}
     # retorna o original
-    return name_and_divisions(splitted_text)
+    return {"name": original}
 
 
 def standardize_acronym_and_name_multiple(
@@ -122,69 +119,34 @@ def standardize_acronym_and_name_multiple(
     q_locations,
 ):
     """
-    Dado o texto original, identifica pares de acrônimo e nome.
-    Os separadores podem separar acrônimo e nome e/ou itens de lista.
-    Ex.: USP / Unicamp
-    São Paulo/SP, Rio de Janeiro/RJ
-
-    Parameters
-    ----------
-    possible_multiple_return : boolean
-    q_locations : int
-        indica se é esperado 1 instituição ou várias
-
+    Retorna os pares acron e name ou somente names ou somente acrons,
+    mas somente se a quantidade está coerente com q_locations,
+    caso contrário retorna `{"name": original}`
     """
+    original = remove_extra_spaces(original)
+
     if acrons and not names:
-        for acron in acrons:
-            yield {"acronym": acron}
+        if q_locations == len(acrons):
+            for acron in acrons:
+                yield {"acronym": acron}
+        return
 
     if names and not acrons:
-        for name in names:
-            yield {"name": name}
+        if q_locations == len(names):
+            for name in names:
+                yield {"name": name}
+        return
 
-    if len(names) == len(acrons):
-        match = None
+    match = False
+    if len(names) == len(acrons) == q_locations:
         for acron, name in zip(acrons, names):
-            if name.startswith(acron[0]):
-                match = True
-            else:
-                match = False
-                break
-        if match:
-            for acron, name in zip(acrons, names):
-                yield {"acronym": acron, "name": name}
-        else:
-            if q_locations == 1:
-                yield name_and_divisions(splitted_text)
-            else:
-                for item in splitted_text:
-                    yield {"name": item}
+            yield {"acronym": acron, "name": name}
 
-    if len(acrons) == 1 and len(names) > 1:
-        acron = acrons[0]
-        if names[0].startswith(acron[0]):
-
-            if q_locations == 1:
-                d = {"acronym": acron}
-                splitted_text.remove(acron)
-                d.update(name_and_divisions(splitted_text))
-                yield d
-            else:
-                yield {"acronym": acron, "name": names[0]}
-                for item in names[1:]:
-                    yield {"name": item}
-        else:
-            if q_locations == 1:
-                yield name_and_divisions(splitted_text)
-            else:
-                for item in splitted_text:
-                    yield {"name": item}
+    elif q_locations == len(splitted_text):
+        for item in splitted_text:
+            yield {"name": item}
     else:
-        if q_locations == 1:
-            yield name_and_divisions(splitted_text)
-        else:
-            for item in splitted_text:
-                yield {"name": item}
+        yield {"name": original}
 
 
 def name_and_divisions(splitted_text):
@@ -202,6 +164,8 @@ def standardize_code_and_name(original):
     Ex.: USP / Unicamp
     São Paulo/SP, Rio de Janeiro/RJ
     """
+    original = remove_extra_spaces(original)
+
     splitted_text = get_splitted_text(original)
     codes = []
     names = []
