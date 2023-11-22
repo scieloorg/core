20252607890568dcac373a23516006e92150ef1e
Samuel <samuelveigarangel@hotmail.com>
2023-11-22 17:34:41 -0300

    Organiza argumentos do script

--- report/scripts/load_report_csv.py
+++ report/scripts/load_report_csv.py
@@ -1,12 +1,12 @@
-from report.tasks import load
+from report.tasks import report_csv_generator
 
 def run(username, type_report, year, issn_scielo):
-    load.apply_async(
+    report_csv_generator.apply_async(
         kwargs={
             "username": username, 
-            "year": year, 
-            "type_report": type_report,
             "issn_scielo": issn_scielo,
+            "type_report": type_report,
+            "year": year, 
         }
     )
     
