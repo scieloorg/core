e7a41923d3cb3abb322759836a27109c780af542
Roberta Takenaka <roberta.takenaka@scielo.org>
2023-12-07 14:07:58 -0300

    Adiciona em research.wagtail, ResearcherIdentifierAdmin e PersonNameAdmin

--- researcher/wagtail_hooks.py
+++ researcher/wagtail_hooks.py
@@ -1,9 +1,9 @@
 from django.http import HttpResponseRedirect
 from django.utils.translation import gettext as _
-from wagtail.contrib.modeladmin.options import ModelAdmin, modeladmin_register
+from wagtail.contrib.modeladmin.options import ModelAdmin, modeladmin_register, ModelAdminGroup
 from wagtail.contrib.modeladmin.views import CreateView
 
-from .models import Researcher
+from .models import Researcher, ResearcherIdentifier, PersonName
 
 
 class ResearcherCreateView(CreateView):
@@ -28,4 +28,66 @@ class ResearcherAdmin(ModelAdmin):
     )
 
 
-modeladmin_register(ResearcherAdmin)
+class ResearcherIdentifierCreateView(CreateView):
+    def form_valid(self, form):
+        self.object = form.save_all(self.request.user)
+        return HttpResponseRedirect(self.get_success_url())
+
+
+class ResearcherIdentifierAdmin(ModelAdmin):
+    model = ResearcherIdentifier
+    create_view_class = ResearcherIdentifierCreateView
+    menu_label = _("Researcher Identifier")
+    menu_icon = "folder"
+    menu_order = 9
+    add_to_settings_menu = False
+    exclude_from_explorer = False
+    list_display = (
+        "identifier",
+        "source_name",
+        "created",
+        "updated",
+    )
+    list_filter = ("source_name",)
+    search_fields = ("identifier",)
+
+
+class PersonNameCreateView(CreateView):
+    def form_valid(self, form):
+        self.object = form.save_all(self.request.user)
+        return HttpResponseRedirect(self.get_success_url())
+
+
+class PersonNameAdmin(ModelAdmin):
+    model = PersonName
+    create_view_class = PersonNameCreateView
+    menu_label = _("PersonName")
+    menu_icon = "folder"
+    menu_order = 9
+    add_to_settings_menu = False
+    exclude_from_explorer = False
+    list_display = (
+        "fullname",
+        "given_names",
+        "last_name",
+        "suffix",
+        "prefix",
+        "created",
+        "updated",
+    )
+    list_filter = ("suffix", "prefix", "gender")
+    search_fields = ("fullname",)
+
+
+class ResearcherAdminGroup(ModelAdminGroup):
+    menu_label = _("Researchers")
+    menu_icon = "folder-open-inverse"  # change as required
+    menu_order = 7
+    items = (
+        ResearcherIdentifierAdmin,
+        ResearcherAdmin,
+        PersonNameAdmin,
+    )
+
+
+modeladmin_register(ResearcherAdminGroup)
