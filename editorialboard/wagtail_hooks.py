feea8f4c1407210a8889f8926a66cf52d93cfbce
Roberta Takenaka <roberta.takenaka@scielo.org>
2023-12-08 14:51:01 -0300

    Remove Researcher*

--- editorialboard/wagtail_hooks.py
+++ editorialboard/wagtail_hooks.py
@@ -10,30 +10,8 @@ from wagtail.contrib.modeladmin.options import (
 from wagtail.contrib.modeladmin.views import CreateView
 
 from .button_helper import EditorialBoardMemberHelper
-from .models import EditorialBoardMember, EditorialBoardMemberFile, Researcher
+from .models import EditorialBoardMember, EditorialBoardMemberFile
 from .views import import_file_ebm, validate_ebm
-
-
-class ResearcherCreateView(CreateView):
-    def form_valid(self, form):
-        self.object = form.save_all(self.request.user)
-        return HttpResponseRedirect(self.get_success_url())
-
-
-class ResearcherAdmin(ModelAdmin):
-    model = Researcher
-    create_view_class = ResearcherCreateView
-    menu_label = _("Researcher")
-    menu_icon = "folder"
-    menu_order = 9
-    add_to_settings_menu = False
-    exclude_from_explorer = False
-    search_fields = (
-        "given_names",
-        "last_name",
-        "declared_name",
-        "orcid",
-    )
 
 
 class EditorialBoardMemberAdmin(ModelAdmin):
