28cb06819f3279162bb71521a6e6bcacb2cf20fe
Roberta Takenaka <roberta.takenaka@scielo.org>
2023-12-10 08:23:01 -0300

    Adiciona à tarefa de inicialização a carga de RoleModel.load para carregar os papéis do corpo editorial

--- bigbang/tasks.py
+++ bigbang/tasks.py
@@ -4,6 +4,7 @@ from django.contrib.auth import get_user_model
 from config import celery_app
 from collection.models import Collection
 from core.models import Language
+from editorialboard.models import RoleModel
 from institution.models import Institution
 from location.models import Country, City, State
 from journal.models import Standard, Subject, WebOfKnowledge, WebOfKnowledgeSubjectCategory
@@ -39,3 +40,4 @@ def task_start(
     ThematicArea.load(user)
     WebOfKnowledgeSubjectCategory.load(user)
     Institution.load(user)
+    RoleModel.load(user)
