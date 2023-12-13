ba00706847a40a5faa2d4e4a8b69571e8850774e
Roberta Takenaka <roberta.takenaka@scielo.org>
2023-12-13 20:49:38 -0300

    Cria os editorialboard.forms

--- editorialboard/forms.py
+++ editorialboard/forms.py
@@ -1,7 +1,10 @@
+import logging
+
+# from wagtail.admin.forms import WagtailAdminModelForm
 from wagtail.admin.forms import WagtailAdminModelForm
 
 
-class EditorialboardForm(WagtailAdminModelForm):
+class EditorialBoardForm(WagtailAdminModelForm):
     def save_all(self, user):
         inst = super().save(commit=False)
 
@@ -15,7 +18,36 @@ class EditorialboardForm(WagtailAdminModelForm):
         return inst
 
 
-class EditorialboardRoleForm(WagtailAdminModelForm):
+class EditorialBoardRoleForm(WagtailAdminModelForm):
+    def save_all(self, user):
+        inst = super().save(commit=False)
+
+        if self.instance.pk is not None:
+            inst.updated_by = user
+        else:
+            inst.creator = user
+
+        self.save()
+
+        return inst
+
+
+class EditorialBoardMemberForm(WagtailAdminModelForm):
+    def save_all(self, user):
+        inst = super().save(commit=False)
+
+        if self.instance.pk is not None:
+            inst.updated_by = user
+        else:
+            inst.creator = user
+        # FIXME it is not called
+        self.instance.update_editorial_board(user)
+
+        self.save()
+        return inst
+
+
+class EditorialBoardMemberActivityForm(WagtailAdminModelForm):
     def save_all(self, user):
         inst = super().save(commit=False)
 
