6f6826308e46ce3c8239d3ffe52135e106f21f65
Samuel Veiga Rangel <samuelveigarangel@hotmail.com>
2024-01-15 20:42:52 -0300

    Adiciona view e js para personalizar o campo journal

--- core/static/admin/js/custom.js
+++ core/static/admin/js/custom.js
@@ -1,1 +1,66 @@
 /* your custom js go here */
+document.addEventListener('DOMContentLoaded', function() {
+    const collectionSelect = document.querySelector('select[name="collection"]');
+    const journalSelect = document.querySelector('select[name="journal"]');
+    const currentJournalId = journalSelect.value; // Armazena o valor atual do journal
+
+    function updateJournals() {
+        const selectedCollections = Array.from(collectionSelect.options)
+                                    .filter(option => option.selected)
+                                    .map(option => option.value);
+        fetch(`/filter_journals/?collections[]=${selectedCollections.join('&collections[]=')}`)
+            .then(response => response.json())
+            .then(data => {
+                journalSelect.innerHTML = '';
+                const defaultOption = new Option("Selecione um Journal", "");
+                journalSelect.add(defaultOption);
+
+                data.forEach(function(journal) {
+                    const option = new Option(journal.name, journal.id);
+                    if (journal.id.toString() === currentJournalId) {
+                        option.selected = true;
+                    }
+                    journalSelect.add(option);
+                });
+                if (!journalSelect.querySelector(`option[value="${currentJournalId}"]`)) {
+                    journalSelect.value = ""; 
+                }
+            })
+            .catch(error => console.error('Error:', error));
+    }
+    
+    if (collectionSelect && journalSelect) {
+        collectionSelect.addEventListener('change', updateJournals);
+        updateJournals();
+    }
+});
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     const collectionSelect = document.querySelector('select[name="collection"]');
+//     const journalSelect = document.querySelector('select[name="journal"]');
+
+//     function updateJournals() {
+//         const selectedCollections = Array.from(collectionSelect.options)
+//                                     .filter(option => option.selected)
+//                                     .map(option => option.value);
+//         fetch(`/filter_journals/?collections[]=${selectedCollections.join('&collections[]=')}`)
+//             .then(response => response.json())
+//             .then(data => {
+//                 journalSelect.innerHTML = '';
+//                 // Adicione uma opção vazia ou com texto de 'nenhuma seleção'
+//                 const defaultOption = new Option("Selecione um Journal", "");
+//                 journalSelect.add(defaultOption);
+//                 data.forEach(function(journal) {
+//                     const option = new Option(journal.name, journal.id);
+//                     journalSelect.add(option);
+//                 });
+//             })
+//             .catch(error => console.error('Error:', error));
+//     }
+    
+
+//     if (collectionSelect && journalSelect) {
+//         collectionSelect.addEventListener('change', updateJournals);
+//         updateJournals();
+//     }
+// });
