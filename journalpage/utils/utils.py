56d8871e6218ed83088a39141516dfbbec813844
Samuel Veiga Rangel <samuelveigarangel@hotmail.com>
2024-01-31 16:32:21 -0300

    Cria funcao find_most_recent_journal

--- journalpage/utils/utils.py
+++ journalpage/utils/utils.py
@@ -9,3 +9,14 @@ def get_journal_by_acronyms(journal_acron, acron_collection):
         scielojournal__journal_acron=journal_acron,
         scielojournal__collection__acron3=acron_collection,
     )
+
+def find_most_recent_journal(journal):
+    # TODO
+    # Por enquanto, enquanto os campos old_title e new_title nao estao com valores corretos,
+    # esta funcao ira pegar o valor de next_journal_title
+    while journal.official.next_journal_title:
+        try:
+            journal = Journal.objects.get(title=journal.official.next_journal_title)
+        except Journal.DoesNotExist:
+            break
+    return journal
