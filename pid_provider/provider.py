6679bd28f37d32fe2d2da7acdfb267c176a49168
Roberta Takenaka <roberta.takenaka@scielo.org>
2024-02-03 20:03:38 -0300

    Cria pid_provider.provider.PidProvider.get_xmltree e get_sps_pkg_name

--- pid_provider/provider.py
+++ pid_provider/provider.py
@@ -1,9 +1,23 @@
 
 from pid_provider.base_pid_provider import BasePidProvider
+from pid_provider.models import PidProviderXML
 
 
 class PidProvider(BasePidProvider):
     """
     Recebe XML para validar ou atribuir o ID do tipo v3
     """
-    pass
+
+    @staticmethod
+    def get_xmltree(pid_v3):
+        try:
+            return PidProviderXML.get_xml_with_pre(pid_v3).xmltree
+        except (PidProviderXML.DoesNotExist, AttributeError):
+            return None
+
+    @staticmethod
+    def get_sps_pkg_name(pid_v3):
+        try:
+            return PidProviderXML.get_xml_with_pre(pid_v3).sps_pkg_name
+        except (PidProviderXML.DoesNotExist, AttributeError):
+            return None
