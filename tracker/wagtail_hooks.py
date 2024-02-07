5f8d22b0817d079f8210bad1cf889cbfa570a5aa
Roberta Takenaka <roberta.takenaka@scielo.org>
2024-02-07 10:56:17 -0300

    Cria tracker.wagtail_hooks.HelloModelAdmin

--- tracker/wagtail_hooks.py
+++ tracker/wagtail_hooks.py
@@ -9,7 +9,7 @@ from wagtail.contrib.modeladmin.views import CreateView
 
 from config.menu import get_menu_order
 
-from .models import UnexpectedEvent
+from .models import UnexpectedEvent, Hello
 
 
 class UnexpectedEventModelAdmin(ModelAdmin):
@@ -41,12 +41,42 @@ class UnexpectedEventModelAdmin(ModelAdmin):
     )
 
 
+class HelloModelAdmin(ModelAdmin):
+    model = Hello
+    inspect_view_enabled = True
+    menu_label = _("Hello events")
+    menu_icon = "folder"
+    menu_order = 200
+    add_to_settings_menu = False
+    exclude_from_explorer = False
+
+    list_display = (
+        "status",
+        "exception_type",
+        "exception_msg",
+        "traceback",
+        "created",
+    )
+    list_filter = ("status", "exception_type",)
+    search_fields = (
+        "exception_msg",
+        "detail",
+    )
+    inspect_view_fields = (
+        "exception_type",
+        "exception_msg",
+        "traceback",
+        "detail",
+        "created",
+    )
+
+
 class UnexpectedEventModelAdminGroup(ModelAdminGroup):
     menu_icon = "folder"
     menu_label = _("Unexpected errors")
     # menu_order = get_menu_order("journal")
     menu_order = 200
-    items = (UnexpectedEventModelAdmin,)
+    items = (UnexpectedEventModelAdmin, HelloModelAdmin)
     menu_order = get_menu_order("unexpected-error")
 
 
