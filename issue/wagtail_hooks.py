97732cb71a5c6ec80846ccdb280bc90ef4236068
Samuel Veiga Rangel <samuelveigarangel@hotmail.com>
2024-02-22 12:26:15 -0300

    Adiciona edit_view_class em IssueAdmin

--- issue/wagtail_hooks.py
+++ issue/wagtail_hooks.py
@@ -8,19 +8,24 @@ from wagtail.contrib.modeladmin.options import (
 from wagtail.contrib.modeladmin.views import CreateView
 
 from .models import Issue
-
+from core.wagtail_hooks import BaseEditView
 
 class IssueCreateView(CreateView):
     def form_valid(self, form):
         self.object = form.save_all(self.request.user)
         return HttpResponseRedirect(self.get_success_url())
 
+class IssueEditView(BaseEditView):
+    readonly_fields = [
+        "journal"
+    ]
 
 class IssueAdmin(ModelAdmin):
     model = Issue
     inspect_view_enabled = True
     menu_label = _("Issues")
     create_view_class = IssueCreateView
+    edit_view_class = IssueEditView
     menu_icon = "folder-open-inverse"
     menu_order = 3
     add_to_settings_menu = False
