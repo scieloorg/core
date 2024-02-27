6efcef73354b9e633435c4f02e01465fe2657df6
Samuel Veiga Rangel <samuelveigarangel@hotmail.com>
2024-02-27 17:43:46 -0300

    black

--- journal/tasks.py
+++ journal/tasks.py
@@ -9,7 +9,10 @@ from collection.models import Collection
 from config import celery_app
 from journal.models import SciELOJournal, JournalLogo, Journal
 from journal.sources import classic_website
-from journal.sources.article_meta import process_journal_article_meta, _register_journal_data
+from journal.sources.article_meta import (
+    process_journal_article_meta,
+    _register_journal_data,
+)
 from tracker.models import UnexpectedEvent
 
 
@@ -51,7 +54,7 @@ def load_journal_from_article_meta(
                     username=username,
                     collection_acron=item.acron3,
                     limit=limit,
-                    load_data=load_data
+                    load_data=load_data,
                 )
             )
     except Exception as e:
@@ -67,7 +70,12 @@ def load_journal_from_article_meta(
 
 @celery_app.task(bind=True)
 def load_journal_from_article_meta_for_one_collection(
-    self, username=None, user_id=None, collection_acron=None, limit=None, load_data=None,
+    self,
+    username=None,
+    user_id=None,
+    collection_acron=None,
+    limit=None,
+    load_data=None,
 ):
     user = _get_user(self.request, username=username, user_id=user_id)
     try:
@@ -101,28 +109,40 @@ def load_journal_logo(self, user_id=None, username=None):
         collection_acron3 = collection.acron3
         journal_acron = scielo_journal.journal_acron
         journal_id = scielo_journal.journal.id
-        fetch_and_process_journal_logo.apply_async(kwargs=dict(collection_acron3=collection_acron3, domain=domain, journal_acron=journal_acron, journal_id=journal_id, username=username))
+        fetch_and_process_journal_logo.apply_async(
+            kwargs=dict(
+                collection_acron3=collection_acron3,
+                domain=domain,
+                journal_acron=journal_acron,
+                journal_id=journal_id,
+                username=username,
+            )
+        )
 
-        
+
 @celery_app.task(bind=True)
-def fetch_and_process_journal_logo(self, collection_acron3, domain, journal_acron, journal_id, user_id=None, username=None):
+def fetch_and_process_journal_logo(
+    self,
+    collection_acron3,
+    domain,
+    journal_acron,
+    journal_id,
+    user_id=None,
+    username=None,
+):
     user = _get_user(self.request, username=username, user_id=user_id)
-    if collection_acron3 == 'scl':
+    if collection_acron3 == "scl":
         url_logo = f"https://{domain}/media/images/{journal_acron}_glogo.gif"
     else:
         url_logo = f"http://{domain}/img/revistas/{journal_acron}/glogo.gif"
-    
+
     try:
         response = fetch_data(url_logo, json=False, timeout=2, verify=True)
         logo_data = response
         img_wagtail = Image(title=journal_acron)
         img_wagtail.file.save(f"{journal_acron}_glogo.gif", ContentFile(logo_data))
         journal = Journal.objects.get(id=journal_id)
-        JournalLogo.create_or_update(
-            journal=journal,
-            logo=img_wagtail,
-            user=user
-        )
+        JournalLogo.create_or_update(journal=journal, logo=img_wagtail, user=user)
     except Exception as e:
         exc_type, exc_value, exc_traceback = sys.exc_info()
         UnexpectedEvent.create(
@@ -139,9 +159,12 @@ def fetch_and_process_journal_logo(self, collection_acron3, domain, journal_acro
 @celery_app.task(bind=True)
 def assign_logo_to_all_journals(self, user_id=None, username=None):
     for journal in Journal.objects.all():
-        update_journal_logo_if_exists.apply_async(kwargs=dict(
-            journal_id=journal.id,
-        ))
+        update_journal_logo_if_exists.apply_async(
+            kwargs=dict(
+                journal_id=journal.id,
+            )
+        )
+
 
 @celery_app.task(bind=True)
 def update_journal_logo_if_exists(self, journal_id, user_i=None, username=None):
@@ -149,4 +172,3 @@ def update_journal_logo_if_exists(self, journal_id, user_i=None, username=None):
     if journal_logo := JournalLogo.objects.filter(journal=journal).first():
         journal.logo = journal_logo.logo
         journal.save()
-        
