98b2ac2a4e52d10f756f3981edc8b80b98f3a9ca
Samuel Veiga Rangel <samuelveigarangel@hotmail.com>
2024-03-04 13:32:15 -0300

    migracoes

--- reference/migrations/0003_alter_journaltitle_unique_together.py
+++ reference/migrations/0003_alter_journaltitle_unique_together.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
     dependencies = [
         ("journal", "0019_alter_journallogo_unique_together"),
-        ("reference", "0003_alter_journaltitle_title"),
+        ("reference", "0002_journaltitle_journal"),
     ]
 
     operations = [
