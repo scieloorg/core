a1d136b3fe5b09c43442e0639b2f08c0fd92543f
Samuel Veiga Rangel <samuelveigarangel@hotmail.com>
2024-03-04 13:32:04 -0300

    Remove scripts

--- journal/scripts/assign_logo_to_all_journals.py
+++
@@ -1,5 +0,0 @@
-from journal import tasks
-
-
-def run():
-    tasks.assign_logo_to_all_journals.apply_async()
--- journal/scripts/load_journal_logo.py
+++
@@ -1,9 +0,0 @@
-from journal import tasks
-
-
-def run(username):
-    tasks.load_journal_logo.apply_async(
-        kwargs={
-            "username": username,
-            }
-    )
