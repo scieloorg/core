25b2473fd3defb302be5d51d34b038d9f3ee9fbc
Samuel Veiga Rangel <samuelveigarangel@hotmail.com>
2024-03-05 15:49:58 -0300

    git checkout main --

--- reference/models.py
+++ reference/models.py
@@ -1,15 +1,12 @@
-from django.db import models, IntegrityError
-from modelcluster.fields import ParentalKey
+from django.db import models
 
 from core.models import CommonControlField
 
-from journal.models import Journal
 # Create your models here.
 
 
 class JournalTitle(CommonControlField):
     title = models.TextField(null=True, blank=True)
-
 
     def __str__(self):
         return f"{self.title}"
