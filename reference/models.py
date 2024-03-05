4d367fa434b8f42160a89f9f9b093f1235ca07f1
Samuel Veiga Rangel <samuelveigarangel@hotmail.com>
2024-03-05 14:55:52 -0300

    Remove metodos e campo journal

--- reference/models.py
+++ reference/models.py
@@ -8,55 +8,8 @@ from journal.models import Journal
 
 
 class JournalTitle(CommonControlField):
-    journal = ParentalKey(
-        Journal, on_delete=models.SET_NULL, related_name="other_titles", null=True
-    )
     title = models.TextField(null=True, blank=True)
 
 
-    class Meta:
-        unique_together = [("journal", "title")]
-
-
-    @classmethod
-    def get(
-        cls,
-        title,
-        journal,
-    ):
-        if not title and not journal:
-            raise ValueError("JournalTitle.get requires title paramenter")
-        return journal.other_titles.get(title=title)
-        
-    @classmethod
-    def create(
-        cls,
-        title,
-        journal,
-        user,
-    ):
-        try:
-            obj = cls(
-                title=title,
-                journal=journal,
-                creator=user,
-            )
-            obj.save()
-            return obj
-        except IntegrityError:
-            return cls.get(title=title, journal=journal)
-
-    @classmethod
-    def create_or_update(
-        cls,
-        title,
-        journal,
-        user,
-    ):
-        try:
-            return cls.get(title=title, journal=journal)
-        except cls.DoesNotExist:
-            return cls.create(title=title, journal=journal, user=user)
-    
     def __str__(self):
         return f"{self.title}"
