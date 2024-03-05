ea8cbd1de83c7f4952a9cd52dde734ab469fcebb
Samuel Veiga Rangel <samuelveigarangel@hotmail.com>
2024-03-05 14:55:14 -0300

    Cria JournalOtherTitle

--- journal/models.py
+++ journal/models.py
@@ -2538,3 +2538,58 @@ class JournalLogo(CommonControlField):
             return cls.get(journal=journal, logo=logo)
         except cls.DoesNotExist:
             return cls.create(journal=journal, logo=logo, user=user)
+        
+
+class JournalOtherTitle(CommonControlField):
+    journal = ParentalKey(
+        Journal, on_delete=models.SET_NULL, related_name="other_titles", null=True
+    )
+    title = models.TextField(null=True, blank=True)
+
+
+    class Meta:
+        unique_together = [("journal", "title")]
+
+
+    @classmethod
+    def get(
+        cls,
+        title,
+        journal,
+    ):
+        if not title and not journal:
+            raise ValueError("JournalTitle.get requires title paramenter")
+        return journal.other_titles.get(title=title)
+        
+    @classmethod
+    def create(
+        cls,
+        title,
+        journal,
+        user,
+    ):
+        try:
+            obj = cls(
+                title=title,
+                journal=journal,
+                creator=user,
+            )
+            obj.save()
+            return obj
+        except IntegrityError:
+            return cls.get(title=title, journal=journal)
+
+    @classmethod
+    def create_or_update(
+        cls,
+        title,
+        journal,
+        user,
+    ):
+        try:
+            return cls.get(title=title, journal=journal)
+        except cls.DoesNotExist:
+            return cls.create(title=title, journal=journal, user=user)
+    
+    def __str__(self):
+        return f"{self.title}"
