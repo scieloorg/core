5ecbf22b4603b30f765a6914052c7c34f19d00c3
Roberta Takenaka <roberta.takenaka@scielo.org>
2024-03-17 09:36:55 -0300

    Adiciona rota para fix_pid_v2

--- config/api_router.py
+++ config/api_router.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter, SimpleRouter
 
 from article.api.v1.views import ArticleViewSet
 from issue.api.v1.views import IssueViewSet
-from pid_provider.api.v1.views import PidProviderViewSet
+from pid_provider.api.v1.views import PidProviderViewSet, FixPidV2ViewSet
 
 app_name = "pid_provider"
 
@@ -15,5 +15,6 @@ else:
 router.register("article", ArticleViewSet, basename="Article")
 router.register("issue", IssueViewSet, basename="Issue")
 router.register("pid_provider", PidProviderViewSet, basename="pid_provider")
+router.register("fix_pid_v2", FixPidV2ViewSet, basename="fix_pid_v2")
 
 urlpatterns = router.urls
