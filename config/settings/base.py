713a435c338b8e442037ade88e1d143263e926b9
Samuel Veiga Rangel <samuelveigarangel@hotmail.com>
2024-03-21 17:10:02 -0300

    remove captcha

--- config/settings/base.py
+++ config/settings/base.py
@@ -105,7 +105,6 @@ THIRD_PARTY_APPS = [
     "allauth.account",
     "allauth.socialaccount",
     "django_celery_beat",
-    "captcha",
     "wagtailautocomplete",
     "wagtailcaptcha",
     "wagtailmenus",
