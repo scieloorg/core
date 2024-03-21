5dd8eb0db7185cac76188b17282b02d5e2c5082f
Samuel Veiga Rangel <samuelveigarangel@hotmail.com>
2024-03-21 17:10:48 -0300

    Cria Serializers para Journal

--- journal/api/v1/serializers.py
+++ journal/api/v1/serializers.py
@@ -1,11 +1,81 @@
 from rest_framework import serializers
 
-from journal.models import Journal
+from core.api.v1.serializers import LanguageSerializer
+from journal import models
 
 
-class JournalSerialiazer(serializers.ModelSerializer):
+
+class OfficialJournalSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Journal
+        model = models.OfficialJournal
         fields = [
             "title",
+            "issn_print",
+            "issn_electronic",
+            "issnl",
+        ]
+        datatables_always_serialize = ("id",)
+
+
+class SubjectDescriptorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.SubjectDescriptor
+        fields = [
+            "value",
+        ]
+
+
+class SubjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Subject
+        fields = [
+            "value",
+        ]
+
+
+class JournalUseLicenseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.JournalLicense
+        fields = [
+            "license_type",
+        ]
+
+
+class PublisherSerializer(serializers.ModelSerializer):
+    name = serializers.CharField(source='institution.institution.institution_identification.name')
+
+    class Meta:
+        model = models.PublisherHistory
+        fields = [
+            "name",
+        ]
+
+
+class JournalSerializer(serializers.ModelSerializer):
+    # Serializadores para campos de relacionamento, como 'official', devem corresponder aos campos do modelo.
+    official = OfficialJournalSerializer(many=False, read_only=True)
+    subject_descriptor = SubjectDescriptorSerializer(many=True, read_only=True)
+    subject = SubjectSerializer(many=True, read_only=True)
+    text_language = LanguageSerializer(many=True, read_only=True)
+    journal_use_license = JournalUseLicenseSerializer(many=False, read_only=True)
+    publisher = PublisherSerializer(many=True, read_only=True, source="publisher_history")
+    acronym = serializers.SerializerMethodField()
+    
+    def get_acronym(self, obj):
+        scielo_journal = obj.scielojournal_set.first()
+        return scielo_journal.journal_acron if scielo_journal else None
+
+    class Meta:
+        model = models.Journal
+        fields = [
+            "official",
+            "title",
+            "short_title",
+            "acronym",
+            "journal_use_license",
+            "publisher",
+            "subject_descriptor",
+            "subject",
+            "text_language",
+        
         ]
