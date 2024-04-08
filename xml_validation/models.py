999560ce3593ca0dbddc3db779f8a7bad30ea973
Samuel Veiga Rangel <samuelveigarangel@hotmail.com>
2024-04-08 15:44:42 -0300

    Cria modelo ValidationConfiguration

--- xml_validation/models.py
+++ xml_validation/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from wagtail.admin.panels import FieldPanel
 
-# Create your models here.
+from core.models import CommonControlField
+
+
+class ValidationConfiguration(CommonControlField):
+    key = models.CharField(
+        max_length=100, 
+        blank=False, 
+        null=False,
+    )
+    value = models.JSONField()
+
+    class Meta:
+        verbose_name = _("Validation Configuration")
+        verbose_name_plural = _("Validation Configurations")
+
+    panels = [
+        FieldPanel("key"),
+        FieldPanel("value"),
+    ]
+
+    def __str__(self): 
+        return f"{self.key}: {self.value}"
