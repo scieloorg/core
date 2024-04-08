4e2b0109ee93919570443f610a0fb44bfbec5973
Samuel Veiga Rangel <samuelveigarangel@hotmail.com>
2024-04-08 15:54:08 -0300

    Insere API na rota

--- config/api_router.py
+++ config/api_router.py
@@ -4,7 +4,8 @@ from rest_framework.routers import DefaultRouter, SimpleRouter
 from article.api.v1.views import ArticleViewSet
 from issue.api.v1.views import IssueViewSet
 from pid_provider.api.v1.views import PidProviderViewSet, FixPidV2ViewSet
-from journal.api.v1.views import JournalViewSet, GenericJournalViewSet
+from journal.api.v1.views import JournalViewSet
+from xml_validation.api.v1.views import GenericJournalViewSet
 
 
 app_name = "pid_provider"
@@ -19,7 +20,7 @@ router.register("issue", IssueViewSet, basename="Issue")
 router.register("pid_provider", PidProviderViewSet, basename="pid_provider")
 router.register("fix_pid_v2", FixPidV2ViewSet, basename="fix_pid_v2")
 router.register("journal", JournalViewSet, basename="journal")
-
+router.register("xml_validation", GenericJournalViewSet, basename="xml_validation")
 
 
 urlpatterns = router.urls
