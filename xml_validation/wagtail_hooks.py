95ede3e7afe1048abc4a4f9823463eb1215be2fc
Samuel Veiga Rangel <samuelveigarangel@hotmail.com>
2024-04-08 15:44:59 -0300

    Adiciona modelo ValidationConfiguration na area administrativa

--- xml_validation/wagtail_hooks.py
+++ xml_validation/wagtail_hooks.py
@@ -1,8 +1,18 @@
+from django.utils.translation import gettext as _
+from wagtail.snippets.models import register_snippet
+from wagtail.snippets.views.snippets import SnippetViewSet
+from config.menu import get_menu_order
+
+from xml_validation.models import ValidationConfiguration
 
 
+class ValidationConfigurationViewSet(SnippetViewSet):
+    model = ValidationConfiguration
+    icon = 'folder-open-inverse'
+    add_to_admin_menu = True
+    menu_label = _("Validation Configutarion")
+    menu_order = get_menu_order("xml_validation")
 
 
-
-
-
+register_snippet(ValidationConfigurationViewSet)
 
