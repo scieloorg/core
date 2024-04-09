ca4a81054989fb42f6f8fe835564db9ea034f436
Samuel Veiga Rangel <samuelveigarangel@hotmail.com>
2024-04-09 15:44:13 -0300

    Inclui modelo VersionSPS na area administrativa

--- xml_validation/wagtail_hooks.py
+++ xml_validation/wagtail_hooks.py
@@ -1,18 +1,28 @@
 from django.utils.translation import gettext as _
 from wagtail.snippets.models import register_snippet
-from wagtail.snippets.views.snippets import SnippetViewSet
+from wagtail.snippets.views.snippets import SnippetViewSet, SnippetViewSetGroup
 from config.menu import get_menu_order
 
-from xml_validation.models import ValidationConfiguration
+from xml_validation.models import ValidationConfiguration, VersionSPS
 
 
 class ValidationConfigurationViewSet(SnippetViewSet):
     model = ValidationConfiguration
     icon = 'folder-open-inverse'
-    add_to_admin_menu = True
     menu_label = _("Validation Configutarion")
+    menu_order = 1
+
+
+class VersionSPSViewSet(SnippetViewSet):
+    model = VersionSPS
+    icon = 'folder-open-inverse'
+    menu_label = _("SciELO Publishing Schema")
+    menu_order = 2
+
+
+class XmlValidationGroup(SnippetViewSetGroup):
+    items = (ValidationConfigurationViewSet, VersionSPSViewSet)
     menu_order = get_menu_order("xml_validation")
 
 
-register_snippet(ValidationConfigurationViewSet)
-
+register_snippet(XmlValidationGroup)
