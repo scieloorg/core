68fb7e5bff00663ec4ab57a9e72f6a00afa70461
Samuel Veiga Rangel <samuelveigarangel@hotmail.com>
2024-04-09 17:00:49 -0300

    Cria ValidationConfigSerializerView

--- xml_validation/api/v1/views.py
+++ xml_validation/api/v1/views.py
@@ -1,7 +1,7 @@
 from django.db.models import Q
 from rest_framework import viewsets
 
-
+from core.validators import validate_params
 from .serializers import ValidationConfigurationSerializer
 from xml_validation.models import ValidationConfiguration
 
@@ -10,3 +10,16 @@ class GenericValidationConfigViewSet(viewsets.ModelViewSet):
     serializer_class = ValidationConfigurationSerializer
     http_method_names = ["get"]
     queryset = ValidationConfiguration.objects.all()
+
+
+class ValidationConfigSerializerView(GenericValidationConfigViewSet):
+    def get_queryset(self):
+        qs = super().get_queryset()
+        version = self.request.query_params.get("version")
+        validate_params(self.request, "version")
+        try:
+            queryset = qs.filter(version__version=version)
+        except ValidationConfiguration.DoesNotExist:
+            queryset = qs.none()
+        return queryset
+
