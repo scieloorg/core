73f664e8c1777d5c22a3836059304c19e8f5ed65
Samuel Veiga Rangel <samuelveigarangel@hotmail.com>
2024-04-09 15:43:44 -0300

    Cria modelo VersionSPS e adiciona relacionamento FK em ValidationConfiguration

--- xml_validation/models.py
+++ xml_validation/models.py
@@ -1,11 +1,35 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from wagtailautocomplete.edit_handlers import AutocompletePanel
 from wagtail.admin.panels import FieldPanel
 
 from core.models import CommonControlField
 
 
+class VersionSPS(CommonControlField):
+    version = models.CharField(_("Version SPS"), null=False, blank=False)
+    link = models.URLField(null=True, blank=True, verbose_name=_("Link SciELO PS"))
+    date = models.DateField(null=True, blank=True)
+
+    autocomplete_search_field  = "version"
+    
+    class Meta:
+        verbose_name = _("Version SPS")
+        verbose_name_plural = _("Version SPS")
+
+    def autocomplete_label(self):
+        return str(self)
+
+    def __str__(self):
+        return f"{self.version} - {self.date}"
+
+
 class ValidationConfiguration(CommonControlField):
+    version = models.ForeignKey(VersionSPS, 
+        on_delete=models.CASCADE,
+        blank=False, 
+        null=False
+    )
     key = models.CharField(
         max_length=100, 
         blank=False, 
@@ -18,6 +42,7 @@ class ValidationConfiguration(CommonControlField):
         verbose_name_plural = _("Validation Configurations")
 
     panels = [
+        AutocompletePanel("version"),
         FieldPanel("key"),
         FieldPanel("value"),
     ]
