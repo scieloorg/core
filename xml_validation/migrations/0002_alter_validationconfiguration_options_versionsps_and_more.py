8f54abf2a3b02480b53f6d09cdcce1889107e3c4
Samuel Veiga Rangel <samuelveigarangel@hotmail.com>
2024-04-19 14:36:03 -0300

    migration

--- xml_validation/migrations/0002_alter_validationconfiguration_options_versionsps_and_more.py
+++ xml_validation/migrations/0002_alter_validationconfiguration_options_versionsps_and_more.py
@@ -82,7 +82,7 @@ class Migration(migrations.Migration):
         ),
         migrations.AddField(
             model_name="validationconfiguration",
-            name="version",
+            name="sps_version",
             field=models.ForeignKey(
                 default=1,
                 on_delete=django.db.models.deletion.CASCADE,
