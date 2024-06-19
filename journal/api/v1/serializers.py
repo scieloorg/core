2a95fbfd81105db59416e4986f50e5838ac28f1f
Samuel Veiga Rangel <samuelveigarangel@hotmail.com>
2024-06-19 00:33:05 -0300

    black

--- journal/api/v1/serializers.py
+++ journal/api/v1/serializers.py
@@ -4,7 +4,6 @@ from core.api.v1.serializers import LanguageSerializer
 from journal import models
 
 
-
 class OfficialJournalSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.OfficialJournal
@@ -43,7 +42,9 @@ class JournalUseLicenseSerializer(serializers.ModelSerializer):
 
 
 class PublisherSerializer(serializers.ModelSerializer):
-    name = serializers.CharField(source='institution.institution.institution_identification.name')
+    name = serializers.CharField(
+        source="institution.institution.institution_identification.name"
+    )
 
     class Meta:
         model = models.PublisherHistory
@@ -53,8 +54,10 @@ class PublisherSerializer(serializers.ModelSerializer):
 
 
 class OwnerSerializer(serializers.ModelSerializer):
-    name = serializers.CharField(source='institution.institution.institution_identification.name')
-    
+    name = serializers.CharField(
+        source="institution.institution.institution_identification.name"
+    )
+
     class Meta:
         model = models.OwnerHistory
         fields = [
@@ -69,7 +72,9 @@ class JournalSerializer(serializers.ModelSerializer):
     subject = SubjectSerializer(many=True, read_only=True)
     text_language = LanguageSerializer(many=True, read_only=True)
     journal_use_license = JournalUseLicenseSerializer(many=False, read_only=True)
-    publisher = PublisherSerializer(many=True, read_only=True, source="publisher_history")
+    publisher = PublisherSerializer(
+        many=True, read_only=True, source="publisher_history"
+    )
     owner = OwnerSerializer(many=True, read_only=True, source="owner_history")
     acronym = serializers.SerializerMethodField()
     scielo_journal = serializers.SerializerMethodField()
@@ -78,17 +83,18 @@ class JournalSerializer(serializers.ModelSerializer):
         scielo_journal = obj.scielojournal_set.first()
         return scielo_journal.journal_acron if scielo_journal else None
 
-
     def get_scielo_journal(self, obj):
-        scielo_journals = obj.scielojournal_set.prefetch_related('journal_history').values(
-                'journal_acron', 
-                'issn_scielo', 
-                'journal_history__day', 
-                'journal_history__month', 
-                'journal_history__year', 
-                'journal_history__event_type', 
-                'journal_history__interruption_reason'
-            )
+        scielo_journals = obj.scielojournal_set.prefetch_related(
+            "journal_history"
+        ).values(
+            "journal_acron",
+            "issn_scielo",
+            "journal_history__day",
+            "journal_history__month",
+            "journal_history__year",
+            "journal_history__event_type",
+            "journal_history__interruption_reason",
+        )
         renamed_results = [
             {
                 "journal_acron": item["journal_acron"],
@@ -98,7 +104,7 @@ class JournalSerializer(serializers.ModelSerializer):
                 "year": item["journal_history__year"],
                 "event_type": item["journal_history__event_type"],
                 "interruption_reason": item["journal_history__interruption_reason"],
-            } 
+            }
             for item in scielo_journals
         ]
         return renamed_results
