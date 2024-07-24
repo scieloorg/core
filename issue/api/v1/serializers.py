d3b5e59032df7d113859153169081c88c035ceab
Samuel Veiga Rangel <samuelveigarangel@hotmail.com>
2024-07-23 22:10:44 -0300

    Remove funcao get_scielo_journal

--- issue/api/v1/serializers.py
+++ issue/api/v1/serializers.py
@@ -18,13 +18,11 @@ class IssueSerializer(serializers.ModelSerializer):
     journal = serializers.SerializerMethodField()
     sections = TocSectionsSerializer(many=True, read_only=True)
     license = LicenseStatementSerializer(many=True, read_only=True)
-    scielo_journal = serializers.SerializerMethodField()
     
     class Meta:
         model = models.Issue
         fields = [
             "journal",
-            "scielo_journal",
             "sections",
             "license",
             "number",
@@ -34,42 +32,6 @@ class IssueSerializer(serializers.ModelSerializer):
             "month",
             "supplement",
         ]
-
-    def get_scielo_journal(self, obj):
-        results = obj.journal.scielojournal_set.prefetch_related("journal_history").values(
-            "issn_scielo", 
-            "journal_acron",
-            "collection__acron3",
-            "journal_history__day",
-            "journal_history__month",
-            "journal_history__year",
-            "journal_history__event_type",
-            "journal_history__interruption_reason",
-        )
-        journal_dict = {}
-
-        for item in results:
-            journal_acron = item["journal_acron"]
-            issn_scielo = item["issn_scielo"]
-
-            journal_history = dict(
-            collection_acron=item["collection__acron3"],
-            day=item["journal_history__day"],
-            month=item["journal_history__month"],
-            year=item["journal_history__year"],
-            event_type=item["journal_history__event_type"],
-            interruption_reason=item["journal_history__interruption_reason"])
-
-            if journal_acron not in journal_dict:
-                journal_dict[journal_acron] = {
-                    "issn_scielo": issn_scielo,
-                    "journal_acron": journal_acron,
-                    "journal_history": []
-                }
-
-            journal_dict[journal_acron]["journal_history"].append(journal_history)
-        
-        return list(journal_dict.values())
 
     def get_journal(self, obj):
         collection_acron3 = self.context.get("request").query_params.get("collection")
