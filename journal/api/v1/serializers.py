0172cb7f5a8b211d4652d80b6664c4c4e287f0a3
Samuel Veiga Rangel <samuelveigarangel@hotmail.com>
2024-08-08 14:48:16 -0300

    - Adiciona os metodos get_next_journal_title e get_previous_journal_titles - Modifica os metodos de history

--- journal/api/v1/serializers.py
+++ journal/api/v1/serializers.py
@@ -68,6 +68,7 @@ class OwnerSerializer(serializers.ModelSerializer):
 
 class MissionSerializer(serializers.ModelSerializer):
     code2 = serializers.CharField(source="language.code2")
+    
     class Meta:
         model = models.Mission
         fields = [
@@ -91,31 +92,29 @@ class JournalSerializer(serializers.ModelSerializer):
     mission = MissionSerializer(many=True, read_only=True)
     issn_print = serializers.CharField(source="official.issn_print")
     issn_electronic = serializers.CharField(source="official.issn_electronic")
-    next_journal_title = serializers.CharField(source="official.next_journal_title")
-    previous_journal_titles = serializers.CharField(source="official.previous_journal_titles")
     other_titles = serializers.SerializerMethodField()
     sponsor = serializers.SerializerMethodField()
     email = serializers.SerializerMethodField()
     copyright = serializers.SerializerMethodField()
+    next_journal_title = serializers.SerializerMethodField()
+    previous_journal_titles = serializers.SerializerMethodField()
 
-    def get_institution_data(self, history):
-        data = []
-        for record in history.all():
-            if record.institution:
-                data.append({"name": record.institution.institution.institution_identification.name})
-        return data if data else None
     
     def get_publisher(self, obj):
-       return self.get_institution_data(obj.publisher_history)
+        if queryset := obj.publisher_history.all():
+            return [{"name": str(item)} for item in queryset]
 
     def get_owner(self, obj):
-        return self.get_institution_data(obj.owner_history)
+        if queryset := obj.owner_history.all():
+            return [{"name": str(item)} for item in queryset]
 
     def get_sponsor(self, obj):
-        return self.get_institution_data(obj.sponsor_history)
+        if queryset := obj.sponsor_history.all():
+            return [{"name": str(item)} for item in queryset]        
 
     def get_copyright(self, obj):
-        return self.get_institution_data(obj.copyright_holder_history)
+        if queryset := obj.copyright_holder_history.all():
+            return [{"name": str(item)} for item in queryset]        
 
     def get_acronym(self, obj):
         scielo_journal = obj.scielojournal_set.first()
@@ -146,8 +145,7 @@ class JournalSerializer(serializers.ModelSerializer):
     def get_url_logo(self, obj):
         if obj.logo:
             domain = Site.objects.get(is_default_site=True).hostname
-            if not domain.startswith('https'):
-                domain = f"https://{domain}"
+            domain = f"https://{domain}"
             return f"{domain}{obj.logo.file.url}"
         return None
 
@@ -161,6 +159,32 @@ class JournalSerializer(serializers.ModelSerializer):
             return [other_title.title for other_title in obj.other_titles.all()]
         return None
     
+    def get_next_journal_title(self, obj):
+        if obj.official.next_journal_title:
+            return {
+                "next_journal_title": obj.official.next_journal_title,
+                "issn_print": obj.official.new_title.issn_print,
+                "issn_electronic": obj.official.new_title.issn_electronic,
+            }
+
+    def get_previous_journal_titles(self, obj):
+        if obj.official.previous_journal_titles:
+            try:
+                old_issn_print = obj.official.old_title.get(title__icontains=obj.official.previous_journal_titles).issn_print
+            except models.OfficialJournal.DoesNotExist:
+                old_issn_print = None
+
+            try:
+                old_issn_electronic = obj.official.old_title.get(title__icontains=obj.official.previous_journal_titles).issn_electronic
+            except models.OfficialJournal.DoesNotExist:
+                old_issn_electronic = None                
+            
+            return {
+                "previous_journal_title": obj.official.previous_journal_titles,
+                "issn_print": old_issn_print,
+                "issn_electronic": old_issn_electronic,
+            }
+
 
     class Meta:
         model = models.Journal
