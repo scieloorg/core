12008f181820e4948fef913d3c0a02cf312dc98c
Samuel Veiga Rangel <samuelveigarangel@hotmail.com>
2024-08-12 21:01:12 -0300

    Altera max_length de doi_prefix

--- journal/migrations/0027_journal_doi_prefix.py
+++ journal/migrations/0027_journal_doi_prefix.py
@@ -13,6 +13,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name="journal",
             name="doi_prefix",
-            field=models.CharField(blank=True, max_length=50, null=True),
+            field=models.CharField(blank=True, max_length=100, null=True),
         ),
     ]
