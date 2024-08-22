b342ba163a6a716b3dc8cb744db5b76ec85b60ae
Samuel Veiga Rangel <samuelveigarangel@hotmail.com>
2024-08-22 12:19:17 -0300

    Cria testes para modelo ArticleFormat

--- article/tests.py
+++ article/tests.py
@@ -1,11 +1,13 @@
 from freezegun import freeze_time
 from django.test import TestCase
+from django.core.files.uploadedfile import SimpleUploadedFile
 from django_test_migrations.migrator import Migrator
 from datetime import datetime
 from django.utils.timezone import make_aware
 
-from article.models import Article
+from article.models import Article, ArticleFormat
 from article.tasks import remove_duplicate_articles
+from core.users.models import User
 
 
 class TestArticleMigration(TestCase):
@@ -58,3 +60,105 @@ class RemoveDuplicateArticlesTest(TestCase):
         self.assertEqual(Article.objects.filter(pid_v3="pid3").count(), 1)
         self.assertEqual(Article.objects.get(pid_v3="pid2").created, make_aware(datetime(2022, 6, 3)))
         self.assertEqual(Article.objects.get(pid_v3="pid3").created, make_aware(datetime(2022, 6, 14)))
+
+
+class ArticleFormatModelTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(
+            name="admin",
+        )
+        self.article = Article.objects.create(
+            pid_v3="P3swRmPHQfy37r9xRbLCw8G",
+            sps_pkg_name="0001-3714-rm-30-04-299",
+        )
+
+        self.test_file = SimpleUploadedFile("test_file.xml", b"<root><element>Test</element></root>", content_type="application/xml")
+        self.test_file2 = SimpleUploadedFile("test_file.xml", b"<root><element>Test2</element></root>", content_type="application/xml")
+        self.article_format = ArticleFormat.objects.create(
+            article=self.article,
+            format_name='pmc',
+            version=1,
+            file=self.test_file,
+            valid=True,
+            status="S",
+        )
+
+    def test_get_method(self):
+        article_format = ArticleFormat.get(self.article, format_name='pmc', version=1)
+        self.assertEqual(article_format.article,  self.article)
+        self.assertEqual(article_format.format_name, 'pmc')
+        self.assertEqual(article_format.version, 1)
+
+    def test_create_classmethod(self):
+        article_format = ArticleFormat.create(
+            user=self.user,
+            article=self.article,
+            format_name='pubmed',
+            version=1
+        )
+        self.assertEqual(article_format.article,  self.article)
+        self.assertEqual(article_format.format_name, 'pubmed')
+        self.assertEqual(article_format.version, 1)
+
+    def test_get_method_raises_value_error(self):
+        with self.assertRaises(ValueError) as context:
+            ArticleFormat.get(self.article, format_name='pubmed')
+
+        self.assertEqual(str(context.exception), "ArticleFormat.get requires article and format_name and version")
+
+    def test_create_or_update_classmethod(self):
+        article_format = ArticleFormat.create_or_update(
+            user=self.user,
+            article=self.article,
+            format_name="pmc",
+            version=1,
+        )
+        self.assertEqual(article_format.article,  self.article)
+        self.assertEqual(article_format.format_name, 'pmc')
+        self.assertEqual(article_format.version, 1)
+
+    def test_save_file_method(self):
+        filename = "0034-7094-rba-69-03-0227.xml"
+        content = self.test_file
+        content.seek(0)
+        content_bytes = content.read()
+        article_format = ArticleFormat.get(self.article, format_name='pmc', version=1)
+        article_format.save_file(filename=filename, content=content_bytes)
+        
+        with article_format.file.open('rb') as f:
+            saved_content = f.read()
+        
+        self.assertEqual(saved_content, content_bytes)
+
+    def test_update_xml_in_save_file_method(self):
+        filename = "0034-7094-rba-69-03-0227.xml"
+        content = self.test_file
+        content_update = self.test_file2
+        content.seek(0)
+        content_bytes = content.read()
+        article_format = ArticleFormat.get(self.article, format_name='pmc', version=1)
+        article_format.save_file(filename=filename, content=content_bytes)
+
+        content_update.seek(0)
+        content_update_bytes = content_update.read()
+        article_format.save_file(filename=filename, content=content_update_bytes)
+
+        with article_format.file.open('rb') as f:
+            saved_content = f.read()
+
+        self.assertEqual(saved_content, content_update_bytes)
+
+    def test_save_format_xml_method(self):
+        article_format = ArticleFormat.get(self.article, format_name='pmc', version=1)
+        input_xml = "<article><element>Original</element></article>"
+        
+        filename = article_format.article.sps_pkg_name + ".xml"
+        article_format.save_format_xml(format_xml=input_xml, filename=filename)
+        with article_format.file.open('rb') as f:
+            saved_content = f.read()
+        self.assertEqual(saved_content, input_xml.encode('utf-8'))
+
+        self.assertEqual(article_format.status, "S")
+        self.assertEqual(article_format.report, None)
+        self.assertEqual(article_format.version, 1)
+
