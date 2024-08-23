1925e54c4f9fedcd7a8233d95a05ecf61cdffd20
Samuel Veiga Rangel <samuelveigarangel@hotmail.com>
2024-08-23 15:06:36 -0300

    Cria tasks para converter os artigos nos formatos especificados.

--- article/tasks.py
+++ article/tasks.py
@@ -5,11 +5,14 @@ from datetime import datetime
 from django.db.models import Q, Count
 from django.contrib.auth import get_user_model
 from django.utils.translation import gettext as _
+from packtools.sps.formats import pubmed, pmc, crossref
 
 from article.models import Article, ArticleFormat
 from article.sources import xmlsps
 from article.sources.preprint import harvest_preprints
 from config import celery_app
+from doi_manager.models import CrossRefConfiguration
+from journal.models import Journal
 from pid_provider.models import PidProviderXML
 from pid_provider.provider import PidProvider
 from tracker.models import UnexpectedEvent
@@ -141,7 +144,7 @@ def task_convert_xml_to_other_formats_for_articles(
                     kwargs={
                         "user_id": user.id,
                         "username": user.username,
-                        "item_id": item.id,
+                        "pid_v3": item.pid_v3,
                         "force_update": force_update,
                     }
                 )
@@ -191,6 +194,135 @@ def convert_xml_to_other_formats(
     if not done or force_update:
         ArticleFormat.generate_formats(user, article=article)
 
+
+@celery_app.task(bind=True)
+def task_convert_xml_to_other_formats_for_articles(self, format_name, user_id=None, username=None, force_update=False):
+    journals = Journal.objects.filter(indexed_at__acronym=format_name)
+    articles = Article.objects.filter(journal__in=journals)
+
+    if not force_update:
+        articles = articles.filter(article_format__isnull=True)
+
+    try:
+        task_function_dict = {
+            "pubmed": convert_xml_to_pubmed_or_pmc_formats,
+            "pmc": convert_xml_to_pubmed_or_pmc_formats,
+            "crossref": convert_xml_to_crossref_format,
+        }
+        task_function = task_function_dict[format_name]
+    except Exception as exception:
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        UnexpectedEvent.create(
+            exception=exception,
+            exc_traceback=exc_traceback,
+            detail={
+                "task": "article.tasks.task_convert_xml_to_other_formats_for_articles",
+                "item": str(article),
+            },
+        )
+        return
+
+    for article in articles:
+        try:
+            task_function.apply_async(
+                user_id=user_id,
+                username=username,
+                format_name=format_name,
+            )
+        except Exception as exception:
+            exc_type, exc_value, exc_traceback = sys.exc_info()
+            UnexpectedEvent.create(
+                exception=exception,
+                exc_traceback=exc_traceback,
+                detail={
+                    "task": "article.tasks.task_convert_xml_to_other_formats_for_articles",
+                    "item": str(article),
+                },
+            )
+
+
+def get_function_format_xml(format_name):
+    dict_functions_formats = {
+        "pmc": pmc.pipeline_pmc,
+        "pubmed": pubmed.pipeline_pubmed,
+        "crossref": crossref.pipeline_crossref,
+    }
+    return dict_functions_formats.get(format_name)
+
+
+def handler_formatting_error(article_format, message):
+    article_format.save_format_xml(
+        filename=None,
+        format_xml=None,
+        status="E",
+        report={"exception_msg": message}
+    )
+
+def get_article_format(user, pid_v3, format_name):
+    try:
+        article = Article.objects.get(pid_v3=pid_v3)
+    except Article.DoesNotExist:
+        logging.info(f"Unable to convert article {pid_v3} to the specified format")
+        return
+
+    try:
+        article_format = ArticleFormat.objects.get(article=article, format_name="pmc")
+    except ArticleFormat.DoesNotExist:
+        article_format = ArticleFormat.create_or_update(
+            user=user,
+            article=article,
+            format_name=format_name,
+            version=1
+        )
+    return article_format
+
+@celery_app.task(bind=True)
+def convert_xml_to_pubmed_or_pmc_formats(self, pid_v3, format_name, user_id=None, username=None):
+    user = _get_user(request=self.request, username=username, user_id=user_id)    
+
+    article_format = get_article_format(pid_v3=pid_v3, format_name=format_name, user=user)
+
+    function_format = get_function_format_xml(format_name=format_name)
+
+    content = function_format(article_format.article.xmltree)
+    article_format.save_format_xml(
+        format_xml=content,
+        filename=article_format.article.sps_pkg_name + ".xml",
+        status="S"
+    )
+
+
+@celery_app.task(bind=True)
+def convert_xml_to_crossref_format(self, pid_v3, format_name, user_id=None, username=None):
+    user = _get_user(request=self.request, username=username, user_id=user_id)    
+
+    article_format = get_article_format(pid_v3=pid_v3, format_name=format_name, user=user)
+    
+    doi = article_format.article.doi.first()
+    if not doi:
+        handler_formatting_error(
+            article_format=article_format, 
+            message=f"Unable to format because the article {pid_v3} has no DOI associated with it"
+        )
+        return
+    
+    prefix = doi.value.split("/")[0]
+    try:
+        data = CrossRefConfiguration.get_data(prefix)
+    except CrossRefConfiguration.DoesNotExist:
+        handler_formatting_error(
+            article_format=article_format, 
+            message=f"Unable to convert article {pid_v3} to crossref format. CrossrefConfiguration missing"
+        )
+        return
+
+    function_format = get_function_format_xml(format_name=format_name)
+    content = function_format(article_format.article.xmltree, data)
+    article_format.save_format_xml(
+        format_xml=content,
+        filename=article_format.article.sps_pkg_name + ".xml",
+        status="S"
+    )
 
 @celery_app.task(bind=True)
 def task_articles_complete_data(
