7b5ba924a1415f68bd22ab09f7b2b224a8ca0ed7
Roberta Takenaka <roberta.takenaka@scielo.org>
2024-10-09 22:13:09 -0300

    Cria rename_issue_dictionary_keys (versão mais legível de rename_dictionary_keys)

--- core/utils/rename_dictionary_keys.py
+++ core/utils/rename_dictionary_keys.py
@@ -1,3 +1,6 @@
+import logging
+
+
 def rename_dictionary_keys(list_dictionary, corresp):
     """
     Renomeia as chaves de um dicionário com base em um dicionário de correspondência.
@@ -14,3 +17,28 @@ def rename_dictionary_keys(list_dictionary, corresp):
         for dictionary in list_dictionary
         for key in dictionary
     }
+
+
+def rename_issue_dictionary_keys(list_dictionary, corresp):
+    """
+    Renomeia as chaves de um dicionário com base em um dicionário de correspondência.
+
+    Args:
+        list_dictionary (list): Lista de dicionário.
+
+    Returns:
+        dict: Um novo dicionário com as chaves atualizadas de acordo com o dicionário de correspondência.
+    """
+    logging.info(corresp)
+    logging.info(list_dictionary)
+    
+    d = {}
+    for dictionary in list_dictionary:
+        for key in dictionary:
+            try:
+                k = corresp.get(key) or key
+                d[k] = dictionary[key]
+            except Exception as e:
+                logging.exception(e)
+                logging.info(key)
+    return d
