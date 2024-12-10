2fb0a6d4d264c007a3a77a14e7d3eac0fdd159c8
Samuel Veiga Rangel <samuelveigarangel@hotmail.com>
2024-12-10 11:14:07 -0300

    Muda funcao para receber paramentros mais genericos

--- article/utils/extracts_normalized_email.py
+++ article/utils/extracts_normalized_email.py
@@ -1,7 +1,6 @@
 import re
 
-def extracts_normalized_email(author, aff):
-    raw_email = author.get("email") or aff.get("email")
+def extracts_normalized_email(raw_email):
     if raw_email:
         email_match = re.search(r"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}", raw_email.replace(" ", ""))
         if email_match:
