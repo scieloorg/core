7e661127bfad721f43628a79c9cb2ddb51791b18
Samuel Veiga Rangel <samuelveigarangel@hotmail.com>
2024-12-10 01:17:01 -0300

    Adiciona funcao para extrair email normalizado

---
+++ article/utils/extract_email.py
@@ -0,0 +1,9 @@
+import re
+
+def extract_email(author, aff):
+    raw_email = author.get("email") or aff.get("email")
+    if raw_email:
+        email_match = re.search(r"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}", raw_email.replace(" ", ""))
+        if email_match:
+            return email_match.group()
+    return None
