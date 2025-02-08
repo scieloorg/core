4910e0990948ed77aea7055e6bbf0509dbc7cf25
Samuel Veiga Rangel <samuelveigarangel@hotmail.com>
2025-02-08 04:17:24 -0300

    Realiza melhorias em add_to_result e cria add_items

--- journal/sources/api_article_meta.py
+++ journal/sources/api_article_meta.py
@@ -1,110 +1,105 @@
 from journal.models import SciELOJournal, TitleInDatabase
+from journal.choices import STATUS
 
 def get_articlemeta_format(obj):
     result = {}
     scielo_journal = SciELOJournal.objects.filter(journal=obj, collection__is_active=True).first()
-    publisher_exists = obj.publisher_history.exists()
-
+    publisher_history = obj.publisher_history.all()
+    
+    def add_items(key, items):
+        for item in items:
+            add_to_result(key, item)
+    
     def add_to_result(key, value):
         if value:
-            result[key] = value
+            if key not in result:
+                result[key] = []
+            result[key].append({"_": value})
 
-    add_to_result("collection", scielo_journal.collection.acron3 if scielo_journal else None)
-    add_to_result("v5", [{"_": obj.type_of_literature}] if obj.type_of_literature else None)
-    add_to_result("v6", [{"_": obj.treatment_level}] if obj.treatment_level else None)
-    add_to_result("v10", [{"_": obj.center_code}] if obj.center_code else None)
-    add_to_result("v20", [{"_": obj.national_code}] if obj.national_code else None)
-    add_to_result("v30", [{"_": obj.identification_number}] if obj.identification_number else None)
+    add_to_result("collection", scielo_journal.collection.acron3 if scielo_journal and scielo_journal.collection else None)
+    add_to_result("v5", obj.type_of_literature)
+    add_to_result("v6", obj.treatment_level)
+    add_to_result("v10", obj.center_code)
+    add_to_result("v20", obj.national_code)
+    add_to_result("v30", obj.identification_number)
 
     secs_code = TitleInDatabase.objects.filter(journal=obj, indexed_at__acronym__iexact="secs")
-    add_to_result("v37", [{"_": sc.identifier} for sc in secs_code if sc.identifier] if secs_code.exists() else None)
-
-    add_to_result("v50", [{"_": scielo_journal.status}] if scielo_journal and scielo_journal.status else None)
-    add_to_result("v62", [{"_": ch.institution.institution.institution_identification.name} 
-                            for ch in obj.copyright_holder_history.all() if ch.institution] if obj.copyright_holder_history.exists() else None)
-    add_to_result("v66", [{"_": obj.ftp}] if obj.ftp else None)
-    add_to_result("v67", [{"_": obj.user_subscription}] if obj.user_subscription else None)
-    add_to_result("v68", [{"_": scielo_journal.journal_acron}] if scielo_journal and scielo_journal.journal_acron else None)
-    add_to_result("v69", [{"_": obj.journal_url}] if obj.journal_url else None)
-    add_to_result("v85", [{"_": obj.vocabulary.acronym}] if obj.vocabulary else None)
-    add_to_result("v100", [{"_": obj.title}] if obj.official and obj.official.title else None)
-    add_to_result("v110", [{"_": obj.subtitle}] if obj.subtitle else None)
-    add_to_result("v117", [{"_": obj.standard.code}] if obj.standard and obj.standard.code else None)
-    add_to_result("v130", [{"_": obj.section}] if obj.section else None)
-    add_to_result("v140", [{"_": sponsor.institution.institution.institution_identification.name} 
-                            for sponsor in obj.sponsor_history.all() if sponsor.institution] if obj.sponsor_history.exists() else None)
-    add_to_result("v150", [{"_": obj.short_title}] if obj.short_title else None)
-    add_to_result("v151", [{"_": obj.official.iso_short_title}] if obj.official and obj.official.iso_short_title else None)
-
-    parallel_titles = [{"_": pt.text} for pt in obj.official.parallel_titles if pt.text]
-    add_to_result("v230", parallel_titles if parallel_titles else None)
-
-    add_to_result("v240", [{"_": other_title.title} for other_title in obj.other_titles.all()] if obj.other_titles.exists() else None)
-    add_to_result("v301", [{"_": obj.official.initial_year}] if obj.official and obj.official.initial_year else None)
-    add_to_result("v302", [{"_": obj.official.initial_volume}] if obj.official and obj.official.initial_volume else None)
-    add_to_result("v303", [{"_": obj.official.initial_number}] if obj.official and obj.official.initial_number else None)
+    add_items("v37", [sc.identifier for sc in secs_code if sc.identifier])
+    add_to_result("v50", scielo_journal.status.lower() if scielo_journal.status else None)
+    add_items("v62", [ch.get_institution_name for ch in obj.copyright_holder_history.all()])
+    add_to_result("v66", obj.ftp)
+    add_to_result("v67", obj.user_subscription)
+    add_to_result("v68", scielo_journal.journal_acron if scielo_journal and scielo_journal.journal_acron else None)
+    add_to_result("v69", obj.journal_url)
+    add_to_result("v85", obj.vocabulary.acronym)
+    add_to_result("v110", obj.subtitle)
+    add_to_result("v117", obj.standard.code if obj.standard and obj.standard.code else None)
+    add_to_result("v130", obj.section)
+    add_items("v140", [sponsor.get_institution_name for sponsor in obj.sponsor_history.all()])
+    add_to_result("v150", obj.short_title)
+    add_to_result("v240", [other_title.title for other_title in obj.other_titles.all()])
+    
+    # Data of the object official 
     if obj.official:
+        add_to_result("v100", obj.title if obj.official.title else None)
+        add_to_result("v151", obj.official.iso_short_title if obj.official.iso_short_title else None)
+        add_items("v230", [pt.text for pt in obj.official.parallel_titles if pt.text])
+        add_to_result("v301", obj.official.initial_year if obj.official.initial_year else None)
+        add_to_result("v302", obj.official.initial_volume if obj.official.initial_volume else None)
+        add_to_result("v303", obj.official.initial_number if obj.official.initial_number else None)        
+    
         year = obj.official.terminate_year
         month = obj.official.terminate_month
+
         if year and month:
-            add_to_result("v304", [{"_": year + month}])
+            add_to_result("v304", year + month)
         elif year:
-            add_to_result("v304", [{"_": year}])
+            add_to_result("v304", year)
+        add_to_result("v305", obj.official.final_volume if obj.official.final_volume else None)
+        add_to_result("v306", obj.official.final_number if obj.official and obj.official.final_number else None)
 
-    add_to_result("v305", [{"_": obj.official.final_volume}] if obj.official and obj.official.final_volume else None)
-    add_to_result("v306", [{"_": obj.official.final_number}] if obj.official and obj.official.final_number else None)
+        issns = []
+        if obj.official.issn_print:
+            issns.append({"_": obj.official.issn_print, "t": "PRINT"})
+        if obj.official.issn_electronic:
+            issns.append({"_": obj.official.issn_electronic, "t": "ONLIN"})
+        result["435"] = issns
 
-    if publisher_exists:
-        add_to_result("v310", [{"_": publisher.institution.institution.location.country.name or publisher.institution.institution.location.country.acronym} 
-                                for publisher in obj.publisher_history.all() if publisher.institution and publisher.institution.institution.location and publisher.institution.institution.location.country])
-        add_to_result("v320", [{"_": publisher.institution.institution.location.state.acronym or publisher.institution.institution.location.state.name} 
-                                for publisher in obj.publisher_history.all() if publisher.institution and publisher.institution.institution.location and publisher.institution.institution.location.state])
-        add_to_result("v480", [{"_": publisher.institution.institution.institution_identification.name}
-                                for publisher in obj.publisher_history.all() if publisher.institution and publisher.institution.institution.location and publisher.institution.institution.location.country])
-        add_to_result("v490", [{"_": publisher.institution.institution.location.city.name} 
-                                for publisher in obj.publisher_history.all() if publisher.institution and publisher.institution.institution.location and publisher.institution.institution.location.city])
+        if obj.official.old_title.all():
+            add_items("v610", [old_title.title for old_title in obj.official.old_title.all()])
+        if obj.official.new_title:
+            add_to_result("v710", obj.official.new_title.title)
 
-    add_to_result("v330", [{"_": obj.level_of_publication}] if obj.level_of_publication else None)
-    add_to_result("v340", [{"_": obj.alphabet}] if obj.alphabet else None)
-    add_to_result("v350", [{"_": lang.code2} for lang in obj.text_language.all()] if obj.text_language.exists() else None)
-    add_to_result("v360", [{"_": lang.code2} for lang in obj.abstract_language.all()] if obj.abstract_language.exists() else None)
-    add_to_result("v380", [{"_": obj.frequency}] if obj.frequency else None)
+    if publisher_history:
+        add_to_result("v310",  [{"_": publisher.get_institution_country_name} for publisher in publisher_history if publisher.get_institution_country_name])
+        add_to_result("v320",  [{"_": publisher.get_instition_state_acronym} for publisher in publisher_history if publisher.get_instition_state_acronym])
+        add_to_result("v480",  [{"_": publisher.get_institution_name} for publisher in publisher_history if publisher.get_institution_name])
+        add_to_result("v490",  [{"_": publisher.get_institution_city_name} for publisher in publisher_history])
+
+    add_to_result("v330", obj.level_of_publication if obj.level_of_publication else None)
+    add_to_result("v340", obj.alphabet if obj.alphabet else None)
+    add_items("v350", [lang.code2 for lang in obj.text_language.all()])
+    add_items("v360", [lang.code2 for lang in obj.abstract_language.all()])
+    add_to_result("v380", obj.frequency if obj.frequency else None)
 
     medline_titles = TitleInDatabase.objects.filter(journal=obj, indexed_at__acronym__iexact="medline")
     if medline_titles.exists():
-        v420 = [{"_": medline.identifier} for medline in medline_titles if medline.identifier]
-        if v420:
-            add_to_result("v420", v420)
+        add_items("v420", [medline.identifier for medline in medline_titles])
         add_to_result("v421", [{"_": medline.title} for medline in medline_titles])
+    add_to_result("v430", obj.classification)
 
-    add_to_result("v430", [{"_": obj.classification}] if obj.classification else None)
+    add_items("v440", [descriptor.value for descriptor in obj.subject_descriptor.all()])
+    add_items("v441", [subject.value for subject in obj.subject.all()])
+    add_items("v450", [index.name for index in obj.indexed_at.all()])
 
-    issns = []
-    if obj.official and obj.official.issn_print:
-        issns.append({"_": obj.official.issn_print, "t": "PRINT"})
-    if obj.official and obj.official.issn_electronic:
-        issns.append({"_": obj.official.issn_electronic, "t": "ONLIN"})
-    add_to_result("v435", issns if issns else None)
+    add_to_result("v550", obj.has_supplement)
+    add_to_result("v560", obj.is_supplement)
 
-    add_to_result("v440", [{"_": descriptor.value} for descriptor in obj.subject_descriptor.all()] if obj.subject_descriptor.exists() else None)
-    add_to_result("v441", [{"_": subject.value} for subject in obj.subject.all()] if obj.subject.exists() else None)
-    add_to_result("v450", [{"_": index.name} for index in obj.indexed_at.all()] if obj.indexed_at.exists() else None)
+    add_items("v900", [annotation.notes for annotation in obj.annotation.all()])
+    result["v901"] = [{"l": mission.language.code2, "_": mission.get_text_pure} 
+                        for mission in obj.mission.all() if mission.language and mission.get_text_pure] if obj.mission else None
 
-    add_to_result("v550", [{"_": obj.has_supplement}] if obj.has_supplement else None)
-    add_to_result("v560", [{"_": obj.is_supplement}] if obj.is_supplement else None)
+    add_to_result("v940", obj.created)
+    add_to_result("v941", obj.updated)
 
-    if obj.official and obj.official.old_title:
-        add_to_result("v610", [{"_": old_title.title} for old_title in obj.official.old_title.all()])
-    if obj.official and obj.official.new_title:
-        add_to_result("v710", [{"_": obj.official.new_title.title}])
-
-    add_to_result("v900", [{"_": annotation.notes} for annotation in obj.annotation.all()] if obj.annotation.exists() else None)
-    add_to_result("v901", [{"l": mission.language.code2, "_": mission.get_text_pure} 
-                            for mission in obj.mission.all() if mission.language and mission.get_text_pure] if obj.mission else None)
-
-    result["v940"] = [{"_": obj.created}]
-    result["v941"] = [{"_": obj.updated}]
-    
-    # Ordena o dicionário por chave
-    result = dict(sorted(result.items()))
     return result
