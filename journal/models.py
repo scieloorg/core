0413de420f2411362da372b08449ce01ddd8cfa5
Samuel Veiga Rangel <samuelveigarangel@hotmail.com>
2025-02-09 22:27:53 -0300

    Realoca importacao de get_article_meta_format

--- journal/models.py
+++ journal/models.py
@@ -52,6 +52,7 @@ from location.models import Location
 from vocabulary.models import Vocabulary
 from thematic_areas.models import ThematicArea
 
+from .sources.api_article_meta import get_articlemeta_format
 from . import choices
 
 User = get_user_model()
@@ -905,7 +906,6 @@ class Journal(CommonControlField, ClusterableModel):
 
     @property
     def articlemeta_format(self):
-        from journal.sources.api_article_meta import get_articlemeta_format
         return get_articlemeta_format(self)
 
     base_form_class = CoreAdminModelForm
