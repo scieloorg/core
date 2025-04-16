a42ef5b9e14360b22de808a2c764a310f94f2f81
Samuel Veiga Rangel <samuelveigarangel@hotmail.com>
2025-04-16 12:02:36 -0300

    Cria modelos OrgLevel dinamicamente

--- organization/apps.py
+++ organization/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class OrganizationConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "organization"
+
+    def ready(self):
+        from . import dynamic_models
+        dynamic_models.create_all()
