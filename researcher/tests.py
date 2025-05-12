b8cbc6106d11167ee63dbb8c1278798074e911d7
Samuel Veiga Rangel <samuelveigarangel@hotmail.com>
2025-05-12 14:56:32 -0300

    Add test ResearcherOrcid and new tests to NewResearcher

--- researcher/tests.py
+++ researcher/tests.py
@@ -1,18 +1,18 @@
 from unittest.mock import patch
 
+from django.core.exceptions import ValidationError
 from django.test import SimpleTestCase, TestCase
 
 from core.models import Gender
 from core.users.models import User
-from institution.models import Institution
 from location.models import Location
 from organization.models import Organization
 from researcher.models import (
+    Affiliation,
     NewResearcher,
     PersonName,
     Researcher,
     ResearcherIds,
-    Affiliation,
     ResearcherOrcid,
 )
 
@@ -85,11 +85,15 @@ class NewResearcherTest(TestCase):
     def setUp(self):
         self.user = User.objects.create(username="teste", password="teste")
         # ParentalKey
-        self.researcher_id_orcid = ResearcherIds.get_or_create(
+        self.researcher_id_orcid = ResearcherOrcid.get_or_create(
             user=self.user,
-            identifier="0000-0002-9147-0547",
-            source_name="ORCID",
+            orcid="0000-0002-9147-0547",
         )
+        self.researcher_id_orcid_2 = ResearcherOrcid.get_or_create(
+            user=self.user,
+            orcid="0000-0002-9147-0548",
+        )        
+
         self.location = Location.create_or_update(
             user=self.user,
             city_name="Campinas",
@@ -101,6 +105,7 @@ class NewResearcherTest(TestCase):
             state_name=None,
         )
         self.organization = Organization.create_or_update(
+            user=self.user,
             name="Name of institution",
             acronym="Acronym of institution",
             url="www.teste.com.br",
@@ -108,44 +113,309 @@ class NewResearcherTest(TestCase):
             institution_type_mec="institution_type_mec",
             is_official=True,
         )
-
-    def test_researcher_id_create(self):
-        researcher_id = ResearcherIds.get_or_create(
+        self.organization_2 = Organization.create_or_update(
             user=self.user,
-            identifier="0000-0002-9147-0547",
-            source_name="ORCID",
+            name="Name of institution 2",
+            acronym="Acronym of institution 2",
+            url="www.teste2.com.br",
+            location=self.location,
+            institution_type_mec="institution_type_mec",
+            is_official=True,
         )
 
-        self.assertEqual(researcher_id.identifier, "0000-0002-9147-0547")
-        self.assertEqual(researcher_id.source_name, "ORCID")
+    @staticmethod
+    def get_or_create_researcher(user, given_names, last_name, suffix, orcid=None, affiliation=None):
+        return NewResearcher.get_or_create(
+            user=user,
+            given_names=given_names,
+            last_name=last_name,
+            suffix=suffix,
+            orcid=orcid,
+            affiliation=affiliation,
+        )
+
+    @staticmethod
+    def get_or_create_researcher_id(user, researcher, identifier, source_name):
+        return ResearcherIds.get_or_create(
+            user=user,
+            researcher=researcher,
+            identifier=identifier,
+            source_name=source_name,
+        )
+
+    def test_researcher_orcid_create_with_wrong_orcid(self):
+        with self.assertRaises(ValidationError):
+            ResearcherOrcid.get_or_create(
+                user=self.user,
+                orcid="0000-0002-9147",
+            )
 
     def test_new_researcher_create(self):
-        NewResearcher.get_or_create(
+        researcher = self.get_or_create_researcher(
             user=self.user,
-            given_names="Given names",
-            last_name="Last name",
-            suffix="Suffix",
+            given_names="Anna",
+            last_name="Carla",
+            suffix="Jr.",
             affiliation=self.organization,
-            researcher_identifier=self.researcher_id_orcid,
+            orcid=self.researcher_id_orcid,
         )
-
-        NewResearcher.get_or_create(
+        self.get_or_create_researcher_id(
             user=self.user,
-            given_names="Given names",
-            last_name="Last name",
-            suffix="Suffix",
-            affiliation=self.organization,
-            researcher_identifier=self.researcher_id_orcid,
+            researcher=researcher,
+            identifier="user.teste@dom.org",
+            source_name="EMAIL",
         )
         researcher = NewResearcher.objects.all()
         self.assertEqual(researcher.count(), 1)
-        self.assertEqual(researcher[0].given_names, "Given names")
-        self.assertEqual(researcher[0].last_name, "Last name")
-        self.assertEqual(researcher[0].suffix, "Suffix")
+        self.assertEqual(researcher[0].given_names, "Anna")
+        self.assertEqual(researcher[0].last_name, "Carla")
+        self.assertEqual(researcher[0].suffix, "Jr.")
         self.assertEqual(researcher[0].researcher_ids.count(), 1)
         self.assertEqual(
-            researcher[0].researcher_ids.first().identifier, "0000-0002-9147-0547"
+            researcher[0].researcher_ids.first().identifier, "user.teste@dom.org"
         )
+        self.assertEqual(researcher[0].orcid.orcid, "0000-0002-9147-0547")
+        self.assertEqual(researcher[0].affiliation, self.organization)
+
+    def test_researcher_id_create_with_wrong_email(self):
+        researcher = self.get_or_create_researcher(
+            user=self.user,
+            given_names="Anna",
+            last_name="Carla",
+            suffix="Jr.",
+            affiliation=self.organization,
+            orcid=self.researcher_id_orcid,
+        )
+
+        with self.assertRaises(ValidationError):
+            ResearcherIds.get_or_create(
+                user=self.user,
+                researcher=researcher,
+                identifier="user.teste",
+                source_name="EMAIL",
+            )
+
+    def test_researcher_id_create_with_wrong_lattes(self):
+        researcher = self.get_or_create_researcher(
+            user=self.user,
+            given_names="Anna",
+            last_name="Carla",
+            suffix="Jr.",
+            affiliation=self.organization,
+            orcid=self.researcher_id_orcid,
+        )
+
+        with self.assertRaises(ValidationError):
+            ResearcherIds.get_or_create(
+                user=self.user,
+                researcher=researcher,
+                identifier="user.teste",
+                source_name="EMAIL",
+            )
+
+    def test_new_researcher_create_two_times(self):
+        researcher_1 = self.get_or_create_researcher(
+            user=self.user,
+            given_names="Anna",
+            last_name="Carla",
+            suffix="Jr.",
+            affiliation=self.organization,
+            orcid=self.researcher_id_orcid,
+        )
+        researcher_2 = self.get_or_create_researcher(
+            user=self.user,
+            given_names="Anna",
+            last_name="Carla",
+            suffix="Jr.",
+            affiliation=self.organization,
+            orcid=self.researcher_id_orcid,
+        )
+        researcher_id_email_1 = self.get_or_create_researcher_id(
+            user=self.user,
+            researcher=researcher_1,
+            identifier="user.teste@dom.org",
+            source_name="EMAIL",
+        )
+        researcher_id_email_2 = self.get_or_create_researcher_id(
+            user=self.user,
+            researcher=researcher_2,
+            identifier="user.teste@dom.org",
+            source_name="EMAIL",
+        )
+
+        researcher = NewResearcher.objects.all()
+        self.assertEqual(researcher.count(), 1)
+        self.assertEqual(researcher[0].given_names, "Anna")
+        self.assertEqual(researcher[0].last_name, "Carla")
+        self.assertEqual(researcher[0].suffix, "Jr.")
+        self.assertEqual(researcher[0].researcher_ids.count(), 1)
+        self.assertEqual(
+            researcher[0].researcher_ids.first().identifier, "user.teste@dom.org"
+        )
+        self.assertEqual(researcher[0].orcid.orcid, "0000-0002-9147-0547")
+        self.assertEqual(researcher[0].affiliation, self.organization)
+        self.assertEqual(researcher[0].affiliation, self.organization)
+        self.assertEqual(ResearcherIds.objects.all().count(), 1)
+        self.assertEqual(researcher_id_email_1, researcher_id_email_2)
+        self.assertEqual(researcher_1, researcher_2)
+
+    def test_new_researcher_create_without_orcid(self):
+        researcher = self.get_or_create_researcher(
+            user=self.user,
+            given_names="Anna",
+            last_name="Carla",
+            suffix="Jr.",
+            affiliation=self.organization,
+        )
+        self.get_or_create_researcher_id(
+            user=self.user,
+            researcher=researcher,
+            identifier="user.teste@dom.org",
+            source_name="EMAIL",
+        )
+        researcher = NewResearcher.objects.all()
+        self.assertEqual(researcher.count(), 1)
+        self.assertEqual(researcher[0].given_names, "Anna")
+        self.assertEqual(researcher[0].last_name, "Carla")
+        self.assertEqual(researcher[0].suffix, "Jr.")
+        self.assertEqual(researcher[0].researcher_ids.count(), 1)
+        self.assertEqual(
+            researcher[0].researcher_ids.first().identifier, "user.teste@dom.org"
+        )
+        self.assertEqual(researcher[0].orcid, None)
+
+    def test_new_researcher_create_two_times_different_name_same_orcid(self):
+        researcher_1 = self.get_or_create_researcher(
+            user=self.user,
+            given_names="Anna",
+            last_name="Carla",
+            suffix="Jr.",
+            affiliation=self.organization,
+            orcid=self.researcher_id_orcid,
+        )
+        self.get_or_create_researcher_id(
+            user=self.user,
+            researcher=researcher_1,
+            identifier="user.teste@dom.org",
+            source_name="EMAIL",
+        )
+        researcher_2 = self.get_or_create_researcher(
+            user=self.user,
+            given_names="Anna2",
+            last_name="Carla2",
+            suffix="Jr2.",
+            affiliation=self.organization_2,
+            orcid=self.researcher_id_orcid,
+        )
+        self.get_or_create_researcher_id(
+            user=self.user,
+            researcher=researcher_2,
+            identifier="user.teste2@dom.org",
+            source_name="EMAIL",
+        )        
+        researcher = NewResearcher.objects.all()
+        self.assertEqual(researcher.count(), 2)
+        self.assertEqual(researcher[0].given_names, "Anna")
+        self.assertEqual(researcher[0].last_name, "Carla")
+        self.assertEqual(researcher[0].suffix, "Jr.")
+        self.assertEqual(researcher[0].researcher_ids.count(), 1)
+        self.assertEqual(
+            researcher[0].researcher_ids.first().identifier, "user.teste@dom.org"
+        )
+        self.assertEqual(researcher[0].orcid.orcid, "0000-0002-9147-0547")
+        self.assertEqual(researcher[1].given_names, "Anna2")
+        self.assertEqual(researcher[1].last_name, "Carla2")
+        self.assertEqual(researcher[1].suffix, "Jr2.")
+        self.assertEqual(researcher[1].researcher_ids.count(), 1)
+        self.assertEqual(
+            researcher[1].researcher_ids.first().identifier, "user.teste2@dom.org"
+        )
+        self.assertEqual(researcher[0].orcid.orcid, researcher[1].orcid.orcid) 
+
+    def test_new_researcher_create_two_times_same_name_different_orcid(self):
+        researcher_1 = self.get_or_create_researcher(
+            user=self.user,
+            given_names="Anna",
+            last_name="Carla",
+            suffix="Jr.",
+            affiliation=self.organization,
+            orcid=self.researcher_id_orcid,
+        )
+        self.get_or_create_researcher_id(
+            user=self.user,
+            researcher=researcher_1,
+            identifier="user.teste@dom.org",
+            source_name="EMAIL",
+        )
+        researcher_2 = self.get_or_create_researcher(
+            user=self.user,
+            given_names="Anna",
+            last_name="Carla",
+            suffix="Jr.",
+            affiliation=self.organization_2,
+            orcid=self.researcher_id_orcid_2,
+        )
+        self.get_or_create_researcher_id(
+            user=self.user,
+            researcher=researcher_2,
+            identifier="user.teste@dom.org",
+            source_name="EMAIL",
+        )
+        researcher = NewResearcher.objects.all()
+        self.assertEqual(researcher.count(), 2)
+        self.assertEqual(researcher[0].given_names, "Anna")
+        self.assertEqual(researcher[0].last_name, "Carla")
+        self.assertEqual(researcher[0].suffix, "Jr.")
+        self.assertEqual(researcher[0].researcher_ids.count(), 1)
+        self.assertEqual(researcher[0].orcid.orcid, "0000-0002-9147-0547")
+        self.assertEqual(
+            researcher[0].researcher_ids.first().identifier, "user.teste@dom.org"
+        )
+        self.assertEqual(researcher[1].given_names, "Anna")
+        self.assertEqual(researcher[1].last_name, "Carla")
+        self.assertEqual(researcher[1].suffix, "Jr.")
+        self.assertEqual(researcher[1].researcher_ids.count(), 1)
+        self.assertEqual(
+            researcher[1].researcher_ids.first().identifier, "user.teste@dom.org"
+        )
+        self.assertEqual(researcher[1].orcid.orcid, "0000-0002-9147-0548")
+        self.assertNotEqual(researcher[0], researcher[1])
+        
+    def test_new_researcher_create_without_orcid_same_affiliation(self):
+        researcher_1 = self.get_or_create_researcher(
+            user=self.user,
+            given_names="Anna",
+            last_name="Carla",
+            suffix="Jr.",
+            affiliation=self.organization,
+        )
+        self.get_or_create_researcher_id(
+            user=self.user,
+            researcher=researcher_1,
+            identifier="user.test@dom.org",
+            source_name="EMAIL",
+        )
+        researcher_2 = self.get_or_create_researcher(
+            user=self.user,
+            given_names="Anna",
+            last_name="Carla",
+            suffix="Jr.",
+            affiliation=self.organization,
+        )
+        researcher = NewResearcher.objects.all()
+        self.assertEqual(researcher.count(), 1)
+        self.assertEqual(researcher[0].given_names, "Anna")
+        self.assertEqual(researcher[0].last_name, "Carla")
+        self.assertEqual(researcher[0].suffix, "Jr.")
+        self.assertEqual(researcher[0].researcher_ids.count(), 1)
+        self.assertEqual(
+            researcher[0].researcher_ids.first().identifier, "user.test@dom.org"
+        )
+        self.assertEqual(researcher[0].orcid, None)
+        self.assertEqual(researcher_1, researcher_2)
+        self.assertEqual(researcher_1.affiliation, self.organization)
+        self.assertEqual(researcher_1.affiliation, researcher_2.affiliation)
+        self.assertEqual(researcher_1, researcher_2)
 
 
 class MigrationResearcherTest(TestCase):
