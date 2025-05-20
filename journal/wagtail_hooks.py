836ff6e6f4c4aef65ab92283a19edb169e131fa1
Samuel Veiga Rangel <samuelveigarangel@hotmail.com>
2025-05-19 23:28:54 -0300

    Delete ModelAdmin Journals

--- journal/wagtail_hooks.py
+++ journal/wagtail_hooks.py
@@ -18,7 +18,8 @@ COLLECTION_TEAM = "Collection Team"
 JOURNAL_TEAM = "Journal Team"
 
 from wagtail.snippets.models import register_snippet
-from wagtail.snippets.views.snippets import SnippetViewSet, CreateView, SnippetViewSetGroup, EditView
+from wagtail.snippets.views.snippets import SnippetViewSet, CreateView, SnippetViewSetGroup
+
 
 class OfficialJournalCreateViewSnippet(CreateView):
     def form_valid(self, form):
@@ -54,32 +55,31 @@ class OfficialJournalSnippetViewSet(SnippetViewSet):
         "issnl",
     )
 
-class JournalEditViewSnippet(EditView):
-    def form_valid(self, form):
-        self.object = form.save_all(self.request.user)
-        return HttpResponseRedirect(self.get_success_url())
+# class JournalEditViewSnippet(SnippetEditView):
+#     def form_valid(self, form):
+#         self.object = form.save_all(self.request.user)
+#         return HttpResponseRedirect(self.get_success_url())
 
-    def get_edit_handler(self):
-        """
-        Sobrescreve o método 'get_edit_handler' de EditView.
-        Verifica se o usuário tem permissão para editar o campo. Caso não tenha, os campos que não possuem a permissão
-        'journal.can_edit_{field_name}' no 'user_permissions' serão configurados como somente leitura.
-        Isso é aplicado para 'FieldPanel', 'AutocompletePanel'
-        e 'InlinePanel'.
-        """
-        edit_handler = super().get_edit_handler()
-        print(edit_handler)
-        # user_permissions = self.request.user.get_all_permissions()
-        # if not self.request.user.is_superuser:
-        #     for object_list in edit_handler.children:
-        #         for field in object_list.children:
-        #             if isinstance(field, FieldPanel) and f"journal.can_edit_{field.field_name}" not in user_permissions:
-        #                 field.__setattr__('read_only', True)
-        #             elif isinstance(field, InlinePanel) and f"journal.can_edit_{field.relation_name}" not in user_permissions:
-        #                 field.classname = field.classname + ' read-only-inline-panel'
-        #                 for inline_field in field.panel_definitions:
-        #                     inline_field.__setattr__('read_only', True)
-        return edit_handler
+#     def get_edit_handler(self):
+#         """
+#         Sobrescreve o método 'get_edit_handler' de EditView.
+#         Verifica se o usuário tem permissão para editar o campo. Caso não tenha, os campos que não possuem a permissão
+#         'journal.can_edit_{field_name}' no 'user_permissions' serão configurados como somente leitura.
+#         Isso é aplicado para 'FieldPanel', 'AutocompletePanel'
+#         e 'InlinePanel'.
+#         """
+#         edit_handler = super().get_edit_handler()
+#         user_permissions = self.request.user.get_all_permissions()
+#         if not self.request.user.is_superuser:
+#             for object_list in edit_handler.children:
+#                 for field in object_list.children:
+#                     if isinstance(field, FieldPanel) and f"journal.can_edit_{field.field_name}" not in user_permissions:
+#                         field.__setattr__('read_only', True)
+#                     elif isinstance(field, InlinePanel) and f"journal.can_edit_{field.relation_name}" not in user_permissions:
+#                         field.classname = field.classname + ' read-only-inline-panel'
+#                         for inline_field in field.panel_definitions:
+#                             inline_field.__setattr__('read_only', True)
+#         return edit_handler
     
 class JournalCreateView(CreateView):
     def form_valid(self, form):
@@ -92,7 +92,6 @@ class JournalAdminSnippetViewSet(SnippetViewSet):
     inspect_view_enabled = True
     menu_label = _("Journals")
     add_view_class = JournalCreateView
-    edit_view_class = JournalEditViewSnippet
     menu_icon = "folder"
     menu_order = get_menu_order("journal")
     add_to_settings_menu = False
@@ -174,169 +173,6 @@ class JournalSnippetViewSetGroup(SnippetViewSetGroup):
     items = (JournalAdminSnippetViewSet, OfficialJournalSnippetViewSet, SciELOJournalAdminViewSet)
 
 register_snippet(JournalSnippetViewSetGroup)
-
-
-class OfficialJournalCreateView(CreateView):
-    def form_valid(self, form):
-        self.object = form.save_all(self.request.user)
-        return HttpResponseRedirect(self.get_success_url())
-
-
-class OfficialJournalAdmin(ModelAdmin):
-    model = models.OfficialJournal
-    inspect_view_enabled = True
-    menu_label = _("ISSN Journals")
-    create_view_class = OfficialJournalCreateView
-    menu_icon = "folder"
-    menu_order = 100
-    add_to_settings_menu = False
-    exclude_from_explorer = False
-
-    list_display = (
-        "title",
-        "initial_year",
-        "terminate_year",
-        "issn_print",
-        "issn_print_is_active",
-        "issn_electronic",
-        "created",
-        "updated",
-    )
-    list_filter = ("issn_print_is_active", "terminate_year", "initial_year", )
-    search_fields = (
-        "title",
-        "initial_year",
-        "issn_print",
-        "issn_electronic",
-        "issnl",
-    )
-
-
-class JournalCreateView(CreateView):
-    def form_valid(self, form):
-        self.object = form.save_all(self.request.user)
-        return HttpResponseRedirect(self.get_success_url())
-
-from wagtail.admin.panels import FieldPanel, InlinePanel
-
-
-class JournalEditView(EditView):
-    readonly_fields = ['official', 'mission']
-    def form_valid(self, form):
-        self.object = form.save_all(self.request.user)
-        return HttpResponseRedirect(self.get_success_url())
-
-    def get_edit_handler(self):
-        """
-        Sobrescreve o método 'get_edit_handler' de EditView.
-        Verifica se o usuário tem permissão para editar o campo. Caso não tenha, os campos que não possuem a permissão
-        'journal.can_edit_{field_name}' no 'user_permissions' serão configurados como somente leitura.
-        Isso é aplicado para 'FieldPanel', 'AutocompletePanel'
-        e 'InlinePanel'.
-        """
-        edit_handler = super().get_edit_handler()
-        user_permissions = self.request.user.get_all_permissions()
-        if not self.request.user.is_superuser:
-            for object_list in edit_handler.children:
-                for field in object_list.children:
-                    if isinstance(field, FieldPanel) and f"journal.can_edit_{field.field_name}" not in user_permissions:
-                        field.__setattr__('read_only', True)
-                    elif isinstance(field, InlinePanel) and f"journal.can_edit_{field.relation_name}" not in user_permissions:
-                        field.classname = field.classname + ' read-only-inline-panel'
-                        for inline_field in field.panel_definitions:
-                            inline_field.__setattr__('read_only', True)
-        return edit_handler
-
-
-class JournalAdmin(ModelAdmin):
-    model = models.Journal
-    inspect_view_enabled = True
-    menu_label = _("Journals")
-    create_view_class = JournalCreateView
-    edit_view_class = JournalEditView
-    menu_icon = "folder"
-    menu_order = get_menu_order("journal")
-    add_to_settings_menu = False
-    exclude_from_explorer = False
-    list_per_page = 20
-
-    list_display = (
-        "title",
-        "contact_location",
-        "valid",
-        "created",
-        "updated",
-    )
-    list_filter = (
-        "valid",
-        "use_license",
-        "publishing_model",
-        "subject",
-        "main_collection",
-    )
-    search_fields = (
-        "title",
-        "official__issn_print",
-        "official__issn_electronic",
-        "contact_location__country__name",
-    )
-    def get_queryset(self, request):
-        qs = super().get_queryset(request)
-        user_groups = request.user.groups.values_list('name', flat=True)
-        if COLLECTION_TEAM in user_groups:
-            return qs.filter(scielojournal__collection__in=request.user.collection.all())
-        elif JOURNAL_TEAM in user_groups:
-            return qs.filter(id__in=request.user.journal.all().values_list("id", flat=True))
-        return qs
-    
-
-class SciELOJournalCreateView(CreateView):
-    def form_valid(self, form):
-        self.object = form.save_all(self.request.user)
-        return HttpResponseRedirect(self.get_success_url())
-
-
-class SciELOJournalAdmin(ModelAdmin):
-    model = models.SciELOJournal
-    inspect_view_enabled = True
-    menu_label = _("SciELO Journals")
-    create_view_class = SciELOJournalCreateView
-    menu_icon = "folder"
-    menu_order = 200
-    add_to_settings_menu = False
-    exclude_from_explorer = False
-
-    list_display = (
-        "custom_journal",
-        "issn_scielo",
-        "journal_acron",
-        "collection",
-        "status",
-        "created",
-        "updated",
-    )
-    list_filter = ("status", "collection", )
-    search_fields = (
-        "journal_acron",
-        "journal__title",
-        "issn_scielo",
-    )
-
-    def get_queryset(self, request):
-        qs = super().get_queryset(request)
-        user_groups = request.user.groups.values_list('name', flat=True)
-        if COLLECTION_TEAM in user_groups:
-            return qs.filter(collection__in=request.user.collection.all())
-        elif JOURNAL_TEAM in user_groups:
-            return qs.filter(id__in=request.user.journal.all().values_list("id", flat=True))
-        return qs
-
-    def custom_journal(self, obj):
-        return f"{obj.journal.title}" or f"{obj.journal.official}"
-    custom_journal.short_description = "Journal"
-    custom_journal.admin_order_field = "journal"
-
-
 
 
 class TOCSectionAdmin(ModelAdmin):
@@ -484,16 +320,6 @@ class ArticleSubmissionFormatCheckListAdmin(ModelAdmin):
     menu_order = get_menu_order("article_subm")
 
 # modeladmin_register(ArticleSubmissionFormatCheckListAdmin)
-
-
-class JournalAdminGroup(ModelAdminGroup):
-    menu_label = _("Journals")
-    menu_icon = "folder-open-inverse"  # change as required
-    menu_order = get_menu_order("journal")
-    items = (JournalAdmin, OfficialJournalAdmin, SciELOJournalAdmin, AMJournalAdmin, TOCSectionAdmin)
-
-
-modeladmin_register(JournalAdminGroup)
 
 
 class OwnerAdmin(ModelAdmin):
