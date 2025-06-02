5629731983da68a1da73a588aacdba5658bf07f2
Samuel Veiga Rangel <samuelveigarangel@hotmail.com>
2025-06-02 13:11:42 -0300

    change name to reflect  functions

--- core/utils/api_articlemeta_format.py
+++
@@ -1,9 +0,0 @@
-def add_items(key, items, dictonary):
-    for item in items:
-        add_to_result(key, item, dictonary)
-
-def add_to_result(key, value, dictonary):
-    if value:
-        if key not in dictonary:
-            dictonary[key] = []
-        dictonary[key].append({"_": value})
