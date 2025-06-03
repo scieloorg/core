df1bd00981b4c32a246af1a8e92e87d642b0d75d
Samuel Veiga Rangel <samuelveigarangel@hotmail.com>
2025-06-03 11:16:18 -0300

    Improve queries in API ViewSet

--- journal/api/v1/views.py
+++ journal/api/v1/views.py
@@ -15,18 +15,8 @@ class GenericJournalViewSet(viewsets.ModelViewSet):
 
 class JournalViewSet(GenericJournalViewSet):
     def get_queryset(self):
-        issn = self.request.query_params.get("issn")
-        title = self.request.query_params.get("title")
-        issn_print = self.request.query_params.get("issn_print")
-        issn_electronic = self.request.query_params.get("issn_electronic")
-        issnl = self.request.query_params.get("issnl")
-        thematic_areas = self.request.query_params.get("thematic_areas")
-        toc_item = self.request.query_params.get("toc_item")
-        from_date_created = self.request.query_params.get("from_date_created")
-        until_date_created = self.request.query_params.get("until_date_created")
-        from_date_updated = self.request.query_params.get("from_date_updated")
-        until_date_updated = self.request.query_params.get("until_date_updated")
-
+        query_params = self.request.query_params
+        
         # funcao para permitir apenas estes paramentros
         validate_params(
             self.request,
@@ -47,30 +37,34 @@ class JournalViewSet(GenericJournalViewSet):
 
         queryset = super().get_queryset()
 
-        params = {}
-        if issn:
-            params["scielojournal__issn_scielo"] = issn
-        if issn_electronic:
-            params["official__issn_electronic"] = issn_electronic
-        if issn_print:
-            params["official__issn_print"] = issn_print
-        if issnl:
-            params["official__issnl"] = issnl
-        if title:
-            params["title"] = title
-        if toc_item:
-            params["journaltocsection__toc_items__text"] = toc_item
-        if thematic_areas:
-            params["subject__value__in"] = thematic_areas.split(",")
-        if collection_acron:
-            params["scielojournal__collection__acron3"] = collection_acron
-        if from_date_created:
-            params["created__gte"] = from_date_created.replace("/", "-")
-        if until_date_created:
-            params["created__lte"] = until_date_created.replace("/", "-")
-        if from_date_updated:
-            params["updated__gte"] = from_date_updated.replace("/", "-")
-        if until_date_updated:
-            params["updated__lte"] = until_date_updated.replace("/", "-")
+        if issn := query_params.get("issn"):
+           queryset = queryset.filter(scielojournal__issn_scielo=issn)
+        if issn_electronic := query_params.get("issn_electronic"):
+            queryset = queryset.filter(official__issn_electronic=issn_electronic)
+        if issn_print := query_params.get("issn_print"):
+            queryset = queryset.filter(official__issn_print=issn_print)
+        if issnl := query_params.get("issnl"):
+            queryset = queryset.filter(official__issnl=issnl)
+        if title := query_params.get("title"):
+            queryset = queryset.filter(title=title)
+        if toc_item := query_params.get("toc_item"):
+            queryset = queryset.filter(journaltocsection__toc_items__text=toc_item)
+        if thematic_areas := query_params.get("thematic_areas"):
+            queryset = queryset.filter(subject__value__in=thematic_areas.split(","))
+        if collection_acron := query_params.get("collection_acron"):
+            queryset = queryset.filter(scielojournal__collection__acron3=collection_acron)
 
-        return queryset.filter(**params)
+        for date_param, filter_key in [
+            ("from_date_created", "created__gte"),
+            ("until_date_created", "created__lte"),
+            ("from_date_updated", "updated__gte"),
+            ("until_date_updated", "updated__lte"),
+        ]:
+            if data_value := query_params.get(date_param):
+                try:
+                    formated_date = data_value.replace("/", "-")
+                    queryset = queryset.filter(**{filter_key: formated_date})
+                except (ValueError, AttributeError):
+                    continue
+
+        return queryset
