8426f73e9af9763c7fc0d187e5be3930c3682b35
Rossi-Luciano <luciano.rossi.lucross@gmail.com>
2025-06-10 11:30:32 -0300

    Adiciona conteúdo em 'view'

--- article/views.py
+++ article/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.utils.translation import gettext as _
+from wagtail_modeladmin.views import EditView
 
-# Create your views here.
+
+class ArticleFormatView(EditView):
+    def form_valid(self, form):
+        self.object = form.save_all(self.request.user)
+        return HttpResponseRedirect(self.get_success_url())
+
