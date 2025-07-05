f748e0829a00b2c4232be68498a7189cf23166e2
Samuel Veiga Rangel <samuelveigarangel@hotmail.com>
2025-07-05 12:24:09 -0300

    Changes get_queryset to pass acronym collection to get_articlemeta_format_issue

--- issue/api/v1/views.py
+++ issue/api/v1/views.py
@@ -1,11 +1,20 @@
-from rest_framework.exceptions import ValidationError
-
-from django.db.models import Q
-from rest_framework import viewsets
+from django.db.models import F, Q
+from rest_framework import serializers, viewsets
 
 from article import models
+from core.utils.utils import formated_date_api_params
 from core.validators import validate_params
+
 from .serializers import IssueSerializer
+
+
+class ArticleMetaFormatIssueSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Issue
+    
+    def to_representation(self, instance):
+        collection = getattr(instance, "collection_acron", None)
+        return instance.articlemeta_format(collection)
 
 
 class GenericIssueViewSet(viewsets.ModelViewSet):
@@ -36,40 +45,63 @@ class IssueViewSet(GenericIssueViewSet):
             "from_date_updated",
             "until_date_updated",
             "issn_print",
-            "issn_electronic",  
+            "issn_electronic",
+            "issn",
             "volume", 
             "number",
             "supplement",
             "page",
+            "formats",
+            "code",
             "",
         )
         queryset = super().get_queryset()
 
+        params = {}
+
         if collection := query_params.get("collection"):
-            queryset = queryset.filter(journal__scielojournal__collection__acron3=collection)
+            params["journal__scielojournal__collection__acron3"] = collection
         if from_publication_year := query_params.get("from_publication_year"):
-            queryset = queryset.filter(year__gte=from_publication_year)
+            params["year__gte"] = from_publication_year
         if until_publication_year := query_params.get("until_publication_year"):
-            queryset = queryset.filter(year__lte=until_publication_year)
-        if from_date_created := query_params.get("from_date_created"):
-            queryset = queryset.filter(created__gte=from_date_created.replace("/", "-"))
-        if until_date_created := query_params.get("until_date_created"):
-            queryset = queryset.filter(created__lte=until_date_created.replace("/", "-"))
-        if from_date_updated := query_params.get("from_date_updated"):
-            queryset = queryset.filter(updated__gte=from_date_updated.replace("/", "-"))
-        if until_date_updated := query_params.get("until_date_updated"):
-            queryset = queryset.filter(updated__lte=until_date_updated.replace("/", "-"))
-        if issn_print := query_params.get("issn_print"):
-            queryset = queryset.filter(journal__official__issn_print=issn_print)
-        if issn_electronic := query_params.get("issn_electronic"):
-            queryset = queryset.filter(journal__official__issn_electronic=issn_electronic)
+            params["year__lte"] = until_publication_year
         if volume := query_params.get("volume"):
-            queryset = queryset.filter(volume=volume)
+            params["volume"] = volume
         if number := query_params.get("number"):
-            queryset = queryset.filter(number=number)
+            params["number"] = number
         if supplement := query_params.get("supplement"):
-            queryset = queryset.filter(supplement=supplement)
+            params["supplement"] = supplement
+        if code := query_params.get("code"):
+            params["article__pid_v2"] = code
 
-        return queryset 
+        issn = query_params.get("issn")
+        issn_print = query_params.get("issn_print")
+        issn_electronic = query_params.get("issn_electronic")
 
-    
+        if issn:
+            queryset = queryset.filter(
+                Q(journal__official__issn_print=issn) | Q(journal__official__issn_electronic=issn)
+            )
+        else:
+            if issn_print:
+                params["journal__official__issn_print"] = issn_print
+            if issn_electronic:
+                params["journal__official__issn_electronic"] = issn_electronic
+
+        queryset = queryset.filter(**params)
+
+        dates = formated_date_api_params(query_params)
+        queryset = queryset.filter(**dates)
+        if query_params.get("formats") == "articlemeta":
+            return queryset.filter(journal__scielojournal__journal__isnull=False
+            ).annotate(
+                collection_acron=F('journal__scielojournal__collection__acron3')
+            ).order_by('created').distinct()
+
+        return queryset
+
+    def get_serializer_class(self):
+        format_param = self.request.query_params.get("formats")
+        if format_param == "articlemeta":
+            return ArticleMetaFormatIssueSerializer
+        return IssueSerializer
