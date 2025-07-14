899f6065a0dd2ca1106b22b0d503056799ad7c7a
Samuel Veiga Rangel <samuelveigarangel@hotmail.com>
2025-07-14 14:58:14 -0300

    fix templatetag

--- core/home/templatetags/get_journals_by_name_institutiton.py
+++ core/home/templatetags/get_journals_by_name_institutiton.py
@@ -6,4 +6,5 @@ register = template.Library()
 
 @register.filter(name='get_journals_by_name_institutiton')
 def get_journals_by_name_institutiton(value):
-    return Journal.objects.filter(publisher_history__institution__institution__institution_identification__name__icontains=value)
+    if value:
+        return Journal.objects.filter(publisher_history__institution__institution__institution_identification__name__icontains=value)
