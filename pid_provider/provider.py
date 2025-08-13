04bed1ce8b4e60fa7261759ed2be307728741d34
Roberta Takenaka <roberta.takenaka@scielo.org>
2025-08-13 13:19:38 -0300

    feat: adiciona profiling aos métodos do PidProvider
    - Aplica @profile_staticmethod em get_xmltree
    - Aplica @profile_staticmethod em get_sps_pkg_name
    - Aplica @profile_method em fix_pid_v2
    - Importa decoradores de profiling

--- pid_provider/provider.py
+++ pid_provider/provider.py
@@ -1,5 +1,4 @@
-from django.db.models import Q
-
+from core.utils.profiling_tools import profile_staticmethod, profile_method
 from pid_provider.base_pid_provider import BasePidProvider
 from pid_provider.models import PidProviderXML
 
@@ -10,6 +9,7 @@ class PidProvider(BasePidProvider):
     """
 
     @staticmethod
+    @profile_staticmethod
     def get_xmltree(pid_v3):
         try:
             return PidProviderXML.get_xml_with_pre(pid_v3).xmltree
@@ -17,11 +17,13 @@ class PidProvider(BasePidProvider):
             return None
 
     @staticmethod
+    @profile_staticmethod
     def get_sps_pkg_name(pid_v3):
         try:
             return PidProviderXML.get_xml_with_pre(pid_v3).sps_pkg_name
         except (PidProviderXML.DoesNotExist, AttributeError):
             return None
 
+    @profile_method
     def fix_pid_v2(self, user, pid_v3, correct_pid_v2):
         return PidProviderXML.fix_pid_v2(user, pid_v3, correct_pid_v2)
