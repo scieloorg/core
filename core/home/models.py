2ecc380b6c61facc677f6ccf367c3721dbd79162
Samuel Veiga Rangel <samuelveigarangel@hotmail.com>
2025-09-04 11:57:56 -0300

    black

--- core/home/models.py
+++ core/home/models.py
@@ -29,33 +29,71 @@ def get_page_about():
             # Fallback para locale padrão
             locale = Locale.get_default()
         home_page = HomePage.objects.filter(locale=locale).first()
-        page_about = home_page.get_children().live().public().type(AboutScieloOrgPage).filter(locale=locale).first()
+        page_about = (
+            home_page.get_children()
+            .live()
+            .public()
+            .type(AboutScieloOrgPage)
+            .filter(locale=locale)
+            .first()
+        )
     except (Page.DoesNotExist, Locale.DoesNotExist, Page.MultipleObjectsReturned):
         page_about = Page.objects.filter(slug="sobre-o-scielo").first()
     return page_about
 
+
 def as_item(qs, lang_code):
     return [{"obj": obj, "name": obj.get_name_for_language(lang_code)} for obj in qs]
 
+
 class HomePage(Page):
-    subpage_types = ['home.AboutScieloOrgPage', 'home.ListPageJournal', 'home.ListPageJournalByPublisher']
+    subpage_types = [
+        "home.AboutScieloOrgPage",
+        "home.ListPageJournal",
+        "home.ListPageJournalByPublisher",
+    ]
 
     def get_context(self, request, *args, **kwargs):
         context = super().get_context(request, *args, **kwargs)
         lang_code = get_language()
         lang_code = "pt" if lang_code == "pt-br" else lang_code
-        collections = Collection.objects.filter(domain__isnull=False, is_active=True, collection_name__language__code2=lang_code).order_by("collection_name__text").prefetch_related("collection_name")
+        collections = (
+            Collection.objects.filter(
+                domain__isnull=False,
+                is_active=True,
+                collection_name__language__code2=lang_code,
+            )
+            .order_by("collection_name__text")
+            .prefetch_related("collection_name")
+        )
         children_qs = self.get_children().live().specific()
-        
-        context["collections_journals"] = as_item(qs=collections.filter(Q(status="certified")), lang_code=lang_code)
-        context["collections_in_development"] = as_item(qs=collections.filter(Q(status="development")), lang_code=lang_code)
-        context["collections_servers_and_repositorios"] = as_item(qs=collections.filter((Q(collection_type="repositories") | Q(collection_type="preprints"))), lang_code=lang_code)
-        context["collections_books"] = as_item(qs=collections.filter(Q(collection_type="books")), lang_code=lang_code)
-        context["collections_others"] = as_item(qs=collections.filter(Q(status="diffusion")), lang_code=lang_code)
+
+        context["collections_journals"] = as_item(
+            qs=collections.filter(Q(status="certified")), lang_code=lang_code
+        )
+        context["collections_in_development"] = as_item(
+            qs=collections.filter(Q(status="development")), lang_code=lang_code
+        )
+        context["collections_servers_and_repositorios"] = as_item(
+            qs=collections.filter(
+                (Q(collection_type="repositories") | Q(collection_type="preprints"))
+            ),
+            lang_code=lang_code,
+        )
+        context["collections_books"] = as_item(
+            qs=collections.filter(Q(collection_type="books")), lang_code=lang_code
+        )
+        context["collections_others"] = as_item(
+            qs=collections.filter(Q(status="diffusion")), lang_code=lang_code
+        )
         context["categories"] = [item[0] for item in STUDY_AREA]
         context["page_about"] = get_page_about()
-        context["list_journal_pages"] =[p for p in children_qs if isinstance(p, ListPageJournal)]
-        context["list_journal_by_publisher_pages"] =[p for p in children_qs if isinstance(p, ListPageJournalByPublisher)]
+        context["list_journal_pages"] = [
+            p for p in children_qs if isinstance(p, ListPageJournal)
+        ]
+        context["list_journal_by_publisher_pages"] = [
+            p for p in children_qs if isinstance(p, ListPageJournalByPublisher)
+        ]
 
         return context
 
@@ -149,18 +187,20 @@ class FAQItemBlock(blocks.StructBlock):
 
 
 class AboutScieloOrgPage(Page):
-    subpage_types = ['home.AboutScieloOrgPage']
+    subpage_types = ["home.AboutScieloOrgPage"]
 
     body = RichTextField(_("Body"), blank=True)
-    external_link = models.URLField(_("Link externo"), blank=True, null=True, max_length=2000)
+    external_link = models.URLField(
+        _("Link externo"), blank=True, null=True, max_length=2000
+    )
 
     attached_document = models.ForeignKey(
-        'wagtaildocs.Document',
+        "wagtaildocs.Document",
         null=True,
         blank=True,
         on_delete=models.SET_NULL,
-        related_name='+',
-        help_text=_("Documento principal desta página")
+        related_name="+",
+        help_text=_("Documento principal desta página"),
     )
 
     list_page = StreamField(
