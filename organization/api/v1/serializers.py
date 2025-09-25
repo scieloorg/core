cf7b1a182aaab9b623e3c8a548d39bf0e36554fc
Samuel Veiga Rangel <samuelveigarangel@hotmail.com>
2025-09-25 14:29:22 -0300

    Adiciona url e logo_url

--- organization/api/v1/serializers.py
+++ organization/api/v1/serializers.py
@@ -1,9 +1,17 @@
 from rest_framework import serializers
 from organization import models
-
+from wagtail.models.sites import Site
 
 class OrganizationSerializer(serializers.ModelSerializer):
     location = serializers.SerializerMethodField()
+    logo_url = serializers.SerializerMethodField()
+
+    def get_logo_url(self, obj):
+        if obj.logo:
+            domain = Site.objects.get(is_default_site=True).hostname
+            domain = f"http://{domain}"
+            return f"{domain}{obj.logo.url}"
+        return None
 
     def get_location(self, obj):
         if obj.location:
@@ -14,5 +22,7 @@ class OrganizationSerializer(serializers.ModelSerializer):
         fields = [
             "name",
             "acronym",
+            "url",
+            "logo_url",
             "location",
         ]
