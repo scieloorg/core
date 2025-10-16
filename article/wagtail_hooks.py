5ac427cee6461de832c3724acb7ffa3774b937ef
Roberta Takenaka <roberta.takenaka@scielo.org>
2025-10-16 17:48:27 -0300

    refactor: migrar de ModelAdmin para SnippetViewSet em article.wagtail_hooks
    - Remover imports de wagtail_modeladmin (ModelAdmin, ModelAdminGroup, CreateView)
    - Adicionar import SnippetViewSetGroup
    - Converter ArticleAvailabilitySnippetViewSet com novos list_display e list_filter
    - Converter ArticleSnippetViewSet com list_filter atualizado
    - Converter ArticleExporterSnippetViewSet e remover método pid_v3()
    - Converter ArticleFormatSnippetViewSet
    - Converter ArticleFundingSnippetViewSet
    - Remover DuplicateArticlesViewSet
    - Remover classes customizadas de CreateView (ArticleCreateView, etc)
    - Remover CustomFilters (CollectionFilter, ArticleSourceCollectionFilter)
    - Criar ArticleSnippetViewSetGroup com todos os ViewSets registrados
    - Comentar registros antigos e registrar apenas o novo Group

--- article/wagtail_hooks.py
+++ article/wagtail_hooks.py
@@ -1,18 +1,13 @@
 from typing import Any
 
-from django.contrib.admin import SimpleListFilter
 from django.core.exceptions import PermissionDenied
 from django.db.models import Count
-from django.db.models.query import QuerySet
-from django.http import HttpResponseRedirect
 from django.utils.translation import gettext_lazy as _
 from wagtail import hooks
 from wagtail.snippets.models import register_snippet
-from wagtail.snippets.views.snippets import SnippetViewSet
-from wagtail_modeladmin.options import ModelAdmin, ModelAdminGroup, modeladmin_register
-from wagtail_modeladmin.views import CreateView
+from wagtail.snippets.views.snippets import SnippetViewSet, SnippetViewSetGroup
 
-from article.models import (  # AbstractModel,; Category,; Title,
+from article.models import (
     Article,
     ArticleExporter,
     ArticleFormat,
@@ -26,59 +21,25 @@ from config.menu import get_menu_order
 
 class ArticleAvailabilitySnippetViewSet(SnippetViewSet):
     model = ArticleAvailability
+    menu_label = _("Article Availability")
+    menu_icon = "link"
+    menu_order = 1
     
-    list_display = ["article", "collection", "lang", "fmt", "available", "url"]
-    list_filter = ["collection", "lang", "available", "fmt"]
-    search_fields = ["url", "article__title"]
+    list_display = ["url",  "available", "error", "updated"]
+    list_filter = ["collection", "lang", "fmt", "available", "error"]
+    search_fields = ["url", "article__sps_pkg_name", "article__pid_v3", "article__pid_v2"]
     icon = "link"
     menu_label = _("Article Availability")
 
-register_snippet(ArticleAvailabilitySnippetViewSet)
+# register_snippet(ArticleAvailabilitySnippetViewSet)
 
 
-class CollectionFilter(SimpleListFilter):
-    title = _("Collection")
-    parameter_name = "collection"
-
-    def lookups(self, request, model_admin):
-        collections = Collection.objects.filter(is_active=True)
-        return [(collection.id, collection.main_name) for collection in collections]
-
-    def queryset(self, request, queryset):
-        if self.value():
-            return queryset.filter(journal__scielojournal__collection__id=self.value())
-
-
-class ArticleSourceCollectionFilter(SimpleListFilter):
-    title = _("Collection")
-    parameter_name = "collection"
-
-    def lookups(self, request, model_admin):
-        collections = Collection.objects.filter(is_active=True)
-        return [(collection.id, collection.main_name) for collection in collections]
-
-    def queryset(self, request, queryset):
-        if self.value():
-            return queryset.filter(am_article__collection__id=self.value())
-
-
-class ArticleCreateView(CreateView):
-    def form_valid(self, form):
-        self.object = form.save_all(self.request.user)
-        return HttpResponseRedirect(self.get_success_url())
-
-
-class ArticleAdmin(ModelAdmin):
+class ArticleSnippetViewSet(SnippetViewSet):
     model = Article
-    create_view_class = ArticleCreateView
     menu_label = _("Article")
     menu_icon = "folder"
     menu_order = 1
-    add_to_settings_menu = False  # or True to add your model to the Settings sub-menu
-    exclude_from_explorer = (
-        False  # or True to exclude pages of this type from Wagtail's explorer view
-    )
-    list_per_page = 20
+    
     list_display = (
         "sps_pkg_name",
         "pid_v3",
@@ -89,7 +50,12 @@ class ArticleAdmin(ModelAdmin):
         "created",
         "updated",
     )
-    list_filter = ("is_public", "data_status", "valid", CollectionFilter)
+    list_filter = [
+        "is_public",
+        "data_status",
+        "valid",
+        "journal__scielojournal__collection",
+    ]
     search_fields = (
         "titles__plain_text",
         "pid_v2",
@@ -99,21 +65,12 @@ class ArticleAdmin(ModelAdmin):
     )
 
 
-class ArticleExporterCreateView(CreateView):
-    def form_valid(self, form):
-        self.object = form.save_all(self.request.user)
-        return HttpResponseRedirect(self.get_success_url())
-
-
-class ArticleExporterAdmin(ModelAdmin):
+class ArticleExporterSnippetViewSet(SnippetViewSet):
     model = ArticleExporter
-    create_view_class = ArticleExporterCreateView
     menu_label = _("Article Export")
     menu_icon = "folder"
     menu_order = 100
-    add_to_settings_menu = False
-    exclude_from_explorer = False
-
+    
     list_display = ("parent", "collection", "destination", "status", "updated")
     list_filter = (
         "collection",
@@ -122,29 +79,13 @@ class ArticleExporterAdmin(ModelAdmin):
     )
     search_fields = ("parent__pid_v3", "parent__sps_pkg_name")
 
-    def pid_v3(self, obj):
-        return obj.parent.pid_v3
 
-    pid_v3.short_description = "PID_V3"
-
-
-class ArticleFormatCreateView(CreateView):
-    def form_valid(self, form):
-        self.object = form.save_all(self.request.user)
-        return HttpResponseRedirect(self.get_success_url())
-
-
-class ArticleFormatAdmin(ModelAdmin):
+class ArticleFormatSnippetViewSet(SnippetViewSet):
     model = ArticleFormat
-    create_view_class = ArticleFormatCreateView
     menu_label = _("Article Format")
     menu_icon = "folder"
     menu_order = 500
-    add_to_settings_menu = False  # or True to add your model to the Settings sub-menu
-    exclude_from_explorer = (
-        False  # or True to exclude pages of this type from Wagtail's explorer view
-    )
-
+    
     list_display = (
         "article",
         "format_name",
@@ -160,81 +101,21 @@ class ArticleFormatAdmin(ModelAdmin):
         "version",
     )
 
-
-class ArticleFundingCreateView(CreateView):
-    def form_valid(self, form):
-        self.object = form.save_all(self.request.user)
-        return HttpResponseRedirect(self.get_success_url())
+# register_snippet(ArticleFormatSnippetViewSet)
 
 
-class ArticleFundingAdmin(ModelAdmin):
+class ArticleFundingSnippetViewSet(SnippetViewSet):
     model = ArticleFunding
-    create_view_class = ArticleFundingCreateView
     menu_label = _("Article Funding")
     menu_icon = "folder"
     menu_order = 200
-    add_to_settings_menu = False  # or True to add your model to the Settings sub-menu
-    exclude_from_explorer = (
-        False  # or True to exclude pages of this type from Wagtail's explorer view
-    )
-
+    
     list_display = ("award_id", "funding_source")
     search_fields = (
         "award_id",
         "funding_source__name",
         "funding_source__institution_type",
     )
-
-
-class ArticleAdminGroup(ModelAdminGroup):
-    menu_label = _("Articles")
-    menu_icon = "folder-open-inverse"  # change as required
-    menu_order = get_menu_order(
-        "article"
-    )  # will put in 3rd place (000 being 1st, 100 2nd)
-    items = (
-        ArticleAdmin,
-        # ArticleAvailabilitySnippetViewSet,
-        ArticleExporterAdmin,
-        ArticleFormatAdmin,
-        ArticleFundingAdmin,
-    )
-
-
-modeladmin_register(ArticleAdminGroup)
-
-
-class DuplicateArticlesViewSet(SnippetViewSet):
-    model = Article
-    icon = "folder"
-    list_display = ["pid_v3", "updated", "created"]
-
-    def get_queryset(self, request):
-        if not request.user.is_superuser:
-            raise PermissionDenied
-        ids_duplicates = []
-        duplicates = (
-            Article.objects.all()
-            .values("pid_v3")
-            .annotate(pid_v3_count=Count("pid_v3"))
-            .filter(pid_v3_count__gt=1)
-        )
-
-        for duplicate in duplicates:
-            article_ids = (
-                Article.objects.filter(pid_v3=duplicate["pid_v3"])
-                .order_by("created")[1:]
-                .values_list("id", flat=True)
-            )
-            ids_duplicates.extend(article_ids)
-
-        if ids_duplicates:
-            return Article.objects.filter(id__in=ids_duplicates)
-        else:
-            return Article.objects.none()
-
-
-register_snippet(DuplicateArticlesViewSet)
 
 
 class ArticleSourceSnippetViewSet(SnippetViewSet):
@@ -245,13 +126,29 @@ class ArticleSourceSnippetViewSet(SnippetViewSet):
     menu_icon = "doc-full"
     menu_order = 200
 
-    # List view configuration
     list_display = ["am_article", "pid_provider_xml", "status", "source_date", "updated"]
-    list_filter = ["status", "am_article__collection",]
+    list_filter = [
+        "status",
+        "am_article__collection",
+    ]
     search_fields = ["url", "pid_provider_xml__v3", "am_article__collection__acronym"]
     ordering = ["-updated"]
     list_per_page = 25
 
+# register_snippet(ArticleSourceSnippetViewSet)
 
-# Register the snippet
-register_snippet(ArticleSourceSnippetViewSet)
+
+class ArticleSnippetViewSetGroup(SnippetViewSetGroup):
+    menu_label = _("Articles")
+    menu_icon = "folder-open-inverse"
+    menu_order = get_menu_order("article")
+    items = (
+        ArticleSnippetViewSet,
+        ArticleAvailabilitySnippetViewSet,
+        ArticleExporterSnippetViewSet,
+        ArticleFormatSnippetViewSet,
+        ArticleFundingSnippetViewSet,
+        ArticleSourceSnippetViewSet,
+    )
+
+register_snippet(ArticleSnippetViewSetGroup)
