e0302786f0961b1aab5694dd8fed273efb3dec7d
Samuel Veiga Rangel <samuelveigarangel@hotmail.com>
2025-11-11 16:29:41 -0300

    fix: implementa filtro ensure_protocol para URLs de domínio das coleções

--- journalpage/templatetags/custom_tags.py
+++ journalpage/templatetags/custom_tags.py
@@ -5,3 +5,11 @@ register = template.Library()
 @register.filter
 def dict_key(d, key):
     return d.get(key, {})
+
+@register.filter
+def ensure_protocol(url):
+    if not url:
+        return ''
+    if not url.startswith(('http://', 'https://')):
+        return f'http://{url}'
+    return url
