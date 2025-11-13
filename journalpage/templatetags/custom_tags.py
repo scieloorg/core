8dba9822c7dbb7e913e1e95b34ffb446d8ad4917
Samuel Veiga Rangel <samuelveigarangel@hotmail.com>
2025-11-13 15:23:09 -0300

    remove templatetag ensure_protocol

--- journalpage/templatetags/custom_tags.py
+++ journalpage/templatetags/custom_tags.py
@@ -5,11 +5,3 @@ register = template.Library()
 @register.filter
 def dict_key(d, key):
     return d.get(key, {})
-
-@register.filter
-def ensure_protocol(url):
-    if not url:
-        return ''
-    if not url.startswith(('http://', 'https://')):
-        return f'http://{url}'
-    return url
