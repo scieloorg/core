b562cd3fc1a1464734e09905d0efd5e81e021297
Roberta Takenaka <roberta.takenaka@scielo.org>
2026-01-30 19:34:39 -0300

    refactor(organization): remove deprecated DATA_STATUS_CHOICES

--- organization/choices.py
+++ organization/choices.py
@@ -7,11 +7,3 @@ SOURCE_CHOICES = [
     ("MEC", _("Ministério da Educação e Cultura")),
     ("ror", _("Research Organization Registry")),
 ]
-
-DATA_STATUS_CHOICES = [
-    ("raw", _("raw")),
-    ("pending", _("pending")),
-    ("certified", _("certified")), # validada por um agente confiável
-    ("invalid", _("invalid")),
-    ("matched", _("matched")), # relacionada com um registro certified
-]
