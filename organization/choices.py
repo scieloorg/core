5bb83097addef7bb5e444126e0c8b89ef7d45ecc
Roberta Takenaka <roberta.takenaka@scielo.org>
2026-01-30 15:08:44 -0300

    Cria definição de SOURCE_CHOICES e DATA_STATUS_CHOICES

--- organization/choices.py
+++ organization/choices.py
@@ -1,1 +1,17 @@
 from django.utils.translation import gettext_lazy as _
+
+
+SOURCE_CHOICES = [
+    ("user", _("user")),
+    ("legacy", _("legacy")),
+    ("MEC", _("Ministério da Educação e Cultura")),
+    ("ror", _("Research Organization Registry")),
+]
+
+DATA_STATUS_CHOICES = [
+    ("raw", _("raw")),
+    ("pending", _("pending")),
+    ("certified", _("certified")), # validada por um agente confiável
+    ("invalid", _("invalid")),
+    ("matched", _("matched")), # relacionada com um registro certified
+]
