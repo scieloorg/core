043d19de54d172b6d3ec9c7dae26af62d4e8182a
Roberta Takenaka <roberta.takenaka@scielo.org>
2026-03-13 17:31:25 -0300

    feat(collection): pass verify param in task_load_collections

--- collection/tasks.py
+++ collection/tasks.py
@@ -8,9 +8,9 @@ User = get_user_model()
 
 
 @celery_app.task(bind=True)
-def task_load_collections(self, user_id=None, username=None):
+def task_load_collections(self, user_id=None, username=None, verify=True):
     if user_id:
         user = User.objects.get(pk=user_id)
     if username:
         user = User.objects.get(username=username)
-    Collection.load(user)
+    Collection.load(user, verify=verify)
