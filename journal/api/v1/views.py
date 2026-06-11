d021cc9fbc7c352dbe1cb902c4fd39ee5f7d244d
Samuel Veiga Rangel <samuelveigarangel@hotmail.com>
2026-06-11 14:17:33 -0300

    black

--- journal/api/v1/views.py
+++ journal/api/v1/views.py
@@ -1,5 +1,5 @@
-from rest_framework import serializers, viewsets
 from django.db.models import F, Prefetch, Q
+from rest_framework import serializers, viewsets
 
 from core.utils.utils import formated_date_api_params
 from core.validators import validate_params
@@ -13,19 +13,19 @@ class ArticleMetaFormatSerializer(serializers.ModelSerializer):
         model = models.Journal
 
     def to_representation(self, instance):
-        collection = getattr(instance, 'collection_acron', None)
+        collection = getattr(instance, "collection_acron", None)
         return instance.articlemeta_format(collection)
 
-    
+
 class GenericJournalViewSet(viewsets.ModelViewSet):
     serializer_class = JournalSerializer
     http_method_names = ["get"]
     queryset = models.Journal.objects.prefetch_related(
         Prefetch(
             "crossmark_policy",
-            queryset=models.CrossmarkPolicy.objects
-            .select_related("language", "journal__official")
-            .prefetch_related(
+            queryset=models.CrossmarkPolicy.objects.select_related(
+                "language", "journal__official"
+            ).prefetch_related(
                 Prefetch(
                     "journal__scielojournal_set",
                     queryset=models.SciELOJournal.objects.select_related("collection"),
@@ -38,7 +38,7 @@ class GenericJournalViewSet(viewsets.ModelViewSet):
 class JournalViewSet(GenericJournalViewSet):
     def get_queryset(self):
         query_params = self.request.query_params
-        
+
         validate_params(
             self.request,
             "issn_print",
@@ -60,41 +60,44 @@ class JournalViewSet(GenericJournalViewSet):
 
         params = {}
         if issn := query_params.get("issn"):
-           params["scielojournal__issn_scielo"] = issn
+            params["scielojournal__issn_scielo"] = issn
         if issn_electronic := query_params.get("issn_electronic"):
-           params["official__issn_electronic"] = issn_electronic
+            params["official__issn_electronic"] = issn_electronic
         if issn_print := query_params.get("issn_print"):
-           params["official__issn_print"] = issn_print
+            params["official__issn_print"] = issn_print
         if issnl := query_params.get("issnl"):
-           params["official__issnl"] = issnl
+            params["official__issnl"] = issnl
         if title := query_params.get("title"):
-           params["title"] = title
+            params["title"] = title
         if toc_item := query_params.get("toc_item"):
-           params["journaltocsection__toc_items__text"] = toc_item
+            params["journaltocsection__toc_items__text"] = toc_item
         if thematic_areas := query_params.get("thematic_areas"):
-           params["subject__value__in"] = thematic_areas.split(",")
+            params["subject__value__in"] = thematic_areas.split(",")
         if collection_acron := query_params.get("collection"):
-           params["scielojournal__collection__acron3"] = collection_acron
-        
+            params["scielojournal__collection__acron3"] = collection_acron
+
         formated_date = formated_date_api_params(query_params)
         if formated_date:
             params.update(formated_date)
 
         query = super().get_queryset()
         if query_params.get("formats") == "articlemeta":
-            return query.filter(
-                scielojournal__journal__isnull=False
-            ).filter(**params).annotate(
-                collection_acron=F('scielojournal__collection__acron3')
-            ).order_by('created').distinct()
-        
-        return query.filter(**params).order_by('created').distinct()
+            return (
+                query.filter(scielojournal__journal__isnull=False)
+                .filter(**params)
+                .annotate(collection_acron=F("scielojournal__collection__acron3"))
+                .order_by("created")
+                .distinct()
+            )
+
+        return query.filter(**params).order_by("created").distinct()
 
     def get_serializer_class(self):
         format_param = self.request.query_params.get("formats")
         if format_param == "articlemeta":
             return ArticleMetaFormatSerializer
         return JournalSerializer
+
 
 class CrossmarkPolicyViewSet(viewsets.ModelViewSet):
     """
@@ -163,14 +166,12 @@ class CrossmarkPolicyViewSet(viewsets.ModelViewSet):
 
     serializer_class = CrossmarkPolicySerializer
     http_method_names = ["get"]
-    queryset = (
-        models.CrossmarkPolicy.objects
-        .select_related("language", "journal__official")
-        .prefetch_related(
-            Prefetch(
-                "journal__scielojournal_set",
-                queryset=models.SciELOJournal.objects.select_related("collection"),
-            )
+    queryset = models.CrossmarkPolicy.objects.select_related(
+        "language", "journal__official"
+    ).prefetch_related(
+        Prefetch(
+            "journal__scielojournal_set",
+            queryset=models.SciELOJournal.objects.select_related("collection"),
         )
     )
 
@@ -188,9 +189,8 @@ class CrossmarkPolicyViewSet(viewsets.ModelViewSet):
         params = {}
         issn_filter = None
         if issn := query_params.get("issn"):
-            issn_filter = (
-                Q(journal__official__issn_electronic=issn)
-                | Q(journal__official__issn_print=issn)
+            issn_filter = Q(journal__official__issn_electronic=issn) | Q(
+                journal__official__issn_print=issn
             )
         if collection := query_params.get("collection"):
             params["journal__scielojournal__collection__acron3"] = collection
