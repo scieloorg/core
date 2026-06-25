77df7fcf87ffc9e3e761945ced7920e90cd17460
Samuel Veiga Rangel <samuelveigarangel@hotmail.com>
2026-06-25 11:48:05 -0300

    add router published_article

--- config/api_router.py
+++ config/api_router.py
@@ -3,7 +3,11 @@ from rest_framework.routers import DefaultRouter, SimpleRouter
 
 from article.api.v1.views import ArticleViewSet
 from issue.api.v1.views import IssueViewSet
-from pid_provider.api.v1.views import PidProviderViewSet, FixPidV2ViewSet
+from pid_provider.api.v1.views import (
+    FixPidV2ViewSet,
+    PidProviderViewSet,
+    PublishedArticleRegistrationViewSet,
+)
 from journal.api.v1.views import CrossmarkPolicyViewSet, JournalViewSet
 from xml_validation.api.v1.views import ValidationConfigSerializerView
 from collection.api.v1.view import CollectionViewSet
@@ -19,8 +23,15 @@ router.register("article", ArticleViewSet, basename="Article")
 router.register("issue", IssueViewSet, basename="Issue")
 router.register("pid_provider", PidProviderViewSet, basename="pid_provider")
 router.register("fix_pid_v2", FixPidV2ViewSet, basename="fix_pid_v2")
+router.register(
+    "published_article",
+    PublishedArticleRegistrationViewSet,
+    basename="published_article",
+)
 router.register("journal", JournalViewSet, basename="journal")
-router.register("xml_validation", ValidationConfigSerializerView, basename="xml_validation")
+router.register(
+    "xml_validation", ValidationConfigSerializerView, basename="xml_validation"
+)
 router.register("collection", CollectionViewSet, basename="collection")
 router.register("crossmarkpolicy", CrossmarkPolicyViewSet, basename="crossmarkpolicy")
 
